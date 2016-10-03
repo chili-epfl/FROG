e7474e1e5a0229dd52d51532e0f43f48b2624f3b
Louis Faucon <lpfaucon@gmail.com>
2016-10-03 10:33:00 +0200

    Writing a first Activity object in the database

--- frog/imports/ui/Admin.jsx
+++ frog/imports/ui/Admin.jsx
@@ -1,9 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
  
+import { Activities } from '../api/activities.js'; 
+
 export default class Admin extends Component {
 
   createBasicActivities() {
     console.log("creating activities");
+    Activities.upsert({
+      _id: "quizz_std_1",
+    }, {
+      $set: {
+        type: "quizz",
+        name: "Simple qizz about the standard deviation",
+        plane: 3,
+        object: {
+          Q: "sigma = E[(X-E[X])^2] ?",
+          A: ['True','False'],
+        },
+      }
+    });
+  }
+
+  renderList() {
+    return (
+      this.props.activities ? 
+      this.props.activities.map((activity)=>(
+        <li key={activity._id}>{activity._id}:{activity.type}:{activity.plane}</li>
+      )) : <li>empty</li>
+    );
   }
 
   render() {
@@ -12,7 +37,20 @@ export default class Admin extends Component {
         <h2>Admin interface</h2>
         <p>Write activities in the database:</p>
         <button onClick={this.createBasicActivities.bind(this)}>click me</button>
+        <ul>
+          {this.renderList()}
+        </ul>
       </div>
     );
   }
 }
+ 
+Admin.propTypes = {
+  activities: PropTypes.array.isRequired,
+};
+ 
+export default createContainer(() => {
+  return {
+    activities: Activities.find({}).fetch(),
+  };
+}, Admin);
