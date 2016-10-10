d9184360724659b4972986a56b8d7898d0cbe6d6
Louis Faucon <lpfaucon@gmail.com>
2016-10-10 15:34:03 +0200

    Fix a few typos in the json objects and rewrites database inserts in Admin.jsx

--- frog/imports/ui/Admin.jsx
+++ frog/imports/ui/Admin.jsx
@@ -2,17 +2,29 @@ import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
  
 import { Activities } from '../api/activities.js'; 
+import ReactPlayer from 'react-player'
 
 export default class Admin extends Component {
 
   createBasicActivities() {
     console.log("creating activities");
-    Activities.insert(require('../../data/activities/lecture_std_1.json'));
-    Activities.insert(require('../../data/activities/lecture_std_2.json'));
-    Activities.insert(require('../../data/activities/quizz_std_1.json'));
-    Activities.insert(require('../../data/activities/quizz_std_2.json'));
-    Activities.insert(require('../../data/activities/video_std_1.json'));
-    Activities.insert(require('../../data/activities/video_std_2.json'));
+    const items = [
+      require('../../data/activities/lecture_std_1.json'),
+      require('../../data/activities/lecture_std_2.json'),
+      require('../../data/activities/quiz_std_1.json'),
+      require('../../data/activities/quiz_std_2.json'),
+      require('../../data/activities/video_std_1.json'),
+      require('../../data/activities/video_std_2.json')
+    ]
+
+    function insertItems(item) {
+      console.log(item);
+      Activities.remove(item['_id']);
+      Activities.insert(item);
+    }
+
+    items.forEach(insertItems);
+
   }
 
   renderList() {
