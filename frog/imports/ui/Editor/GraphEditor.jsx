46f54d6c8792c7d94a91bda40ea96121374a7f6c
Annick Jollès <Annick@tsf-484-wpa-5-010.epfl.ch>
2016-10-24 15:30:12 +0200

    Begins to work with OG

---
+++ frog/imports/ui/Editor/GraphEditor.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Graph from './Graph.jsx';
+import { Activities } from '../../api/activities.js';
+
+export default class GraphEditor extends Component {
+
+
+  render() {
+    return(
+      <div>
+        <h2>Insert a new Graph:</h2>
+
+      </div>
+    )
+  }
+
+}
