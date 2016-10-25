1934ee3073969aae1b934f653e49b31ba8c9b87c
Annick Jollès <Annick@MacBook-Air-de-Annick.local>
2016-10-25 12:28:57 +0200

    Added back the choice between activity and graph

--- frog/imports/ui/Body.jsx
+++ frog/imports/ui/Body.jsx
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 
 import Repository from './repository/Repository.jsx';
-import Editor from './Editor/Editor.jsx';
+import Editor from './editor/Editor.jsx';
 import Engine from './Engine.jsx';
 import Analytics from './Analytics.jsx';
 import Admin from './Admin.jsx';
@@ -13,7 +13,7 @@ export default class Body extends Component {
   render() {
     // The Body components is the main component of the application
     // It simply performs a switch on the app property set by the App component
-    if (this.props.currentUser) {
+    //if (this.props.currentUser) {
       switch(this.props.app) {
         case "home":
           return (<Home />);
@@ -29,9 +29,9 @@ export default class Body extends Component {
           return (<Admin />);
         default:
           return (<p>MISSING OR WRONG STATE IN APP COMPONENT</p>);}
-    } else {
-      return (<p>NOT LOGGED IN. PLEASE SIGN IN OR SIGN UP. HELLO.</p>);
-    }
+    //} else {
+      //return (<p>NOT LOGGED IN. PLEASE SIGN IN OR SIGN UP. HELLO.</p>);
+    //}
   };
 }
 
--- frog/imports/ui/editor/Editor.jsx
+++ frog/imports/ui/editor/Editor.jsx
@@ -1,33 +1,45 @@
 import React, { Component } from 'react';
-import Activity from './Activity.jsx';
+import ActivityEditor from './ActivityEditor.jsx';
+import GraphEditor from './GraphEditor.jsx';
 import { Activities } from '../../api/activities.js';
 
 export default class Editor extends Component {
 
-  //For now, we show the result of the form with an alert, but of course it'll be then added to the database.
-  handleSubmit() {
-    if(!this.refs.newActivity.haveFieldsCompleted()) {
-      alert("Not all fields have been filled !");
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editorType:"activity", 
     }
-    else if(!this.refs.newActivity.areAnswersInChoices()){
-      alert("Not all answers are in the choices !");
-    }
-    else {
-      var item = this.refs.newActivity.generateActivity();
-      Activities.remove(item['_id']);
-      Activities.insert(item);
-      alert("Your actitvity has been added in the repository.");
-    }
+  }
+
+  handleEditorChange(event) {
+    event.preventDefault();
+    this.setState({editorType: event.target.value,});
   }
 
   render() {
     return(
       <div>
-        <h2>Insert a new Activity:</h2>
-        <Activity ref="newActivity" />
-        <button
-          type="submit"
-          onClick={this.handleSubmit.bind(this)}>Submit</button>
+        <h2>Editor</h2>
+        <form>
+        <input 
+          type="radio" 
+          value="activity"
+          readOnly
+          //checked={this.state.editorType === "activity"}
+          onChange={this.handleEditorChange.bind(this)}
+          />Activity<br/>
+        <input 
+          type="radio" 
+          value="graph"
+          readOnly
+          //checked={this.state.editorType === "graph"} 
+          onChange={this.handleEditorChange.bind(this)}
+          />Graph<br/>
+         </form> 
+
+        { (this.state.editorType === "activity") ? <ActivityEditor ref="activityEditor" /> : <GraphEditor ref="graphEditor" />}
       </div>
     )
   }
