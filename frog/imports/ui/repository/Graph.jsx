e09ee46961899fe121fa359abc184a2f9b567481
palexlee <pierre.alex.ly@gmail.com>
2016-10-31 14:01:00 +0100

    added first example of graphs

--- frog/imports/ui/repository/Graph.jsx
+++ frog/imports/ui/repository/Graph.jsx
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import ReactHighcharts from 'react-highcharts'
+//import { Highcharts } from 'react-highcharts/Highcharts'
 
 export default class Graph extends Component {
   constructor(props) {
@@ -17,12 +19,90 @@ export default class Graph extends Component {
     });
   }
 
-  visualizeGraph() {
-    return(
-      <div className="graph-show">
-        <p>I'm a graph !</p>
-      </div>
-    );
+  loadActivities(renderer, colors) {
+    var x = 100;
+    this.props.nodes.forEach((node) => {
+      renderer.label(node, x, 82)
+              .attr({
+                  fill: colors[0],
+                  stroke: 'white',
+                  'stroke-width': 2,
+                  padding: 5,
+                  r: 5
+              })
+              .css({
+                  color: 'white'
+              })
+              .add()
+              .shadow(true);
+      x += 200;
+    });
+    return renderer;
+
+  }
+
+
+
+  chartConfig() {
+    var context = this;
+    return {
+      chart: {
+        inverted: false,
+        showAxes: true,
+        events: {
+          load: function () {
+
+            // Draw the flow chart
+            var ren = this.renderer,
+                colors = ReactHighcharts.Highcharts.getOptions().colors,
+                rightArrow = ['M', 0, 0, 'L', 100, 0, 'L', 95, 5, 'M', 100, 0, 'L', 95, -5],
+                leftArrow = ['M', 100, 0, 'L', 0, 0, 'L', 5, 5, 'M', 0, 0, 'L', 5, -5];
+
+
+            /*
+            // Separator, client from service
+            ren.path(['M', 120, 40, 'L', 120, 330])
+                .attr({
+                    'stroke-width': 2,
+                    stroke: 'silver',
+                    dashstyle: 'dash'
+                })
+                .add();
+                */
+
+            {context.loadActivities(ren, colors)}
+          },
+        },
+
+
+      },
+      title: {
+        text: this.props.name,
+      },
+      xAxis: {
+        min: 0,
+        max: 100,
+      },
+      yAxis: {
+        categories: ['Plane 1', 'Plane 2', 'Plane 3'],
+        min: 0,
+        max: 2,
+      },
+      series: [{
+        data: []
+      }],
+    }
+
+      /*
+      xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+      },
+      series: [{
+        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
+      }],
+      */
+
+
   }
 
   render() {
@@ -35,7 +115,7 @@ export default class Graph extends Component {
       {
         //If the user has clicked on the graph, put prevous properties in addition
         //to hidden properties (returned by the renderObjectProperties)
-        this.state.isClicked ? this.visualizeGraph() : ""
+        this.state.isClicked ? <ReactHighcharts config={this.chartConfig()} context={this} ref="chart" /> : ""
       }
 
       </div>
