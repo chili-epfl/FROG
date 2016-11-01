0f4d768e5052d783671938bd7771dffa9aea56b5
Stian Håklev <shaklev@gmail.com>
2016-11-01 14:12:21 +0100

    Making it run cleanly from Git install

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
-cd frog-tools
+cd frog-utils
 npm install
 npm link
 cd ac-form
-npm link frog-tools
+npm link frog-utils
 npm install
 npm link
 cd ac-iframe
-npm link frog-tools
+npm link frog-utils
 npm install
 npm link
 cd op-random
-npm link frog-tools
+npm link frog-utils
 npm install
 npm link
 cd ../op-arguegraph
-npm link frog-tools
+npm link frog-utils
 npm install
 npm link
 cd ../ac-video
-npm link frog-tools
+npm link frog-utils
 npm install
 npm link
 cd ../FROG
-npm link frog-tools
+npm link frog-utils
 npm link op-random
 npm link op-arguegraph
 npm link ac-video
 npm link ac-iframe
-npm link oc-form
+npm link ac-form
+npm link ac-text
+npm install
+meteor install
