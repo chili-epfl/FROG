fc06abbe2547a0c1e035a73de29917f7407a2cdc
Louis Faucon <lpfaucon@gmail.com>
2016-11-07 11:11:33 +0100

    Readd the BA onClick display

--- frog/imports/ui/Repository.jsx
+++ frog/imports/ui/Repository.jsx
@@ -4,20 +4,44 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Activities } from '../api/activities';
 import { Graphs } from '../api/graphs';
 
+class ActivityDisplay extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isClicked: false,
+    }
+  }
+
+  toggleDisplay = (event) => {
+    event.preventDefault();
+    this.setState({
+      isClicked: !this.state.isClicked,
+    });
+  }
+
+  render() { return (
+    <div>
+      <b onClick={this.toggleDisplay}>{this.props.activity.activity_type}:</b>
+      <p> {this.props.activity.data.name}</p>
+      { this.state.isClicked ?
+        <pre>{JSON.stringify(this.props.activity.data, null, 2)}</pre>
+        : null
+      }
+    </div>
+  )}
+}
+
 class Repository extends Component {
 
   render() {
     return (
       <div>
         <h1> Activities </h1>
-        <ul> {
-          this.props.activities.map((activity)=>(
-            <li key={activity._id}>
-              <b>{activity.activity_type}:</b><p> {activity.data.name}</p>
-              <pre>{JSON.stringify(activity.data, null, 2)}</pre>
-            </li>
-          ))
-        } </ul>
+        <ul> { this.props.activities.map( (activity) => (
+          <li key={activity._id}>
+            <ActivityDisplay activity={activity} />
+          </li>
+        ))} </ul>
         <h1> Graphs </h1>
         <ul> {
           this.props.graphs.map((graph)=>(
@@ -34,6 +58,7 @@ class Repository extends Component {
 
 Repository.propTypes = {
   activities: PropTypes.array.isRequired,
+  graphs: PropTypes.array.isRequired
 };
 
 export default createContainer(() => {
