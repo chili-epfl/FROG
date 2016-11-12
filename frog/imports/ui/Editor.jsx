7461ee15533a9bdd0fbb2660190a40af8162bb13
Annick Jollès <Annick@MacBook-Air-de-Annick.local>
2016-11-12 21:17:23 +0100

    Lines for planes and draggable. Needs now to constraint draggable

--- frog/imports/ui/Editor.jsx
+++ frog/imports/ui/Editor.jsx
@@ -11,6 +11,8 @@ import { objectize } from '../../lib/utils'
 
 import { activity_types } from '../activity_types';
 
+import Draggable, {DraggableCore} from 'react-draggable';
+
 const ActivityList = ( { activities } ) => { return(
   <div>
     <h1>Activity list</h1>
@@ -51,6 +53,35 @@ const ActivityForm = ( { form, submit } ) => { return (
   </div>
 )}
 
+const GraphDisplay = ( {reference} ) => { return(
+  <div ref={reference}>
+    <svg width="100%" height="200px" xmlns="http://www.w3.org/2000/svg" style={{overflow: "scroll"}}>
+      <text x="0%" y="10%">Plane 1</text>
+      <line x1="10%" y1="10%" x2="100%" y2="10%" style={{stroke: 'black', strokeWidth:"2"}} />
+
+      <text x="0%" y="50%">Plane 2</text>
+      <line x1="10%" y1="50%" x2="100%" y2="50%" style={{stroke: 'black', strokeWidth:"2"}}/>
+
+      <text x="0%" y="90%">Plane 3</text>
+      <line x1="10%" y1="90%" x2="100%" y2="90%" style={{stroke: 'black', strokeWidth:"2"}}/>
+    </svg>
+  </div>
+)}
+
+const ActivityDraggableList = ( {activities, graph} ) => { return(
+  <div>
+      {alert(graph.getBoundingClientRect())}
+      {activities.map((activity, index) => 
+        <Draggable grid={[20, 20]} key={activity._id}>
+          <div>
+            <div>{activity.data.name}</div>
+          </div>
+        </Draggable>
+      ) 
+    }
+  </div>
+)}
+
 class ActivityBody extends Component {
   constructor(props) {
     super(props);
@@ -70,6 +101,8 @@ class ActivityBody extends Component {
         <ActivityForm form={this.state.form} submit={this.submitAddActivity}/>
         <ActivityList activities={this.props.activities} />
         <ActivityTypeList activity_types={activity_types} setFn={(form) => this.setState({form:form})} />
+        <GraphDisplay reference="Graph"/>
+        <ActivityDraggableList activities={this.props.activities} graph={this.refs.Graph} />
       </div>
     )
   }
