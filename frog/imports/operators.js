5c6b682cac3f62fbbe72e6ae0bc6f1ca4bc86c55
Louis Faucon <lpfaucon@gmail.com>
2016-11-14 10:48:57 +0100

    Delete old file operators.js

--- frog/imports/operators.js
+++
@@ -1,7 +0,0 @@
-import opRandom from 'op-random'
-import opArguegraph from 'op-arguegraph'
-
-import { keyBy } from 'lodash'
-
-export const operators = [opRandom, opArguegraph]
-export const operators_hash = keyBy(operators, 'id')
