5427b9fe5d15ea588f5f1550032a6ce95e3d5a32
Khalil Mrini <Khalil.Mrini@epfl.ch>
2016-11-14 13:36:03 +0100

    Created separate ac-quiz #32

--- frog/imports/activity_types.js
+++ frog/imports/activity_types.js
@@ -3,9 +3,10 @@ import acIframe from 'ac-iframe'
 import acText from 'ac-text'
 import acForm from 'ac-form'
 import acCollabForm from 'ac-collab-form'
+import acQuiz from 'ac-quiz'
 
 import { keyBy } from 'lodash'
 
-export const activity_types = [acVideo, acIframe, acText, acForm, acCollabForm]
+export const activity_types = [acVideo, acIframe, acText, acForm, acCollabForm, acQuiz]
 
 export const activity_types_obj = keyBy(activity_types, 'id')
