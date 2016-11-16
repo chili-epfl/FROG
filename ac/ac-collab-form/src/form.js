89a7e45ccb52a315ca8f93fb4c19d96c0e4b6826
Stian Håklev <shaklev@gmail.com>
2016-11-16 09:48:24 +0100

    Social operator (#36)
    * Cleaning up package.json, adding bcrypt, sorting
    
    * Initial attempt at randomizing group operator
    
    * Config for operators works and is displayed
    
    * Activities work
    
    * Activities, logging, persistence of graphs
    
    * Engine, log, student view separated. Current activity in database, objectize.
    
    * Adding pause, cleaning up views
    
    * Lot's of activities and orchestration
    
    * Making it run cleanly from Git install
    
    * Removing meteor install, was mistake
    
    * Fix `../` in initial setup
    
    * add ac-text in initial setup
    
    * Add initial_setup in circle.yml
    
    * sort activities and operators in two folders and prettify initial_setup
    
    * Fix bash bug in initial setup
    
    * - add missing dependency react-iframe in frog/package.json
    - experiment with circle.ci tests
    
    * Refixed tests as Stian did for #29
    
    * Correct Home.app-test.js
    
    * - Separates the Repository and Editor components from controlling the current state of the app, as their role is simply to display or create new learning activities/orchestration
    - Creates TeacherView with the idea of Sessions. Can create new sessions, select one to be controlled, change its state from CREATED,STARTED,PAUSED,STOPPED and changed its current activity
    -> TODO handle the StudentView with Session choice
    -> TODO handle Logs
    
    * Major code clean-up and reorganization
    
    * Start working on student view
    
    * Quite a lot of debugging already
    
    * Seems that it's working
    TODO:
    - Remove unused appstate collection
    - Handle logs
    - Save in teacher profile their controlled session
    
    * Fix editor
    
    * - remove appstate
    - write the chosen controlled session in teacher user.profile
    - display logs on TeacherView
    
    TODO:
    - in Repository reimplement the filter on type and names to search for activities and the display of config onClick
    
    * WIP Darn react component won't work
    
    * Remove usage of `.bind(this)`
    https://github.com/chili-epfl/FROG/commit/0f0a659b0e9199061dcda8b906abacfd2dfe6ba5#commitcomment-19717268
    
    * Fix a few bugs on the Home component
    
    * Readd the BA onClick display
    
    * Reimplement filtering in Repository
    
    * more code clean up to be ready to merge with develop
    
    * Handle start/pause buttons of TeacherView
    
    * Removes import to react-iframe in ac-text
    
    * Adding collab-form, reactiveData and reactiveFn work
    
    * Updated README
    
    * Added chat and reactive lists for activity_runners
    
    * Dashboard for video player
    
    * Dashboard for video and collab form with changing colors
    
    * Cleanup of layout, edit activities, completion
    
    * Cleaning up and small bug fixes
    
    * Switching to Bootstrap layout
    
    * Hiding Home title when not on Home app
    
    * Operator editing works well
    
    * Text aggregator operator works
    
    * Small formatting of chat
    
    * Small fixes
    
    * Operator works - not collab operator
    
    * Fixing tests
    
    * Delete old file operators.js
    
    * Meteor Update
    
    * Small cleaning
    
    * WIP
    
    * Social operator works

--- ac/ac-collab-form/src/form.js
+++ ac/ac-collab-form/src/form.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Form from "react-jsonschema-form";
 
-// Obviously assumes even array
 class CollabForm extends Component {
   constructor(props) {
     super(props);
