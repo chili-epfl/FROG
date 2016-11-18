ebfc73a4b1a395b54a4d97b2f6de545daf83bcce
Khalil Mrini <Khalil.Mrini@epfl.ch>
2016-11-19 00:16:03 +0100

    Finished schema json and uiSchema to show quiz #32

--- ac/ac-quiz/src/index.js
+++ ac/ac-quiz/src/index.js
@@ -59,18 +59,29 @@ export const config = {
   }
 }
 
-// Obviously assumes even array
 export const ActivityRunner = ({ config, logger, onCompletion }) => {
-  const propdef = config.questions.split(',').reduce(
-    (acc, x, i) => ({...acc, [i + '']: { type: 'string', title: x}}),
+  const propdef = config.MCQ.reduce(
+    (acc, x, i) => ({...acc, [i + '']: { 
+      type: 'string', 
+      title: x.title,
+      description: x.details,
+      enum: x.answers.reduce((accum, y, j) => ([...accum, y.title]), {})
+    }}),
     {} )
   const formdef = { 
-    title: config.title,
+    title: config.name,
     type: 'object',
     properties: propdef
-  } 
+  }
+  const uiSchema = {
+    'MCQ': {
+      "ui:options": {
+        "backgroundColor": "pink"
+      }
+    }
+  }
   return (
-    <Form schema={formdef} onSubmit={(x) => onCompletion(x.formData)} onChange={(x) => logger({form: x.formData})} />
+    <Form schema={formdef} uiSchema={uiSchema} onSubmit={(x) => onCompletion(x.formData)} onChange={(x) => logger({form: x.formData})} />
   )
 }
 
