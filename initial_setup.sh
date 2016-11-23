5ca87bbc6bbdbf1b2e5f39d3b68d26c39a62e048
Stian Håklev <shaklev@gmail.com>
2016-11-23 16:49:42 +0100

    Yarn and symlinks of dev-dependencies

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 FROG=`pwd`
 
+yarn install
+
 # install package frog-utils
 cd $FROG/frog-utils
-npm install
-npm link
+mkdir -p node_modules
+ln -s $FROG/node_modules/* node_modules/
+yarn install
+yarn link
 
 # install activities and operators packages
 for dir in $FROG/ac/ac-*/ $FROG/op/op-*/
 do
     cd $dir
-    npm link frog-utils
-    npm install
-    npm link
+    mkdir -p node_modules
+    ln -s $FROG/node_modules/* node_modules/
+    yarn link frog-utils
+    yarn install
+    yarn link
 done
 
 # links all packages to the frog/ meteor project
 cd $FROG/frog
-npm link frog-utils
+mkdir -p node_modules
+ln -s $FROG/node_modules/* node_modules/
+yarn link frog-utils
 for dir in `ls $FROG/ac |grep 'ac'` `ls $FROG/op |grep 'op'`
 do
-    npm link $dir
+    yarn link $dir
 done
 
-npm install
+yarn install
+
