9b28972daeddbb186469513680fa5c14db491333
Khalil Mrini <Khalil.Mrini@epfl.ch>
2016-12-12 17:27:16 +0100

    Can now download json file of graph #49

--- frog/imports/ui/GraphEditor.jsx
+++ frog/imports/ui/GraphEditor.jsx
@@ -15,6 +15,51 @@ const setCurrentGraph = (graphId) => {
   Meteor.users.update({_id:Meteor.userId()},{$set: {'profile.editingGraph': graphId}})
 }
 
+function download(strData, strFileName, strMimeType) {
+    var D = document,
+        A = arguments,
+        a = D.createElement("a"),
+        d = A[0],
+        n = A[1],
+        t = A[2] || "text/plain";
+
+    //build download link:
+    a.href = "data:" + strMimeType + "charset=utf-8," + escape(strData);
+
+
+    if (window.MSBlobBuilder) { // IE10
+        var bb = new MSBlobBuilder();
+        bb.append(strData);
+        return navigator.msSaveBlob(bb, strFileName);
+    } /* end if(window.MSBlobBuilder) */
+
+
+
+    if ('download' in a) { //FF20, CH19
+        a.setAttribute("download", n);
+        a.innerHTML = "downloading...";
+        D.body.appendChild(a);
+        setTimeout(function() {
+            var e = D.createEvent("MouseEvents");
+            e.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+            a.dispatchEvent(e);
+            D.body.removeChild(a);
+        }, 66);
+        return true;
+    }; /* end if('download' in a) */
+
+
+
+    //do iframe dataURL download: (older W3)
+    var f = D.createElement("iframe");
+    D.body.appendChild(f);
+    f.src = "data:" + (A[2] ? A[2] : "application/octet-stream") + (window.btoa ? ";base64" : "") + "," + (window.btoa ? window.btoa : escape)(strData);
+    setTimeout(function() {
+        D.body.removeChild(f);
+    }, 333);
+    return true;
+}
+
 var jsPlumbInstance = null
 
 jsPlumbRemoveAll = () => {
@@ -191,6 +236,16 @@ class GraphEditorClass extends Component {
     jsPlumbRemoveAllAndDrawAgain(this.props.activities,this.props.operators)
   }
 
+  exportToJSON = (event) => {
+    var obj = {
+      'graph': Graphs.findOne({_id:this.props.graphId}),
+      'activities': Activities.find({ graphId: this.props.graphId }).fetch(),
+      'operators': Operators.find({ graphId: this.props.graphId }).fetch()
+    }
+    const str = 'graph' + String(this.props.graphId) + '.json'
+    download(JSON.stringify(obj), str, 'text/plain');
+  }
+
   render() { return(
     <div>
       <h3>Graph editor</h3>
@@ -217,6 +272,7 @@ class GraphEditorClass extends Component {
           !operator.data && <OperatorChoiceComponent key={operator._id} ownId={operator._id} />
         )
       })}
+      <button className='export button' onClick={this.exportToJSON}>Download the Graph</button>
     </div>
   )}
 }
