792685353ea89216030de3e45365f5a9511d1242
Annick Jollès <Annick@MacBook-Air-de-Annick.local>
2017-01-09 03:16:57 +0100

    GraphEditor

--- frog/imports/ui/graph/GraphEditor.jsx
+++ frog/imports/ui/graph/GraphEditor.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import { Meteor } from 'meteor/meteor'
 import { createContainer } from 'meteor/react-meteor-data'
 import { $ } from 'meteor/jquery'
 
 import { Graphs, addGraph, setCurrentGraph, renameGraph } from '../../api/graphs'
 import { Activities, Operators, duplicateGraph, removeGraph } from '../../api/activities'
-import Graph, { RenderGraph, computeTopPosition, scales, scaleButton } from './Graph'
+import Graph from './Graph'
 
 class GraphEditor extends Component {
 
@@ -12,7 +13,7 @@ class GraphEditor extends Component {
     super(props)
 
     this.state = {
-      infoToDisplay: "",
+      infoToDisplay: '',
       loaded: false,
       repoLoaded: false,
       current: props.graphId
@@ -20,40 +21,40 @@ class GraphEditor extends Component {
   }
 
   handleInfoClick = (graphId) => {
-    let info = this.state.infoToDisplay
-    this.setState({infoToDisplay: graphId==info ? "" : graphId})
+    const info = this.state.infoToDisplay
+    this.setState({ infoToDisplay: graphId === info ? '' : graphId })
   }
 
   handleLoaded = () => {
-    this.setState({loaded: true})
+    this.setState({ loaded: true })
   }
 
   handleRepoLoaded = () => {
-    this.setState({repoLoaded: true})
+    this.setState({ repoLoaded: true })
   }
 
   submitReplace = (graphId) => {
     setCurrentGraph(graphId)
-    this.setState({current: graphId, loaded: false})
+    this.setState({ current: graphId, loaded: false })
   }
 
   handleRemove = (graphId) => {
     removeGraph(graphId)
-    if(this.state.current == graphId) {
+    if (this.state.current === graphId) {
       setCurrentGraph(null)
-      this.setState({current: null, loaded: false})
+      this.setState({ current: null, loaded: false })
     }
   }
 
   handleRename = (event, id, oldName) => {
     const newName = event.target.value.trimLeft()
-    if(oldName != newName) {
+    if(oldName !== newName) {
       renameGraph(id, newName)
     }
   }
 
   handleDuplicate = (graphId) => {
-    let newGraphId = duplicateGraph(graphId)
+    const newGraphId = duplicateGraph(graphId)
     this.submitReplace(newGraphId)
   }
 
@@ -62,21 +63,24 @@ class GraphEditor extends Component {
       <div>
         <h3>Graph list</h3>
         <ul> { this.props.graphs.map((graph) =>
-          <li style={{listStyle: 'none', margin: "10px 0"}} key={graph._id}>
-            <a href='#' onClick={ () => this.handleRemove(graph._id) }><i className="fa fa-times" /></a>
-            <a href='#' onClick={ () => this.submitReplace(graph._id) } ><i className="fa fa-pencil" /></a>
-            <a href='#' onClick={ () => this.handleInfoClick(graph._id)} ><i className="fa fa-info" /></a>
-            <a href='#' onClick={ () => this.handleDuplicate(graph._id)} ><i className="fa fa-copy" /></a>
-            <input type="text" value={graph.name} onChange={(event) => this.handleRename(event, graph._id, graph.name)}/>
-            {this.state.current == graph._id ? '(current)':null}
-            {this.state.infoToDisplay == graph._id ?
+          <li style = { { listStyle: 'none', margin: '10px 0' } } key={ graph._id }>
+            <a href='#' onClick={() => this.handleRemove(graph._id)}><i className='fa fa-times' /></a>
+            <a href='#' onClick={() => this.submitReplace(graph._id)}><i className='fa fa-pencil' /></a>
+            <a href='#' onClick={() => this.handleInfoClick(graph._id)} ><i className='fa fa-info' /></a>
+            <a href='#' onClick={() => this.handleDuplicate(graph._id)} ><i className='fa fa-copy' /></a>
+            <input type="text" value={graph.name} onChange={(event) => this.handleRename(event, graph._id, graph.name) }/>
+            {this.state.current === graph._id ? '(current)' : null}
+            {this.state.infoToDisplay === graph._id ?
               <Graph
                 activities={[]}
                 operators={[]}
                 graphId={graph._id}
-                loaded={this.state.current == graph._id ? this.state.loaded : this.state.repoLoaded}
-                handleLoaded={this.state.current == graph._id ? this.handleLoaded : this.handleRepoLoaded}
-                editorMode={false}/> : "" }
+                loaded={this.state.current === graph._id ? this.state.loaded : this.state.repoLoaded}
+                handleLoaded={this.state.current === graph._id ? this.handleLoaded  
+                                                                : this.handleRepoLoaded}
+                editorMode={false}
+              /> 
+              : '' }
           </li>
         )} </ul>
 
@@ -87,14 +91,15 @@ class GraphEditor extends Component {
             <div>
               <br/>
               <Graph
-                activities = {this.props.activities}
-                operators = {this.props.operators}
+                activities={this.props.activities}
+                operators={this.props.operators}
                 graphId={this.state.current}
                 loaded={this.state.loaded}
                 handleLoaded={this.handleLoaded}
-                editorMode={true}/>
+                editorMode
+              />
             </div>
-            : ""
+            : ''
         }
       </div>
     )
@@ -102,14 +107,14 @@ class GraphEditor extends Component {
 }
 
 export default createContainer(() => {
-  const user = Meteor.users.findOne({_id:Meteor.userId()})
-  let currentGraphId = ""
-  if(user.profile && user.profile.editingGraph) {
+  const user = Meteor.users.findOne({ _id: Meteor.userId() })
+  let currentGraphId = ''
+  if (user.profile && user.profile.editingGraph) {
     currentGraphId = user.profile.editingGraph
   }
   return {
     graphs: Graphs.find().fetch(),
-    activities: Activities.find({ graphId: null}).fetch(),
+    activities: Activities.find({ graphId: null }).fetch(),
     operators: Operators.find({ graphId: null }).fetch(),
     graphId: currentGraphId
   }
