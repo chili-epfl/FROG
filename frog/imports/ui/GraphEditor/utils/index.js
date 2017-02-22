d186e41f2d13991d45fa47cd5acafb91337739aa
Stian Håklev <shaklev@gmail.com>
2017-02-22 16:33:43 +0100

    Fixing renaming with scale positioning

--- frog/imports/ui/GraphEditor/utils/index.js
+++ frog/imports/ui/GraphEditor/utils/index.js
@@ -7,7 +7,7 @@ export const timeToPx = (time: number, scale: number): number =>
 export const pxToTime = (px: number, scale: number): number =>
   px / 3900 / scale * 120;
 export const timeToPxScreen = (time: number): number =>
-  time * 3900 * store.ui.scale / 120 - store.ui.panx * 4;
+  time * 3900 * store.ui.scale / 120 - store.ui.panx * 4 * store.ui.scale;
 
 export const between = (
   rawminval: number,
