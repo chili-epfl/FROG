cfa2a67999826648c6e8fbc61a3b63c6b7c1586e
lfaucon <lpfaucon@gmail.com>
2017-03-17 16:52:37 +0100

    TeacherView cleaner and more robust

--- frog/imports/ui/TeacherView.jsx
+++ frog/imports/ui/TeacherView.jsx
@@ -1,3 +1,5 @@
+// @flow
+
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
@@ -7,149 +9,123 @@ import {
   addSession,
   removeSession,
   updateSessionState,
-  updateSessionActivity,
   setTeacherSession
 } from '../api/sessions';
-import { Activities, Operators, addResult } from '../api/activities';
+import { Activities } from '../api/activities';
 import { Graphs } from '../api/graphs';
 import { Logs } from '../api/logs';
-import { Products } from '../api/products';
-
 import { runSession, nextActivity } from '../api/engine';
 
 import { activityTypesObj } from '../activityTypes';
-import { operatorTypesObj } from '../operatorTypes';
-
-// check if there are any operators, and run these first
-const runProduct = (sessionId, activityId) => {
-  const ops = Operators
-    .find({ to: activityId, type: 'product' }, {
-      reactive: false
-    })
-    .fetch();
-  if (ops.length > 0) {
-    const op = ops[0];
-    const operatorType = operatorTypesObj[op.operatorType];
-    const prod = Products
-      .find({ activityId: op.from }, {
-        reactive: false
-      })
-      .fetch();
-    if (prod.length > 0) {
-      const result = operatorType.operator(op.data, prod);
-      addResult('product', activityId, result);
-    }
-  }
-};
-
-const runSocial = (sessionId, activityId) => {
-  const ops = Operators
-    .find({ to: activityId, type: 'social' }, {
-      reactive: false
-    })
-    .fetch();
-  if (ops.length > 0) {
-    const op = ops[0];
-    const operatorType = operatorTypesObj[op.operatorType];
-    const prod = Products
-      .find({ activityId: op.from }, {
-        reactive: false
-      })
-      .fetch();
-    if (prod.length > 0) {
-      const result = operatorType.operator(op.data, prod);
-      addResult('social', activityId, result);
-    }
-  }
-};
-
-const switchActivity = (sessionId, activityId) => {
-  runProduct(sessionId, activityId);
-  runSocial(sessionId, activityId);
-  updateSessionActivity(sessionId, activityId);
-};
 
 const StudentList = ({ students }) => (
-  <ul>
-    {students.map(student => (
-      <li key={student._id}>
-        <p>{student.username}</p>
-      </li>
-    ))}
-  </ul>
+  <div>
+    <h3>Registered students</h3>
+    {students && students.length
+      ? <ul>
+          {students.map(student => (
+            <li key={student._id}>{student.username}</li>
+          ))}
+        </ul>
+      : <p>NO STUDENTS</p>}
+  </div>
 );
 
 const ActivityList = ({ activities, session }) => (
-  <ul>
-    {activities.map(activity => (
-      <li key={activity._id}>
-        <a href="#" onClick={() => switchActivity(session._id, activity._id)}>
-          {activity.title} -
-          <i>{activity.activityType}</i>
-          {activity._id === session.activityId ? <i> (running)</i> : null}
-        </a>
-      </li>
-    ))}
-  </ul>
+  <div>
+    <h3>Available Activities</h3>
+    {activities && activities.length
+      ? <ul>
+          {activities.map(activity => (
+            <li key={activity._id}>
+              {activity.title} - <i>{activity.activityType}</i>
+              {activity._id === session.activityId ? ' (running)' : ''}
+            </li>
+          ))}
+        </ul>
+      : <p>NO ACTIVITY</p>}
+  </div>
 );
 
-const SessionController = createContainer(
-  ({ session }) => {
-    const activities = session
-      ? Activities.find({ sessionId: session._id }).fetch()
-      : null;
-    const students = session
-      ? Meteor.users.find({ 'profile.currentSession': session._id }).fetch()
-      : null;
-    return { session, activities, students };
-  },
-  ({ session, activities, students }) => (
+const Buttons = ({ session }) => {
+  const buttons = [
+    {
+      states: ['CREATED'],
+      type: 'primary',
+      onClick: () => runSession(session._id),
+      text: 'Start'
+    },
+    {
+      states: ['STARTED'],
+      type: 'primary',
+      onClick: () => nextActivity(session._id),
+      text: 'Next Activity'
+    },
+    {
+      states: ['STARTED'],
+      type: 'warning',
+      onClick: () => updateSessionState(session._id, 'PAUSED'),
+      text: 'Pause'
+    },
+    {
+      states: ['PAUSED', 'STOPPED'],
+      type: 'primary',
+      onClick: () => updateSessionState(session._id, 'STARTED'),
+      text: 'Restart'
+    },
+    {
+      states: ['CREATED', 'STARTED', 'PAUSED'],
+      type: 'danger',
+      onClick: () => updateSessionState(session._id, 'STOPPED'),
+      text: 'Stop'
+    },
+    {
+      states: ['STOPPED'],
+      type: 'danger',
+      onClick: () => removeSession(session._id),
+      text: 'Delete'
+    }
+  ];
+  return (
     <div>
-      <h1>Session control</h1>
-      {session
-        ? <div>
-            <p>
-              Current state <b>{session.state}</b>.
-              Control the session through selecting an activity below,
-              or Starting/Pausing/Stopping the session.
-            </p>
-            <pre>{JSON.stringify(session, null, 2)}</pre>
-            <button
-              className="btn btn-primary btn-sm"
-              onClick={() => runSession(session._id)}
-            >
-              Start
-            </button>
-            <button
-              className="btn btn-primary btn-sm"
-              onClick={() => nextActivity(session._id)}
-            >
-              Next
-            </button>
-            <button
-              className="btn btn-warning btn-sm"
-              onClick={() => updateSessionState(session._id, 'PAUSED')}
-            >
-              Pause
-            </button>
-            <button
-              className="btn btn-danger btn-sm"
-              onClick={() => updateSessionState(session._id, 'STOPPED')}
-            >
-              Stop
-            </button>
-            <h3>Registered students</h3>
-            {students && students.length
-              ? <StudentList students={students} />
-              : <p>NO STUDENTS</p>}
-            <h3>Available Activities</h3>
-            {activities && activities.length
-              ? <ActivityList activities={activities} session={session} />
-              : <p>NO ACTIVITY</p>}
-          </div>
-        : <p>Chose a session</p>}
+      {buttons
+        .filter(button => button.states.indexOf(session.state) > -1)
+        .map(button => (
+          <button
+            key={button.text}
+            className={'btn btn-' + button.type + ' btn-sm'}
+            onClick={button.onClick}
+          >
+            {button.text}
+          </button>
+        ))}
     </div>
-  )
+  );
+};
+
+const DisplaySession = ({ session }) => ({
+  CREATED: (
+    <p>
+      You have created the session and can wait for your students to join it before starting it
+    </p>
+  ),
+  STARTED: <p>The session is started, you can run the activities</p>,
+  PAUSED: <p>The session is paused, you can restart it at any time</p>,
+  STOPPED: <p>The session has been stopped</p>
+})[session.state];
+
+const SessionController = ({ session, activities, students }) => (
+  <div>
+    <h1>Session control</h1>
+    {session
+      ? <div>
+          <DisplaySession session={session} />
+          <Buttons session={session} />
+          <StudentList students={students} />
+          <ActivityList activities={activities} session={session} />
+        </div>
+      : <p>Create or select a session from the list bellow</p>}
+  </div>
 );
 
 class SessionList extends Component {
@@ -188,13 +164,9 @@ class SessionList extends Component {
         <ul>
           {this.props.sessions.map(session => (
             <li key={session._id}>
-              <a href="#" onClick={() => removeSession(session._id)}>
-                <i className="fa fa-times" />
-              </a>
               <a href="#" onClick={() => setTeacherSession(session._id)}>
-                <i className="fa fa-pencil" />
+                {session._id}
               </a>
-              {session._id}
             </li>
           ))}
         </ul>
@@ -228,47 +200,53 @@ const DashView = ({ logs, session }) => {
   );
 };
 
-const LogView = createContainer(
-  ({ session }) => {
-    const logs = session ? Logs.find({ sessionId: session._id }).fetch() : [];
-    return { logs };
-  },
-  ({ logs }) => (
-    <div>
-      <h1>Logs</h1>
-      <ul>
-        {logs
-          ? logs.map(log => (
-              <li key={log._id}>
-                <pre>{JSON.stringify(log, null, 2)}</pre>
-              </li>
-            ))
-          : <li>NO LOGS</li>}
-      </ul>
-    </div>
-  )
+const LogView = ({ logs }) => (
+  <div>
+    <h1>Logs</h1>
+    {logs.length
+      ? <ul>
+          {logs.map(log => (
+            <pre key={log._id}>{JSON.stringify(log, null, 2)}</pre>
+          ))}
+        </ul>
+      : <p>NO LOGS</p>}
+  </div>
 );
 
 export default createContainer(
   () => {
     const user = Meteor.users.findOne({ _id: Meteor.userId() });
+
     const session = user.profile
       ? Sessions.findOne({ _id: user.profile.controlSession })
       : null;
+
+    const logs = session ? Logs.find({ sessionId: session._id }).fetch() : [];
+
+    const activities = session
+      ? Activities.find({ sessionId: session._id }).fetch()
+      : null;
+
+    const students = session
+      ? Meteor.users.find({ 'profile.currentSession': session._id }).fetch()
+      : null;
+
     return {
       sessions: Sessions.find().fetch(),
       session,
       graphs: Graphs.find().fetch(),
-      logs: Logs.find({}, { sort: { createdAt: -1 }, limit: 100 }).fetch(),
+      activities,
+      students,
+      logs,
       user
     };
   },
-  ({ graphs, session, sessions, logs }) => (
+  props => (
     <div>
-      <SessionController session={session} />
-      <DashView session={session} logs={logs} />
-      <SessionList sessions={sessions} graphs={graphs} />
-      <LogView session={session} />
+      <SessionController {...props} />
+      <DashView {...props} />
+      <SessionList {...props} />
+      <LogView {...props} />
     </div>
   )
 );
