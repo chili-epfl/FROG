c25fbe7c7f25683638f051b33f69198672c6c65e
Stian Håklev <shaklev@gmail.com>
2017-03-31 14:24:41 +0200

    Adding address bar routing for apps

--- frog/imports/ui/App.jsx
+++ frog/imports/ui/App.jsx
@@ -7,6 +7,13 @@ import Body from './Body.jsx';
 import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 
 const apps = ['Home', 'Admin', 'Graph Editor', 'Teacher View', 'Student View'];
+const appSlugs = {
+  '': 'Home',
+  admin: 'Admin',
+  graph: 'Graph Editor',
+  teacher: 'Teacher View',
+  student: 'Student View'
+};
 
 const Navigation = ({ appList, changeFn, currentApp }) => (
   <Nav bsStyle="pills" activeKey={currentApp} onSelect={changeFn}>
@@ -29,7 +36,31 @@ export default class App extends Component {
       admin: 'Admin',
       noname: 'Home'
     }[username] || 'Student View';
-    this.setState({ app });
+    newApp = this.handleNewHash();
+    if (!newApp || username === 'noname') {
+      this.setState({ app });
+      this.updateAddressbar(app);
+    }
+  };
+
+  updateAddressbar = app => {
+    const url = Object.entries(appSlugs).find(([_, v]) => v === app);
+    history.pushState(null, null, '/#/' + (url && url[0]));
+  };
+
+  handleNewHash = () => {
+    const location = window.location.hash
+      .replace(/^#\/?|\/$/g, '')
+      .split('/')[0]
+      .trim();
+    if (appSlugs[location]) {
+      this.setState({ app: appSlugs[location] });
+      return appSlugs[location];
+    }
+  };
+
+  componentDidMount = () => {
+    window.addEventListener('hashchange', this.handleNewHash, false);
   };
 
   render() {
@@ -44,7 +75,10 @@ export default class App extends Component {
           <Navigation
             appList={apps}
             currentApp={this.state.app}
-            changeFn={app => this.setState({ app })}
+            changeFn={app => {
+              this.setState({ app });
+              this.updateAddressbar(app);
+            }}
           />
         </div>
         <div id="body">
