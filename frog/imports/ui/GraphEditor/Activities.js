594361785432606a7ec0e7943989d8f1df536981
Stian Håklev <shaklev@gmail.com>
2017-04-05 12:32:50 +0200

    Don't show activity internal elements if width too small

--- frog/imports/ui/GraphEditor/Activities.js
+++ frog/imports/ui/GraphEditor/Activities.js
@@ -49,61 +49,64 @@ const Activity = connect(({
         highlighted={activity.highlighted}
         selected={activity.selected}
       />
-      <svg style={{ overflow: 'hidden' }} width={width + x - 20}>
-        <text x={x + 3} y={activity.y + 20}>
-          {activity.title}
-        </text>
-      </svg>
-      <circle
-        cx={x + width - 10}
-        cy={activity.y + 15}
-        r={5}
-        fill="transparent"
-        stroke="black"
-      />
-      <DraggableCore
-        onStart={() => startDragging(activity)}
-        onStop={stopDragging}
-      >
-        <circle
-          cx={x + width - 10}
-          cy={activity.y + 15}
-          r={10}
-          fill="transparent"
-          stroke="transparent"
-          style={{ cursor: 'crosshair' }}
-        />
-      </DraggableCore>
-      <DraggableCore
-        onStart={() => startResizing(activity)}
-        onDrag={(_, { deltaX }) => activity.resize(deltaX)}
-        onStop={stopResizing}
-      >
-        <rect
-          fill="transparent"
-          stroke="transparent"
-          x={x + width - 5}
-          y={activity.y}
-          width={5}
-          height={30}
-          style={{ cursor: 'ew-resize' }}
-        />
-      </DraggableCore>
-      <DraggableCore
-        onStart={() => startMoving(activity)}
-        onDrag={(_, { deltaX }) => activity.move(deltaX)}
-        onStop={stopMoving}
-      >
-        <rect
-          x={x}
-          y={activity.y}
-          fill="transparent"
-          stroke="transparent"
-          width={width - 20}
-          height={30}
-          style={{ cursor: 'move' }}
-        />
-      </DraggableCore>
+      {width > 21 &&
+        <g>
+          <svg style={{ overflow: 'hidden' }} width={width + x - 20}>
+            <text x={x + 3} y={activity.y + 20}>
+              {activity.title}
+            </text>
+          </svg>
+          <circle
+            cx={x + width - 10}
+            cy={activity.y + 15}
+            r={5}
+            fill="transparent"
+            stroke="black"
+          />
+          <DraggableCore
+            onStart={() => startDragging(activity)}
+            onStop={stopDragging}
+          >
+            <circle
+              cx={x + width - 10}
+              cy={activity.y + 15}
+              r={10}
+              fill="transparent"
+              stroke="transparent"
+              style={{ cursor: 'crosshair' }}
+            />
+          </DraggableCore>
+          <DraggableCore
+            onStart={() => startResizing(activity)}
+            onDrag={(_, { deltaX }) => activity.resize(deltaX)}
+            onStop={stopResizing}
+          >
+            <rect
+              fill="transparent"
+              stroke="transparent"
+              x={x + width - 5}
+              y={activity.y}
+              width={5}
+              height={30}
+              style={{ cursor: 'ew-resize' }}
+            />
+          </DraggableCore>
+          <DraggableCore
+            onStart={() => startMoving(activity)}
+            onDrag={(_, { deltaX }) => activity.move(deltaX)}
+            onStop={stopMoving}
+          >
+            <rect
+              x={x}
+              y={activity.y}
+              fill="transparent"
+              stroke="transparent"
+              width={width - 20}
+              height={30}
+              style={{ cursor: 'move' }}
+            />
+          </DraggableCore>
+        </g>}
     </g>
   );
 });
