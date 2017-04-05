24d5433a94abab5fd715911d01580cf3d0809c9a
Stian Håklev <shaklev@gmail.com>
2017-04-06 01:03:25 +0200

    removing test

--- frog/imports/ui/Home.app-test.js
+++
@@ -1,9 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { assert } from 'meteor/practicalmeteor:chai';
-import { $ } from 'meteor/jquery';
-
-if (Meteor.isClient) {
-  it('renders the top level menu', () => { // eslint-disable-line
-    assert.equal($('a')[0].outerText, 'Home');
-  });
-}
