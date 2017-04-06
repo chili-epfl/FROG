a77f7a542afa2a1432529dbf2d26b6426d19217d
Stian Håklev <shaklev@gmail.com>
2017-04-06 12:05:03 +0200

    findOne

--- frog/imports/api/global.js
+++ frog/imports/api/global.js
@@ -3,14 +3,11 @@ import { Mongo } from 'meteor/mongo';
 export const GlobalSettings = new Mongo.Collection('global_settings');
 
 export const getGlobalSetting = key => {
-  const ret = GlobalSettings.find(1).fetch();
-  return ret ? ret[0][key] : null;
+  const ret = GlobalSettings.findOne(1);
+  return ret ? ret[key] : null;
 };
 
-export const getGlobalSettings = () => {
-  const ret = GlobalSettings.find(1).fetch();
-  return ret || {};
-};
+export const getGlobalSettings = () => GlobalSettings.findOne(1) || {};
 
 export const setGlobalSetting = (k, v) =>
   GlobalSettings.upsert(1, { $set: { [k]: v } });
