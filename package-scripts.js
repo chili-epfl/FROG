d604920f265b99e48ab71188e218e02bbc2bfa37
Stian Håklev <shaklev@gmail.com>
2017-04-11 12:12:48 +0200

    Package-scripts

--- package-scripts.js
+++ package-scripts.js
@@ -1,9 +1,12 @@
 module.exports = {
   scripts: {
-    test: 'flow --quiet && npm run -s eslint-test',
+    test: 'flow --quiet && npm run -s start eslint-test',
     dockerTest: 'rm -rf ./tmp/ && mkdir -p ./tmp/frog && git clone . ./tmp/frog && cd ./tmp/frog && docker build -t frogtest . && docker run frogtest',
     fix: 'eslint --fix -c .eslintrc-prettier.js --ext .js,.jsx .',
     eslintTest: 'eslint -c .eslintrc-prettier.js --ext .js,.jsx .',
     flowTest: 'flow'
+  },
+  options: {
+    silent: true
   }
 };
