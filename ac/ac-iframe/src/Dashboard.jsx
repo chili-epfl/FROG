dad9079e7dc553e39dd512c64208b26741e864c8
Louis Faucon <lpfaucon@gmail.com>
2017-04-19 21:42:28 +0200

    formatting

--- ac/ac-iframe/src/Dashboard.jsx
+++ ac/ac-iframe/src/Dashboard.jsx
@@ -2,24 +2,26 @@
 
 import React from 'react';
 import WordCloud from 'react-d3-cloud';
- 
-const fontSizeMapper = word => Math.max(50,Math.min(word.value * 10, 250));
- 
+
+const fontSizeMapper = word => Math.max(50, Math.min(word.value * 10, 250));
+
 export default ({ logs }) => {
-  const data = {}
+  const data = {};
   logs.filter(log => log.data.type === 'chat').forEach(log => {
     log.data.msg.split(' ').forEach(word => {
-      data[word] = (data[word] || 0) + 1
-    })
-  })
+      data[word] = (data[word] || 0) + 1;
+    });
+  });
 
-  return(
+  return (
     <div>
       <p>{logs.length} logs</p>
       <WordCloud
-        data={[ ...Object.keys(data).map(word => ({ text: word, value: data[word]}))]}
+        data={[
+          ...Object.keys(data).map(word => ({ text: word, value: data[word] }))
+        ]}
         fontSizeMapper={fontSizeMapper}
       />
     </div>
-  )
-}
+  );
+};
