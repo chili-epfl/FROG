4c321d00c6088c0839046f822aac60d98ace8198
Louis Faucon <lpfaucon@gmail.com>
2017-04-19 10:32:46 +0200

    small nightmare experimentation

---
+++ nightmare/main.js
@@ -0,0 +1,20 @@
+const Nightmare = require('nightmare')
+
+
+const connect = (name) => {
+    Nightmare({
+        show: true,
+        waitTimeout: 30000 // increase the default timeout to test things
+    })
+        .goto('http://localhost:3000/#/' + name)
+        .wait(5000)
+        .goto('http://localhost:3000/#/student')
+        .then(result => console.log(result))
+        .catch(error => console.error(error))
+
+}
+
+connect('nightmare1')
+connect('nightmare2')
+connect('nightmare3')
+connect('nightmare4')
