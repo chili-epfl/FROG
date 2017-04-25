e813b16bee3e0c66fcdf957fbb63b642c8f4149e
Louis Faucon <lpfaucon@gmail.com>
2017-04-25 12:53:12 -0400

    remove tests from CI

--- nightmare/test.js
+++ nightmare/test.js
@@ -1,18 +1,18 @@
 const Nightmare = require('nightmare');
 
-const apps = ['home', 'admin', 'graph', 'teacher', 'student']
+const apps = ['home', 'admin', 'graph', 'teacher', 'student'];
 
-const show = process.argv[2] === 'show'
+const show = process.argv[2] === 'show';
 
 const N = Nightmare({
   show,
   waitTimeout: 5000 // increase the default timeout to test things
-})
-  
-apps.forEach(app => {
-  N.goto('http://localhost:3000/#/' + app + '/anyuser')
-  N.wait('#' + app)
-})
+});
 
-N.end()
-N.then()
+apps.forEach(app => {
+  N.goto('http://localhost:3000/#/' + app + '/anyuser');
+  N.wait('#' + app);
+});
+
+N.end();
+N.then();
--- package-scripts.js
+++ package-scripts.js
@@ -1,6 +1,6 @@
 module.exports = {
   scripts: {
-    test: 'flow --quiet && npm run -s start eslint-test && npm start nightmareTest',
+    test: 'flow --quiet && npm run -s start eslint-test',
     dockerTest: 'rm -rf ./tmp/ && mkdir -p ./tmp/frog && git clone . ./tmp/frog && cd ./tmp/frog && docker build -t frogtest . && docker run frogtest',
     fix: 'eslint --fix -c .eslintrc-prettier.js --ext .js,.jsx .',
     eslintTest: 'eslint -c .eslintrc-prettier.js --ext .js,.jsx .',
