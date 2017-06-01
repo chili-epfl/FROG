79edc09e7203c2407c54d13c0e5188b9ef92395c
Stian Håklev <shaklev@gmail.com>
2017-06-01 14:38:24 +0200

    adding dependencies

--- frog/server/main.js
+++ frog/server/main.js
@@ -1,23 +1,24 @@
 // @flow
 
 import { Meteor } from 'meteor/meteor';
+import startShareDB from './start-sharedb';
+import ShareDB from 'sharedb';
+import WebSocket from 'ws';
+import WebsocketJSONStream from 'websocket-json-stream';
 
 import '../imports/startup/shutdown-if-env.js';
 
 import '../imports/api/messages.js';
-
 import '../imports/api/activities.js';
 import '../imports/api/graphs.js';
-
 import '../imports/api/sessions.js';
-
 import '../imports/api/logs.js';
-
 import '../imports/api/activityData.js';
 import '../imports/api/products.js';
 import '../imports/api/objects.js';
 import '../imports/api/global.js';
-
 import '../imports/api/engine.js';
 
 Meteor.publish('userData', () => Meteor.users.find({}));
+
+startShareDB();
