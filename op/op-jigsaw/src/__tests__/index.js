e71b8e5825e3742a96064f6344364ba7060032ca
Stian Håklev <shaklev@gmail.com>
2017-06-13 13:21:28 +0200

    Adding example of testing random function

---
+++ op/op-jigsaw/src/__tests__/index.js
@@ -0,0 +1,20 @@
+import op from '..';
+import lodash from 'lodash';
+
+const object = {
+  globalStructure: { studentIds: [1, 2, 3, 4, 5] },
+  socialStructure: {}
+};
+
+const expectedOutput = {
+  group: { '0': ['1', '2'], '1': ['3', '4'], '2': ['5'] },
+  role: { baker: ['2', '4'], chef: ['1', '3', '5'] }
+};
+
+const configData = { roles: 'chef, baker' };
+
+lodash.shuffle = jest.fn(x => x.sort());
+
+test('Works', () => {
+  expect(op.operator(configData, object)).toEqual(expectedOutput);
+});
