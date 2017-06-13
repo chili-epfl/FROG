35dbb7451b918e4bba9b3363af9377a88cfeadde
Stian Håklev <shaklev@gmail.com>
2017-06-13 13:25:17 +0200

    Removing leftover file

--- op/op-jigsaw/index.js
+++
@@ -1,9 +0,0 @@
-import op from '..'
-
-object = {globalStructure: {studentIds: [1,2,3,4,5]}, socialStructure: {}}
-configData = {roles: ['chef', 'baker']}
-
-
-test('Works', () => {
-  expect(op.operator(configData, object)).toEqual({}))
-}
