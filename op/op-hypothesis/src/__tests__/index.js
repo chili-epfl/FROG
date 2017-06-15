0b9a3b377b2d5ca0f0849a9234ff5c5e8f2779fc
Stian Håklev <shaklev@gmail.com>
2017-06-15 10:15:16 +0200

    Making UUID for testing

--- op/op-hypothesis/src/__tests__/index.js
+++ op/op-hypothesis/src/__tests__/index.js
@@ -1,5 +1,14 @@
-// @flow
 import { operator } from '../index';
+
+jest.mock('frog-utils', () => {
+  const createUUID = () => {
+    let cntr = 0;
+    return () => cntr++;
+  };
+  const uuid = createUUID();
+  const frogutils = require.requireActual('frog-utils');
+  return { ...frogutils, uuid };
+});
 
 test('Correctly parses API request results with contents', () =>
   expect(operator({ tag: 'reason' })).resolves.toMatchSnapshot());
