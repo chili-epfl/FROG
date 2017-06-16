a25ca1bd94c8c2a075e8affd16f54cd70ef441b1
lfaucon <lpfaucon@gmail.com>
2017-06-16 13:47:40 +0200

    Small syntax fix

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,9 @@
 module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   moduleNameMapper: {
-    '^meteor/(.*):(.*)$':
-      '<rootDir>/frog/.meteor/local/build/programs/server/packages/\1_\2',
-    '^meteor/(.*)$':
-      '<rootDir>/frog/.meteor/local/build/programs/server/packages/\1',
-    '^meteor/(.*)$':
-      '<rootDir>/frog/.meteor/local/build/programs/web.browser/packages/\1'
-  },
-  moduleDirectories: [
-    'node_modules',
-    '<rootDir>/frog/.meteor/local/build/programs/server/packages',
-    '<rootDir>/frog/.meteor/local/build/programs/web.browser/packages/',
-    '.meteor/local/build/programs/server/packages',
-    '.meteor/local/build/programs/web.browser/packages/'
-  ]
+    '^meteor/(.*):(.*)':
+      '<rootDir>/frog/.meteor/local/build/programs/server/packages/$1_$2',
+    '^meteor/(.*)':
+      '<rootDir>/frog/.meteor/local/build/programs/server/packages/$1',
+  }
 };
