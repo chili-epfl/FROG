350b4e1a4d5d8ee36530480068a531643c7673af
lfaucon <lpfaucon@gmail.com>
2017-06-20 20:04:04 +0200

    Fix type errors in dataStructureTools

--- frog-utils/src/dataStructureTools.js
+++ frog-utils/src/dataStructureTools.js
@@ -7,16 +7,16 @@ import type {
   socialStructureT,
   activityDataT
 } from './types';
-import { getAttributeKeys, focusStudent } from './socstructTools';
+import { focusStudent } from './socstructTools';
 
 const mergeConfig = (
   configData: Object,
-  payload?: dataUnitStructT
+  payload: dataUnitStructT
 ): dataUnitStructT => ({
-  data: payload && payload.data,
+  data: payload.data,
   config: {
     ...configData,
-    ...(payload && payload.config)
+    ...payload.config
   }
 });
 
@@ -35,44 +35,44 @@ export const extractUnit = (
   activityStructure: structureDefT,
   attributeValue: string,
   socialStructure?: socialStructureT
-): ?dataUnitStructT => {
-  if (!data) {
-    return;
-  }
-
+): dataUnitStructT => {
   if (data.structure === 'all') {
     return data.payload.all;
-  }
-
-  if (data.structure === 'individual') {
+  } else if (data.structure === 'individual') {
     if (activityStructure === 'individual') {
       return data.payload[attributeValue];
-    } else {
-      throw 'Cannot provide individually mapped product to an activity above plane 1';
     }
-  }
-
-  if (
-    typeof data.structure === 'object' &&
-    typeof data.structure.groupingKey === 'string'
-  ) {
+    throw 'Cannot provide individually mapped product to an activity above plane 1';
+  } else {
     if (typeof activityStructure === 'object') {
-      if (getAttributeKeys(data).indexOf(data.structure.groupingKey)) {
+      if (data.structure.groupingKey !== activityStructure.groupingKey) {
+        throw 'Incompatible grouping keys';
+      }
+      if (data.payload[attributeValue] !== undefined) {
         return data.payload[attributeValue];
       } else {
-        throw 'Grouping key not found in activityData';
+        throw 'Grouping value not found in activityData';
       }
-    } else if (activityStructure === 'individual' && socialStructure) {
-      const studentFocused = focusStudent(socialStructure);
-      if (!studentFocused[attributeValue]) {
-        Error('Student not in social structure');
-      }
-      if (!studentFocused[attributeValue][data.structure.groupingKey]) {
-        Error('Student not in group matching groupingKey');
-      }
-      const grp = studentFocused[attributeValue][data.structure.groupingKey];
-      return data.payload[grp];
     }
+
+    if (activityStructure === 'individual') {
+      if (!socialStructure) {
+        throw 'Cannot map group product to individual without a social structure';
+      }
+      const studentAttributes = focusStudent(socialStructure)[attributeValue];
+      if (!studentAttributes) {
+        throw 'Student not in social structure';
+      }
+      if (studentAttributes[data.structure.groupingKey] !== undefined) {
+        const grp = studentAttributes[data.structure.groupingKey];
+        if (data.payload[grp] !== undefined) {
+          return data.payload[grp];
+        }
+        throw 'Grouping value not found in activityData';
+      }
+      throw 'Student not in group matching groupingKey';
+    }
+    throw 'Cannot provide group mapped product to a full-class activity';
   }
 };
 
