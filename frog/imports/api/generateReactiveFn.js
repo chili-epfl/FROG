f01cf34eec56115b8ae2019eedd1bf5eb0d16cfc
Stian Håklev <shaklev@gmail.com>
2017-06-21 15:53:10 +0200

    Updating all paths to take [] as well

--- frog/imports/api/generateReactiveFn.js
+++ frog/imports/api/generateReactiveFn.js
@@ -2,7 +2,9 @@
 
 import { uuid } from 'frog-utils';
 
-const cleanPath = (defPath, rawPath = []) => {
+type rawPathT = string | string []
+
+const cleanPath = (defPath: [], rawPath: rawPathT = []): string[] => {
   const newPath = rawPath.constructor !== Array ? [rawPath] : rawPath;
   return [...defPath, ...newPath];
 };
@@ -15,69 +17,69 @@ class Doc {
     this.doc = doc;
     this.path = path || [];
   }
-  listPrepend(newVal: any, path: string) {
+  listPrepend(newVal: any, path: rawPathT) {
     this.doc.submitOp({ p: [...cleanPath(this.path, path), 0], li: newVal });
   }
-  listAppend(newVal: any, path: string) {
+  listAppend(newVal: any, path: rawPathT) {
     this.doc.submitOp({
       p: [...cleanPath(this.path, path), 999999],
       li: newVal
     });
   }
-  listInsert(newVal: any, path: string) {
+  listInsert(newVal: any, path: rawPathT) {
     this.doc.submitOp({
       p: cleanPath(this.path, path),
       li: newVal
     });
   }
-  listDel(oldVal: any, path: string) {
+  listDel(oldVal: any, path: rawPathT) {
     this.doc.submitOp({
       p: cleanPath(this.path, path),
       ld: oldVal
     });
   }
-  listReplace(oldVal: any, newVal: any, path: string) {
+  listReplace(oldVal: any, newVal: any, path: rawPathT) {
     this.doc.submitOp({
       p: cleanPath(this.path, path),
       ld: oldVal,
       li: newVal
     });
   }
-  numIncr(incr: number, path: string) {
+  numIncr(incr: number, path: rawPathT) {
     this.doc.submitOp({ p: cleanPath(this.path, path), na: incr });
   }
-  objInsert(newVal: Object, path: string) {
+  objInsert(newVal: Object, path: rawPathT) {
     this.doc.submitOp({ p: cleanPath(this.path, path), oi: newVal });
   }
-  keyedObjInsert(newVal: Object, path: string) {
+  keyedObjInsert(newVal: Object, path: rawPathT) {
     const id = uuid();
     this.doc.submitOp({
       p: cleanPath(this.path, [path, id]),
       oi: { id, ...newVal }
     });
   }
-  objDel(oldVal: Object, path: string) {
+  objDel(oldVal: Object, path: rawPathT) {
     this.doc.submitOp({ p: cleanPath(this.path, path), od: oldVal });
   }
-  objReplace(oldVal: Object, newVal: Object, path: string) {
+  objReplace(oldVal: Object, newVal: Object, path: rawPathT) {
     this.doc.submitOp({
       p: cleanPath(this.path, path),
       od: oldVal,
       oi: newVal
     });
   }
-  objSet(newVal: Object, path: string) {
+  objSet(newVal: Object, path: rawPathT) {
     this.doc.submitOp({
       p: [...this.path, path],
       oi: newVal
     });
   }
-  specialize(rawPath: string | string[]) {
+  specialize(rawPath: rawPathT) {
     const newPath = typeof rawPath === 'string' ? [rawPath] : rawPath;
     return new Doc(this.doc, [...this.path, ...newPath]);
   }
 
-  specializeData(path: string | string[], data: Object) {
+  specializeData(path: rawPathT, data: Object) {
     if (typeof path === 'string') {
       return data[[path]];
     }
