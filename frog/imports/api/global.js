fd988b2e0e7fbb1b210d22834c3a5dad2f4843d2
lfaucon <lpfaucon@gmail.com>
2017-06-21 15:06:33 +0200

    Fix remaining flow errors in the engine

--- frog/imports/api/global.js
+++ frog/imports/api/global.js
@@ -1,13 +1,15 @@
+// @flow
+
 import { Mongo } from 'meteor/mongo';
 
 export const GlobalSettings = new Mongo.Collection('global_settings');
 
-export const getGlobalSetting = key => {
+export const getGlobalSetting = (key: string) => {
   const ret = GlobalSettings.findOne(1);
   return ret ? ret[key] : null;
 };
 
 export const getGlobalSettings = () => GlobalSettings.findOne(1) || {};
 
-export const setGlobalSetting = (k, v) =>
+export const setGlobalSetting = (k: string, v: any) =>
   GlobalSettings.upsert(1, { $set: { [k]: v } });
