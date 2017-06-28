01b420f46d83deaab1f3fa1aced35b8e30646381
Stian Håklev <shaklev@gmail.com>
2017-06-28 15:21:33 +0200

    Jest works (#288)

--- package-scripts.js
+++ package-scripts.js
@@ -6,8 +6,8 @@ module.exports = {
       'rm -rf ./tmp/ && mkdir -p ./tmp/frog && git clone . ./tmp/frog && cd ./tmp/frog && docker build -t frogtest . && docker run frogtest',
     fix: 'eslint --fix -c .eslintrc-prettier.js --ext .js,.jsx .',
     eslintTest: 'eslint -c .eslintrc-prettier.js --ext .js,.jsx .',
-    jestWatch: 'jest --watch',
     jest: 'jest',
+    jestWatch: 'jest --watch',
     flowTest: 'flow',
     nightmareTest: 'node nightmare/test.js'
   },
