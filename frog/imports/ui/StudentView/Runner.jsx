343b5f6a633a223a5bc31302ed45967d3dd74c0d
Stian Håklev <shaklev@gmail.com>
2017-07-03 10:54:40 +0200

    Fix

--- frog/imports/ui/StudentView/Runner.jsx
+++ frog/imports/ui/StudentView/Runner.jsx
@@ -26,63 +26,63 @@ const Runner = ({ activity, object, single }) => {
     user: Meteor.userId()
   });
 
-  if (object) {
-    const socStructure = focusStudent(object.socialStructure);
-    const studentSoc = socStructure[Meteor.userId()];
-
-    let groupingValue;
-    if (studentSoc && activity.groupingKey) {
-      groupingValue = studentSoc[activity.groupingKey];
-    } else if (activity.plane === 3) {
-      groupingValue = 'all';
-    } else {
-      groupingValue = Meteor.userId();
-    }
-    const reactiveId = activity._id + '/' + groupingValue;
-
-    const RunComp = activityType.ActivityRunner;
-    RunComp.displayName = activity.activityType;
-    const ActivityToRun = ReactiveHOC(
-      cloneDeep(activityType.dataStructure),
-      reactiveId
-    )(RunComp);
-
-    const groupingStr = activity.groupingKey ? activity.groupingKey + '/' : '';
-    let title = '(' + groupingStr + groupingValue + ')';
-    if (activity.plane === 1) {
-      title = `(individual/${Meteor.user().username})`;
-    }
-
-    const config = activity.data;
-    const activityStructure = doGetInstances(activity, object).structure;
-
-    const activityData = getMergedExtractedUnit(
-      config,
-      object.activityData,
-      activityStructure,
-      groupingValue,
-      object.socialStructure
-    );
-
-    const activity = (
-      <ActivityToRun
-        activityData={activityData}
-        userInfo={{ name: Meteor.user().username, id: Meteor.userId() }}
-        logger={logger}
-      />
-    );
-
-    if (single) {
-      return activity;
-    } else {
-      return (
-        <MosaicWindow title={activity.title + ' ' + title}>
-          {activity}
-        </MosaicWindow>
-      );
-    }
+  if (!object) {
+    return null;
   }
-  return null;
+  const socStructure = focusStudent(object.socialStructure);
+  const studentSoc = socStructure[Meteor.userId()];
+
+  let groupingValue;
+  if (studentSoc && activity.groupingKey) {
+    groupingValue = studentSoc[activity.groupingKey];
+  } else if (activity.plane === 3) {
+    groupingValue = 'all';
+  } else {
+    groupingValue = Meteor.userId();
+  }
+  const reactiveId = activity._id + '/' + groupingValue;
+
+  const RunComp = activityType.ActivityRunner;
+  RunComp.displayName = activity.activityType;
+  const ActivityToRun = ReactiveHOC(
+    cloneDeep(activityType.dataStructure),
+    reactiveId
+  )(RunComp);
+
+  const groupingStr = activity.groupingKey ? activity.groupingKey + '/' : '';
+  let title = '(' + groupingStr + groupingValue + ')';
+  if (activity.plane === 1) {
+    title = `(individual/${Meteor.user().username})`;
+  }
+
+  const config = activity.data;
+  const activityStructure = doGetInstances(activity, object).structure;
+
+  const activityData = getMergedExtractedUnit(
+    config,
+    object.activityData,
+    activityStructure,
+    groupingValue,
+    object.socialStructure
+  );
+
+  const Torun = (
+    <ActivityToRun
+      activityData={activityData}
+      userInfo={{ name: Meteor.user().username, id: Meteor.userId() }}
+      logger={logger}
+    />
+  );
+
+  if (single) {
+    return Torun;
+  } else {
+    return (
+      <MosaicWindow title={activity.title + ' ' + title}>
+        {Torun}
+      </MosaicWindow>
+    );
+  }
 };
 
 export default createContainer(({ activityId, single }) => {
