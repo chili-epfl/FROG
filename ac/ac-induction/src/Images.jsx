390203d17e33f847170bd21339120013afa6a6d0
lfaucon <lpfaucon@gmail.com>
2017-07-06 13:57:36 +0200

    wip

--- ac/ac-induction/src/Images.jsx
+++ ac/ac-induction/src/Images.jsx
@@ -1,72 +1,45 @@
 // @flow
 
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 45%;
+  width: 100%;
+  display: inline-block;
+`
+
+const Img = ({ url }) => <img
+  style={{
+    width: '50px',
+    height: '95%'
+  }}
+  src={url}
+  alt={''}
+/>
+
+const Rectangle = ({ color }) => <div
+  style={{
+    width: '50%',
+    height: '100%',
+    background: color
+  }}
+/>
 
 const Images = (props: { imgTrue: String, imgFalse: String }) => {
   if (!props.imgTrue || !props.imgFalse) {
     return <div>{"Images's URI not found"}</div>;
   } else {
     return (
-      <div
-        style={{
-          height: '100%',
-          width: '50%',
-          display: 'flex',
-          flexDirection: 'column',
-          overflow: 'hidden'
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            width: '100%',
-            height: '45%'
-          }}
-        >
-          <div
-            style={{
-              width: '5%',
-              height: '100%',
-              background: 'green'
-            }}
-          />
-          <img
-            style={{
-              width: '90%',
-              height: '95%',
-              margin: 'auto'
-            }}
-            src={props.imgTrue}
-            alt={''}
-          />
-        </div>
-        <br />
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            width: '100%',
-            height: '45%'
-          }}
-        >
-          <div
-            style={{
-              width: '5%',
-              height: '100%',
-              background: 'red'
-            }}
-          />
-          <img
-            style={{
-              width: '90%',
-              height: '95%',
-              margin: 'auto'
-            }}
-            src={props.imgFalse}
-            alt={''}
-          />
-        </div>
+      <div>
+        <Container>
+          <Rectangle color={'#00ff00'} />
+          <Img url={props.imgTrue} />
+        </Container>
+        <Container>
+          <Rectangle color={'#ff0000'} />
+          <Img url={props.imgFalse} />
+        </Container>
       </div>
     );
   }
