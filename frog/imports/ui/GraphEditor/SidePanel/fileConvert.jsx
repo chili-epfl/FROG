b82d1dfcbe91ea4bbcb262e60d7ac69f55ef140e
agentromain <romain.artru@epfl.ch>
2017-07-07 19:10:27 +0200

    wip : the URL you get leads to a html page containing only this image but it's not possible to use it as a web URL as in ac-induction

--- frog/imports/ui/GraphEditor/SidePanel/fileConvert.jsx
+++ frog/imports/ui/GraphEditor/SidePanel/fileConvert.jsx
@@ -4,6 +4,8 @@ import React, { Component } from 'react';
 import Form from 'react-jsonschema-form';
 import CopyToClipboard from 'react-copy-to-clipboard';
 
+import { Files, addFile } from '/imports/api/activities';
+
 class fileConvert extends Component {
   state: { url: string, copied: boolean, display: boolean };
 
@@ -12,54 +14,15 @@ class fileConvert extends Component {
     this.state = { url: '', copied: false, display: false };
   }
 
-  onChange = (e: { formData: { file: string } }) =>
-    this.setState({ url: e.formData.file, copied: false });
-
+  onChange = (e: { formData: { file: string } }) =>{
+    const [dataType, nameDirty,] = e.formData.file.split(';');
+    const name = nameDirty.split('=')[1];
+    Promise.resolve().then(addFile(name, e.formData.file));
+    this.setState({ url: window.location.host+'/file/'+name, copied: false });
+}
   onCopy = () => this.setState({ copied: true });
 
   render() {
-    let divForm = null;
-    if (!this.state.display) {
-      divForm = <div />;
-    } else
-      divForm = (
-        <div>
-          <Form
-            schema={{
-              type: 'object',
-              properties: {
-                file: {
-                  type: 'string',
-                  title: 'Upload an image to get its URI'
-                }
-              }
-            }}
-            uiSchema={{
-              file: {
-                'ui:widget': 'file'
-              }
-            }}
-            onChange={this.onChange}
-            liveValidate
-          >
-            <div />
-          </Form>
-          <h4>Submitted file as data URL</h4>
-          <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <CopyToClipboard
-              text={this.state.url}
-              onCopy={() => this.setState({ copied: true })}
-              style={{ height: '39px' }}
-            >
-              <button>Copy</button>
-            </CopyToClipboard>
-            <pre style={{ width: '100%' }}>
-              {' ' + this.state.url.toString()}
-            </pre>
-          </div>
-        </div>
-      );
-
     return (
       <div>
         <div style={{ width: '100%', height: '2px', background: 'black' }} />
@@ -74,9 +37,42 @@ class fileConvert extends Component {
             {this.state.display ? 'Close' : 'Open'}
           </button>
         </div>
-
-        {divForm}
-
+        {this.state.display &&
+          <div>
+            <Form
+              schema={{
+                type: 'object',
+                properties: {
+                  file: {
+                    type: 'string',
+                    title: 'Upload an image to get its URI'
+                  }
+                }
+              }}
+              uiSchema={{
+                file: {
+                  'ui:widget': 'file'
+                }
+              }}
+              onChange={this.onChange}
+              liveValidate
+            >
+              <div />
+            </Form>
+            <h4>{'Submitted file as data URL'}</h4>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+              <CopyToClipboard
+                text={this.state.url}
+                onCopy={() => this.setState({ copied: true })}
+                style={{ height: '39px' }}
+              >
+                <button>Copy</button>
+              </CopyToClipboard>
+              <pre style={{ width: '100%' }}>
+                {' ' + this.state.url.toString()}
+              </pre>
+            </div>
+          </div>}
       </div>
     );
   }
