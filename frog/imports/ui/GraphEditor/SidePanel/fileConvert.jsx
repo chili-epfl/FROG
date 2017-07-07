1061deca5989cdb6eac09deffe3c222bac4f0ee8
agentromain <romain.artru@epfl.ch>
2017-07-07 15:38:15 +0200

    added the copy button to get the dataURI

--- frog/imports/ui/GraphEditor/SidePanel/fileConvert.jsx
+++ frog/imports/ui/GraphEditor/SidePanel/fileConvert.jsx
@@ -2,26 +2,20 @@
 
 import React, { Component } from 'react';
 import Form from 'react-jsonschema-form';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 class fileConvert extends Component {
-  state: { url: string };
+  state: { url: string, copied: boolean };
 
   constructor(props: {}) {
     super(props);
-    this.state = { url: '' };
-  }
-
-  processFile(files: Array<any>) {
-    const f = files[0];
-    return new Promise(resolve => {
-      const reader = new FileReader();
-      reader.onload = event => resolve(event.target.result);
-      reader.readAsDataURL(f);
-    });
+    this.state = { url: '', copied: false };
   }
 
   onChange = (e: { formData: { file: string } }) =>
-    this.setState({ url: e.formData.file });
+    this.setState({ url: e.formData.file, copied: false });
+
+    onCopy = () => this.setState({copied: true});
 
   render() {
     return (
@@ -46,10 +40,17 @@ class fileConvert extends Component {
           <div />
         </Form>
         <h4>Submitted file as data URL</h4>
-        <pre>{this.state.url.toString()}</pre>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+        <CopyToClipboard text={this.state.url}
+          onCopy={() => this.setState({copied: true})}
+        style={{height: '39px'}}>
+          <button>Copy</button>
+        </CopyToClipboard>
+        <pre style={{width: '100%'}}>{' '+this.state.url.toString()}</pre>
+        </div>
       </div>
     );
   }
 }
 
-export default UploadFile;
+export default fileConvert;
