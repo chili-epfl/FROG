c715ce9f74bb9cd8b57921d5ff301b67f6516ec8
agentromain <romain.artru@epfl.ch>
2017-07-13 17:17:33 +0200

    created file Validator

---
+++ frog/imports/ui/GraphEditor/Validator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import {Activities, Operators, Connections} from '../../api/activities'
+
+export default connect(
+  ({store: {graphId: string}}) =>
+    <svg >
+
+    </svg>
+);
