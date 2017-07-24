de3815a134601a17ceccd3fff79ecaab6f8d206d
agentromain <romain.artru@epfl.ch>
2017-07-24 10:06:22 +0200

    changed the long description display method

--- frog/imports/ui/GraphEditor/SidePanel/ListComponent.jsx
+++ frog/imports/ui/GraphEditor/SidePanel/ListComponent.jsx
@@ -3,41 +3,61 @@
 import React, { Component } from 'react';
 
 class ListComponent extends Component {
-  state: { over: boolean };
+  state: { longDesc: boolean, over1: boolean, over2: boolean };
   constructor(props: any) {
     super(props);
     this.state = {
-      over: false
+      longDesc: false,
+      over1: false,
+      over2: false
     };
   }
 
   render() {
     const obj = this.props.object;
 
-    const onOver = () => this.setState({ over: true });
-    const onOut = () => this.setState({ over: false });
+    const click = () => this.setState({ longDesc: !this.state.longDesc });
 
     return (
-      <div value="titi" className="list-group-item">
+      <div className="list-group-item">
         <h5 style={{ fontWeight: 'bold' }}>
           {obj.meta.name}
         </h5>
-        {!this.state.over &&obj.meta.shortDesc}
-        {this.state.over &&obj.meta.description}
-        <div
-          onMouseOver={onOver}
-          onMouseOut={onOut}
+        <button
           value={obj.id}
           style={{
             position: 'absolute',
-            left: '0px',
-            top: '0px',
-            backgroundColor: this.state.over ? '#0066CC' : '#FFFFFF',
-            opacity: this.state.over ? '0.3' : '0',
-            width: '100%',
-            height: '100%'
+            right: '2px',
+            top: '4px',
+            width: '13%',
+            height: '35px'
           }}
+        >
+          {' '}Choose{' '}
+        </button>
+        <button
+          style={{
+            position: 'absolute',
+            right: '2px',
+            top: '38px',
+            width: '13%',
+            height: '35px'
+          }}
+          className={
+            this.state.longDesc
+              ? 'glyphicon glyphicon-minus'
+              : 'glyphicon glyphicon-plus'
+          }
+          onClick={click}
         />
+        {!this.state.longDesc &&
+          <div style={{ width: '87%' }}>
+            {obj.meta.shortDesc}{' '}
+          </div>}
+        {this.state.longDesc &&
+          <div style={{ width: '87%' }}>
+            {obj.meta.description}{' '}
+          </div>}
       </div>
     );
   }
