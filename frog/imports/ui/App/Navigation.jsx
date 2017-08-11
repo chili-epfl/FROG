5798f6a1e4573ad31de30700d6d63a3081931189
agentromain <romain.artru@epfl.ch>
2017-08-11 18:54:36 +0200

    wip

--- frog/imports/ui/App/Navigation.jsx
+++ frog/imports/ui/App/Navigation.jsx
@@ -2,20 +2,34 @@
 
 import React from 'react';
 import { Nav, NavItem } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+
+import { Page } from './index';
 
 export default ({
   apps,
   currentApp,
-  username
+  username,
 }: {
   apps: Object,
   currentApp: string,
-  username: string
+  username: string,
 }) =>
-  <Nav bsStyle="pills" activeKey={currentApp}>
-    {Object.keys(apps).map(app =>
-      <NavItem key={app} eventKey={app} href={'/#/' + username + '/' + app}>
-        {apps[app]}
-      </NavItem>
-    )}
-  </Nav>;
+  <Router>
+    <Nav bsStyle="pills" activeKey={currentApp}>
+      {Object.keys(apps).map(app =>
+        <div key={app}>
+          <Link
+            to={'/' + username + '/' + app.split(' ').reduce((acc, x) => (acc += x), '') + '/true'}
+          >
+            <NavItem eventKey={app}>
+              {apps[app]}
+            </NavItem>
+          </Link>
+          <Redirect path="/:username/:currentApp/:ready" component={Page} />
+        </div>,
+      )}
+    </Nav>
+  </Router>;
+
+// ////////////////////route to nothing
--- frog/imports/ui/App/index.js
+++ frog/imports/ui/App/index.js
@@ -7,20 +7,20 @@ import { Accounts } from 'meteor/accounts-base';
 import sharedbClient from 'sharedb/lib/client';
 import ReconnectingWebSocket from 'reconnectingwebsocket';
 import { every } from 'lodash';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Body from './Body.jsx';
 import Navigation from './Navigation';
 
 const DEFAULT_PASSWORD = '123456';
-const connectWithDefaultPwd = username =>
-  Meteor.loginWithPassword(username, DEFAULT_PASSWORD);
+const connectWithDefaultPwd = username => Meteor.loginWithPassword(username, DEFAULT_PASSWORD);
 
 const apps = {
   home: 'Home',
   admin: 'Admin',
   graph: 'Graph Editor',
   teacher: 'Teacher View',
-  student: 'Student View'
+  student: 'Student View',
 };
 
 const socket = new ReconnectingWebSocket('ws://localhost:3002');
@@ -28,71 +28,77 @@ export const connection = new sharedbClient.Connection(socket);
 window.connection = connection;
 
 // App component - represents the whole app
-export default class App extends Component {
-  state: { app: string, username: string };
+// export default class App extends Component {
+//   state: { app: string, username: string };
+//
+//   constructor() {
+//     super();
+//     this.state = { app: 'home', username: '' };
+//     Meteor.subscribe('userData', {
+//       onReady: () => {
+//         this.handleNewHash();
+//         window.addEventListener('hashchange', this.handleNewHash, false);
+//       }
+//     });
+//   }
+//
+//   handleNewHash = () => {
+//     const [, username, location] = window.location.hash.split('/');
+//     const loggedInUsername =
+//       Meteor.userId() && Meteor.users.findOne(Meteor.userId()).username;
+//     if (username && username !== loggedInUsername) {
+//       if (!Meteor.users.findOne({ username })) {
+//         Accounts.createUser({ username, password: DEFAULT_PASSWORD }, () =>
+//           connectWithDefaultPwd(username)
+//         );
+//       } else {
+//         connectWithDefaultPwd(username);
+//       }
+//     }
+//     this.setState({ username });
+//     if (username !== 'teacher') {
+//       this.setState({ app: 'student' });
+//     } else if (location && apps[location]) {
+//       this.setState({ app: location });
+//     }
+//   };
+/* <PageContainer
+    username={this.state.username === undefined ? '' : this.state.username}
+    apps={apps}
+    currentApp={this.state.app === undefined ? '' : this.state.app}
+  /> */
 
-  constructor() {
-    super();
-    this.state = { app: 'home', username: '' };
-    Meteor.subscribe('userData', {
-      onReady: () => {
-        this.handleNewHash();
-        window.addEventListener('hashchange', this.handleNewHash, false);
-      }
-    });
-  }
+export default () =>{
+  console.log('Dans l\'App');
+  return (<Router>
+    <div>
+      <Route path="/:username" component={PageContainer} />
+      <Route path="/:username/:app" component={PageContainer} />
+    </div>
+  </Router>);
+};
 
-  handleNewHash = () => {
-    const [, username, location] = window.location.hash.split('/');
-    const loggedInUsername =
-      Meteor.userId() && Meteor.users.findOne(Meteor.userId()).username;
-    if (username && username !== loggedInUsername) {
-      if (!Meteor.users.findOne({ username })) {
-        Accounts.createUser({ username, password: DEFAULT_PASSWORD }, () =>
-          connectWithDefaultPwd(username)
-        );
-      } else {
-        connectWithDefaultPwd(username);
-      }
-    }
-    this.setState({ username });
-    if (username !== 'teacher') {
-      this.setState({ app: 'student' });
-    } else if (location && apps[location]) {
-      this.setState({ app: location });
-    }
-  };
+// const User = ({match}) => {
+//   <Router>
+//     <Route path='/:app' component={}/>
+//   </Router>
+// }
 
-  render() {
-    return (
-      <PageContainer
-        username={this.state.username === undefined ? '' : this.state.username}
-        apps={apps}
-        currentApp={this.state.app === undefined ? '' : this.state.app}
-      />
-    );
-  }
-}
-
-const Page = (props: {
-  username: string,
-  currentApp: string,
-  apps: {},
-  ready: boolean
-}) => {
-  if (!props.ready) return <div id="app" />;
+export const Page = ({ match }) => {
+  console.log('Dans la page');
+  if (!match.params.ready) return <div id="app" />;
   return (
     <div id="app">
-      {props.username === 'teacher' &&
+      {match.params.username === 'teacher' &&
         <div id="header">
           <Navigation
-            username={props.username}
-            apps={props.apps}
-            currentApp={props.currentApp}
+            username={match.params.username}
+            apps={apps}
+            currentApp={match.params.currentApp ? match.params.currentApp : 'Home'}
           />
         </div>}
       <div id="body">
-        <Body app={props.currentApp} />
+        <Body app={match.params.currentApp} />
       </div>
     </div>
   );
@@ -103,15 +109,20 @@ const setupSubscriptions = (collections: string[]) => {
   return every(subscriptions.map(x => x.ready()), Boolean);
 };
 
-const PageContainer = createContainer((props: { username: string }) => {
+const PageContainer = createContainer(({ match }) => {
+  const username = match.params.username;
+  const app = match.params.app ? match.params.app : 'Home';
+  console.log('Dans le PageContainer');
   let ready = setupSubscriptions([
+    'userData',
     'activity_data',
     'logs',
     'activities',
     'objects',
-    'sessions'
+    'sessions',
   ]);
-  if (props.username === 'teacher') {
+
+  if (match.params.username === 'teacher') {
     ready =
       ready &&
       setupSubscriptions([
@@ -120,8 +131,19 @@ const PageContainer = createContainer((props: { username: string }) => {
         'global_settings',
         'graphs',
         'products',
-        'uploads'
+        'uploads',
       ]);
   }
-  return { ...props, ready };
+  const loggedInUsername = Meteor.userId() && Meteor.users.findOne(Meteor.userId()).username;
+  if (username && username !== loggedInUsername) {
+    if (!Meteor.users.findOne({ username })) {
+      Accounts.createUser({ username, password: DEFAULT_PASSWORD }, () =>
+        connectWithDefaultPwd(username),
+      );
+    } else {
+      connectWithDefaultPwd(username);
+    }
+  }
+
+  return { match: { params: { username, currentApp: match.params.app, ready } } };
 }, Page);
