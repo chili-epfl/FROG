f31594fcc45e9fecd573c4029d9f7283e9ffddd5
Stian Håklev <shaklev@gmail.com>
2017-08-14 15:06:24 +0200

    WIP

--- initial_setup_wo_meteor.sh
+++ initial_setup_wo_meteor.sh
@@ -6,14 +6,15 @@ FROG=`pwd`
 YARN=yarn
 which yarn | grep -qw yarn || npm install yarn@0.28.4
 which yarn | grep -qw yarn || YARN=$FROG/node_modules/.bin/yarn
-$YARN install
 
 mkdir frog/node_modules
+
+$YARN install
+
 
 cd frog-utils
 ln -s $FROG/.babelrc . 2>/dev/null
 npm run build &
-ln -s `pwd` $FROG/frog/node_modules/
 
 # install activities and operators packages
 for dir in $FROG/ac/ac-*/ $FROG/op/op-*/
@@ -21,9 +22,9 @@ do
     cd $dir
     ln -s $FROG/.babelrc . 2>/dev/null
     npm run build &
-    ln -s $dir $FROG/frog/node_modules/
 done
 
+cd $FROG/frog
 ln -s $FROG/node_modules/* node_modules/ 2>/dev/null
 ln -s $FROG/.babelrc . 2>/dev/null
 
