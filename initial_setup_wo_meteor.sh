049ed190a1af4c445ab0fad9b9145216cb6c6e54
Stian Håklev <shaklev@gmail.com>
2017-08-14 14:57:27 +0200

    WIP

--- initial_setup_wo_meteor.sh
+++ initial_setup_wo_meteor.sh
@@ -4,32 +4,26 @@ shopt -s dotglob
 
 FROG=`pwd`
 YARN=yarn
-which yarn | grep -qw yarn || npm install yarn@0.24.6
+which yarn | grep -qw yarn || npm install yarn@0.28.4
 which yarn | grep -qw yarn || YARN=$FROG/node_modules/.bin/yarn
 $YARN install
 
-# install package frog-utils
-cd $FROG/frog-utils
-mkdir -p node_modules
-ln -s $FROG/node_modules/* node_modules/ 2>/dev/null
+mkdir frog/node_modules
+
+cd frog-utils
 ln -s $FROG/.babelrc . 2>/dev/null
-$YARN install
+npm run build &
+ln -s `pwd` $FROG/frog/node_modules/
 
 # install activities and operators packages
 for dir in $FROG/ac/ac-*/ $FROG/op/op-*/
 do
     cd $dir
-    mkdir -p node_modules
-    ln -s $FROG/node_modules/* node_modules/ 2>/dev/null
-    ln -s $FROG/frog-utils node_modules/ 2>/dev/null
     ln -s $FROG/.babelrc . 2>/dev/null
-    $YARN install
     npm run build &
+    ln -s $dir $FROG/frog/node_modules/
 done
 
-# links all packages to the frog/ meteor project
-cd $FROG/frog
-mkdir -p node_modules
 ln -s $FROG/node_modules/* node_modules/ 2>/dev/null
 ln -s $FROG/.babelrc . 2>/dev/null
 
