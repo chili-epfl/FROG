a15aae8248d8d26af5aa68575cb256234c7382b1
Stian Håklev <shaklev@gmail.com>
2017-08-15 09:23:06 +0200

    initial

--- initial_setup.sh
+++ initial_setup.sh
@@ -2,8 +2,4 @@
 
 ./initial_setup_wo_meteor.sh
 
-cd frog
-METEOR=meteor
-which meteor | grep -qw meteor || METEOR=/usr/local/bin/meteor
-$METEOR npm install -g yarn 
-$METEOR yarn install --ignore-engines
+ln -s node_modules/* frog/node_modules/
