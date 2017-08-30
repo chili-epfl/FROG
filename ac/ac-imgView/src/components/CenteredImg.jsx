d430a569b6dce06740791f71b7c278830f5a6a70
lfaucon <lpfaucon@gmail.com>
2017-08-30 10:04:39 +0200

    CSS fix (final)

--- ac/ac-imgView/src/components/CenteredImg.jsx
+++ ac/ac-imgView/src/components/CenteredImg.jsx
@@ -5,18 +5,19 @@ import { ImageReload } from 'frog-utils';
 import FlexView from 'react-flexview';
 
 const CenteredImgComp = ({ url }: { url: string }) =>
-  <FlexView
-    hAlignContent="center"
-    vAlignContent="center"
-    height="100%"
-    width="100%"
-  >
-    <ImageReload
-      alt=""
-      src={url}
-      style={{ margin: 'auto', maxHeight: '100%', maxWidth: '100%' }}
-    />
-  </FlexView>;
+  <ImageReload
+    alt=""
+    src={url}
+    style={{
+      position: 'absolute',
+      maxWidth: '100%',
+      maxHeight: '100%',
+      left: '50%',
+      top: '50%',
+      transform: 'translate(-50%, -50%)',
+      padding: '5%'
+    }}
+  />
 
 CenteredImgComp.displayName = 'CenteredImg';
 export default CenteredImgComp;
