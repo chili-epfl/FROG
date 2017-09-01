d22bbde48beb56188b8ed12f69cab8613a0199df
Louis Faucon <lfaucon@users.noreply.github.com>
2017-09-01 09:25:25 +0200

    Countdown uses his own createContainer (#449)

--- frog/imports/ui/StudentView/SessionBody.jsx
+++ frog/imports/ui/StudentView/SessionBody.jsx
@@ -2,10 +2,9 @@
 
 import React from 'react';
 import { Mosaic } from 'react-mosaic-component';
-import styled from 'styled-components';
-import { msToString } from 'frog-utils';
 
 import Runner from './Runner';
+import Countdown from './Countdown';
 
 const getInitialState = (activities, d = 1) => {
   const n = Math.floor(activities.length / 2);
@@ -18,76 +17,26 @@ const getInitialState = (activities, d = 1) => {
       };
 };
 
-const Countdown = ({ session, currentTime }) => {
-  const secondsLeft =
-    session.countdownStartTime > 0
-      ? Math.round(
-          session.countdownStartTime + session.countdownLength - currentTime
-        )
-      : session.countdownLength;
+const SessionBody = ({ session }: { session: Object }) => {
+  let Body = null;
+  if (!session.openActivities || session.openActivities.length === 0) {
+    Body = <h1>No Activity</h1>;
+  } else if (session.openActivities.length === 1) {
+    Body = <Runner activityId={session.openActivities[0]} single />;
+  } else {
+    Body = (
+      <Mosaic
+        renderTile={activityId => <Runner activityId={activityId} />}
+        initialValue={getInitialState(session.openActivities)}
+      />
+    );
+  }
   return (
-    <div>
-      {session.countdownStartTime !== -1 &&
-        <CountdownDiv>
-          <h4>
-            {msToString(secondsLeft)}
-          </h4>
-        </CountdownDiv>}
+    <div style={{ height: '100%' }}>
+      <Countdown session={session} />
+      {Body}
     </div>
   );
 };
 
-const SessionBody = ({
-  session,
-  currentTime
-}: {
-  session: Object,
-  currentTime: number
-}) => {
-  if (!session.openActivities || session.openActivities.length === 0) {
-    return (
-      <div style={{ height: '100%' }}>
-        <Countdown session={session} currentTime={currentTime} />
-        <h1>NO ACTIVITY</h1>
-      </div>
-    );
-  }
-  if (session.openActivities.length === 1) {
-    return (
-      <div style={{ height: '100%' }}>
-        <Countdown session={session} currentTime={currentTime} />
-        <Runner
-          activityId={session.openActivities[0]}
-          sessionId={session._id}
-          single
-        />
-      </div>
-    );
-  } else {
-    return (
-      <div style={{ height: '100%' }}>
-        <Countdown session={session} currentTime={currentTime} />
-        <Mosaic
-          renderTile={activityId =>
-            <Runner activityId={activityId} sessionId={session._id} />}
-          initialValue={getInitialState(session.openActivities)}
-        />
-      </div>
-    );
-  }
-};
-
 export default SessionBody;
-
-const CountdownDiv = styled.div`
-  border: solid 5px #aa0000;
-  background-color: #ff0000;
-  border-radius: 30%;
-  width: fit-content;
-  min-width: 50px;
-  height: 50px;
-  position: absolute;
-  right: 5px;
-  text-align: center;
-  z-index: 1;
-`;
