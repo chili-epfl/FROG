d9638bc8af3901da6aa55c69e4cb40060e88563a
lfaucon <lpfaucon@gmail.com>
2017-09-05 10:47:16 +0200

    small change in jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 
 module.exports = {
-  rootDir: __dirname,
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   moduleNameMapper: {
     '^meteor/(.*)$': '<rootDir>/frog/lib/fakeMeteor'
