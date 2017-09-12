16927dbadd0c6e581d81b8abd1e9d30dab4ad01c
Louis Faucon <lpfaucon@gmail.com>
2017-09-12 22:11:42 +0200

    Update open_many script to new login mechanism

--- open_many.sh
+++ open_many.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-open http://dev1:3000/#/teacher/peter
-open http://dev2:3000/#/john
-open http://dev3:3000/#/nils
-open http://dev4:3000/#/jens
+open http://dev1:3000/?login=pierre
+open http://dev2:3000/?login=stian
+open http://dev3:3000/?login=louis
+open http://dev4:3000/?login=jennifer
