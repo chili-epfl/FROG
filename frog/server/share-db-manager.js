2998bcf64d76e05efd64e508f7864f53de1de4fa
Stian Håklev <shaklev@gmail.com>
2017-09-29 12:48:57 +0200

    WIP

--- frog/server/share-db-manager.js
+++ frog/server/share-db-manager.js
@@ -5,29 +5,44 @@ import WebSocket from 'ws';
 import WebsocketJSONStream from 'websocket-json-stream';
 import ShareDBMongo from 'sharedb-mongo';
 import http from 'http';
+import sharedbClient from 'sharedb/lib/client';
+import { ReconnectingWebSocket } from 'rws';
 
 declare var Promise: any;
-const dbUrl =
-  (Meteor.settings && Meteor.settings.public.dburl) ||
-  'mongodb://localhost:3001';
-const db = ShareDBMongo(`${dbUrl}/sharedb`);
 
-const server = http.createServer();
-const backend = new ShareDB({ db });
-export const serverConnection = backend.connect();
+let _serverConnection = {};
+let _startShareDB = () => {};
 
-export const startShareDB = () => {
-  new WebSocket.Server({ server }).on('connection', ws => {
-    backend.listen(new WebsocketJSONStream(ws));
-  });
+if (Meteor.settings.public.sharedburl) {
+  const shareDbUrl = Meteor.settings.public.sharedburl;
+  const socket = new ReconnectingWebSocket(shareDbUrl);
+  _serverConnection = new sharedbClient.Connection(socket);
+} else {
+  const dbUrl =
+    (Meteor.settings && Meteor.settings.public.dburl) ||
+    'mongodb://localhost:3001';
+  const db = ShareDBMongo(`${dbUrl}/sharedb`);
 
-  const shareDbPort =
-    (Meteor.settings && Meteor.settings.public.sharedbport) || 3002;
-  // $FlowFixMe
-  server.listen(shareDbPort, err => {
-    if (err) throw err;
-  });
-};
+  const server = http.createServer();
+  const backend = new ShareDB({ db });
+  _serverConnection = backend.connect();
+
+  _startShareDB = () => {
+    new WebSocket.Server({ server }).on('connection', ws => {
+      backend.listen(new WebsocketJSONStream(ws));
+    });
+
+    const shareDbPort =
+      (Meteor.settings && Meteor.settings.public.sharedbport) || 3002;
+    // $FlowFixMe
+    server.listen(shareDbPort, err => {
+      if (err) throw err;
+    });
+  };
+}
+
+export const serverConnection = _serverConnection;
+export const startShareDB = _startShareDB;
 
 export const getDoc = (docId: string): any => {
   const doc = serverConnection.get('rz', docId);
--- mup.js
+++ mup.js
@@ -35,19 +35,12 @@ module.exports = {
 
     env: {
       ROOT_URL: 'https://icchilisrv3.epfl.ch',
-      MONGO_URL: 'mongodb://icchilisrv3.epfl.ch/meteor'
-    },
-
-    ssl: {
-      autogenerate: {
-        email: 'stian.haklev@epfl.ch',
-        domains: 'icchilisrv3.epfl.ch, icchilisrv4.epfl.ch'
-      }
+      MONGO_URL: 'mongodb://localhost:27500/meteor'
     },
 
     docker: {
       image: 'abernix/meteord:node-8.3.0-base',
-      args: ['-p 3002:3002']
+      args: ['--network="host"']
     },
 
     enableUploadProgressBar: true
