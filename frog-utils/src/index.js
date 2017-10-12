e0dad9f7ccdfb6366442b02a95f21dfc7d40aa5d
Stian Håklev <shaklev@gmail.com>
2017-10-12 16:36:41 +0200

    Collaborative editing (#539)

--- frog-utils/src/index.js
+++ frog-utils/src/index.js
@@ -1,8 +1,8 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 
 import { compose, withHandlers, withState } from 'recompose';
-import { shuffle } from 'lodash';
+import { shuffle, omit } from 'lodash';
 
 export {
   default as EnhancedForm,
@@ -132,6 +132,58 @@ export const wordWrap = (text: string, maxLength: number): string[] => {
 
 const groupchars = 'ABCDEFGHIJKLMNOPQRSTUWXYZ123456789'.split('');
 export const getSlug = (n: number) => shuffle(groupchars).slice(0, n).join('');
+
+type ReactivePropsT = {
+  path: string | string[],
+  dataFn: Object,
+  type: 'textarea' | 'textinput'
+};
+
+export class ReactiveText extends Component {
+  textRef: any;
+  binding: any;
+  state: ReactivePropsT;
+
+  update = (props: ReactivePropsT) => {
+    this.setState({ path: props.path, dataFn: props.dataFn });
+    if (this.binding) {
+      this.binding.destroy();
+    }
+    this.binding = props.dataFn.bindTextField(this.textRef, props.path);
+  };
+
+  componentDidMount() {
+    this.update(this.props);
+  }
+
+  componentWillReceiveProps(nextProps: ReactivePropsT) {
+    if (
+      (nextProps.dataFn && nextProps.dataFn.doc.id) !==
+        (this.props.dataFn && this.props.dataFn.doc.id) ||
+      this.props.path !== nextProps.path ||
+      this.props.type !== nextProps.type
+    ) {
+      this.update(nextProps);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.binding) {
+      this.binding.destroy();
+    }
+  }
+
+  render() {
+    const rest = omit(this.props, ['path', 'dataFn']);
+    return this.props.type === 'textarea'
+      ? <textarea ref={ref => (this.textRef = ref)} {...rest} defaultValue="" />
+      : <input
+          type="text"
+          ref={ref => (this.textRef = ref)}
+          {...rest}
+          defaultValue=""
+        />;
+  }
 
 // If you try to insert value=0 path=['a', 'b', 'c']
 // into sharedb with doc={ 'a': { d:5 } }
