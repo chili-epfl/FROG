72ab404321f4e5d92e211501f2a32683756a4c1e
Stian Håklev <shaklev@gmail.com>
2017-10-13 14:37:32 +0200

    WIP

--- ac/ac-imgClass/src/components/ObjectList.jsx
+++ ac/ac-imgClass/src/components/ObjectList.jsx
@@ -39,33 +39,20 @@ const Category = styled.span`
   font-weight: bold;
 `;
 
-<<<<<<< HEAD:ac/ac-imgClass/src/components/ImageList.jsx
-export default ({ images, setImageKey, imageKey }: Object) => (
-  <Scroll>
-    <Main style={{ width: 100 * images.length + 'px' }}>
-      {images.map(image => (
-=======
 export default ({ objects, setObjectKey, objectKey }: Object) =>
   <Scroll>
     <Main style={{ width: 100 * objects.length + 'px' }}>
       {objects.map(obj =>
->>>>>>> unilMontyComponent:ac/ac-imgClass/src/components/ObjectList.jsx
         <Box
           key={obj.key}
           onClick={() => setObjectKey(obj.key)}
           style={{ background: objectKey === obj.key ? 'lightblue' : 'none' }}
         >
-<<<<<<< HEAD:ac/ac-imgClass/src/components/ImageList.jsx
-          <ImagePanel url={image.thumbnail} />
-          <Category>{image.category}</Category>
-=======
-          <ObjectPanel obj={obj} small={true}/>
+          <ObjectPanel obj={obj} small />
           <Category>
             {obj.category}
           </Category>
->>>>>>> unilMontyComponent:ac/ac-imgClass/src/components/ObjectList.jsx
         </Box>
-      ))}
+      )}
     </Main>
-  </Scroll>
-);
+  </Scroll>;
