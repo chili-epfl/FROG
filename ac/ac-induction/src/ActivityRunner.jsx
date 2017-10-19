d6526d361050459673cc8f12af65ec68f71389e0
romainAA <artruromain@hotmail.fr>
2017-10-19 08:18:11 +0200

    Improve ac-induction (#386)

---
+++ ac/ac-induction/src/ActivityRunner.jsx
@@ -0,0 +1,78 @@
+// @flow
+
+import React from 'react';
+import type { ActivityRunnerT } from 'frog-utils';
+
+import { Main } from './StyledComponents';
+import NavigationBar from './NavigationBar';
+import Presentation from './Presentation';
+import Examples from './Examples';
+import Test from './Test';
+import Definition from './Definition';
+import End from './End';
+
+export default ({ activityData, data, dataFn }: ActivityRunnerT) => {
+  const { title, examples, definition } = activityData.config;
+  let page = null;
+  switch (data.parts[data.indexPart]) {
+    case 'Presentation':
+      page = <Presentation title={title} dataFn={dataFn} data={data} />;
+      break;
+    case 'Examples':
+      page = (
+        <Examples
+          title={title}
+          examples={examples}
+          nbExamples={activityData.config.nbExamples}
+          dataFn={dataFn}
+          data={data}
+        />
+      );
+      break;
+    case 'Tests with feedback':
+      page = (
+        <Test
+          examples={examples}
+          nbTest={0}
+          nbTestFeedback={activityData.config.nbTestFeedback}
+          feedback
+          dataFn={dataFn}
+          data={data}
+        />
+      );
+      break;
+    case 'Definition':
+      page = (
+        <Definition
+          title={title}
+          definition={definition}
+          hasTest={activityData.config.hasTest}
+          dataFn={dataFn}
+          data={data}
+        />
+      );
+      break;
+    case 'Tests':
+      page = (
+        <Test
+          examples={examples}
+          nbTest={activityData.config.nbTest}
+          nbTestFeedback={0}
+          feedback={false}
+          dataFn={dataFn}
+          data={data}
+        />
+      );
+      break;
+    case 'End':
+      page = <End />;
+      break;
+    default:
+  }
+  return (
+    <Main>
+      <NavigationBar config={activityData.config} data={data} />
+      {page}
+    </Main>
+  );
+};
