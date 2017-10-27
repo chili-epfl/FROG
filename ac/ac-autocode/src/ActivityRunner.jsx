324236d67a3f9af799226bcc6ce0fe8cff482c72
Clément Launay <clement.launay@epfl.ch>
2017-10-27 14:48:38 +0200

    WIP

--- ac/ac-autocode/src/ActivityRunner.jsx
+++ ac/ac-autocode/src/ActivityRunner.jsx
@@ -3,29 +3,48 @@
 import React from 'react';
 import type { ActivityRunnerT } from 'frog-utils';
 
+
+
 const ActivityRunner = ({ activityData }: ActivityRunnerT) => {
-  const submitCode = () => {};
+
+
+  const outf = (text) => {
+    let codeOutput = document.getElementById("codeOutput");
+    codeOutput.innerHTML = text;
+  };
 
   return (
     <div>
-      <h1>
-        {activityData.config.title || 'Coding exercise'}
-      </h1>
-      <div>
-        <p>
-          {activityData.config.guidelines || 'Instructions for the exercise'}
-        </p>
-        <textarea
-          name="codeArea"
-          rows="20"
-          cols="80"
-          defaultValue="Your code here"
-        />
-      </div>
-      <button type="button" onClick={submitCode}>
-        Submit
-      </button>
-    </div>
+
+<script src="http://www.skulpt.org/static/skulpt.min.js" type="text/javascript"></script>
+<script src="http://www.skulpt.org/static/skulpt-stdlib.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+
+
+function builtinRead(x) {
+    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+            throw "File not found: '" + x + "'";
+    return Sk.builtinFiles["files"][x];
+}
+
+function runit() {
+   var prog = document.getElementById("yourcode").value;
+   out= document.getElementById("output");
+   out.innerHTML=""
+   Sk.configure({output:(text)=>{out.innerHTML=out.innerHTML+text}, read:builtinRead});
+  Sk.importMainWithBody("<stdin>",false,prog);
+}
+</script>
+
+<h3>Try This</h3>
+<textarea id="yourcode" cols="40" rows="10">
+print "Hello World"
+</textarea>
+<button type="button" onclick="runit()">Run</button>
+<p id="output" ></p>
+</body>
+</div>
   );
 };
 
