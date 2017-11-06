abade2f98969b3bfacc1b1b14e77ccc6e8d3dcdf
Romain Artru <romain.artru@epfl.ch>
2017-11-06 11:38:32 +0100

    done ?

--- ac/ac-induction/src/End.jsx
+++ ac/ac-induction/src/End.jsx
@@ -1,34 +1,47 @@
 // @flow
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export default class End extends Component {
-
-  // constructor(props: Object){
-  //   super(props);
-  // }
-
   componentWillMount() {
     this.props.dataFn.objDel({}, ['feedbackOpen']);
+    this.props.dataFn.objDel({}, ['testChoice']);
   }
 
-  render(){
-    console.log(this.props.data);
+  render() {
     return (
       <div style={{ margin: '50px' }}>
         <h2>End of the activity</h2>
+        {this.props.data.parts.includes('Tests with feedback') && (
+          <Result list={this.props.data.listIndexTestWithFeedback} />
+        )}
+        {this.props.data.parts.includes('Tests') && (
+          <Result list={this.props.data.listIndexTest} />
+        )}
       </div>
     );
   }
 }
 
-// export default ({ dataFn, data }: Object) =>{
-//   dataFn.objDel({}, ['feedbackOpen']);
-//
-//   console.log(data);
-//   return (
-//     <div style={{ margin: '50px' }}>
-//       <h2>End of the activity</h2>
-//     </div>
-//   );
-// }
+const Result = ({ list }: Object) => (
+  <div style={{ display: 'flex', flexDirection: 'row', margin: '10px' }}>
+    Test with feedback :
+    {list.map((x, i) => (
+      <div
+        key={x + i.toString()}
+        style={{
+          width: 400 / list.length + 'px',
+          height: '30px',
+          backgroundColor:
+            x.correction.result === 0
+              ? '#00CC00'
+              : x.correction.result === 1 ? '#FF9933' : '#CC0000',
+          border: 'solid black 1px',
+          textAlign: 'center'
+        }}
+      >
+        {i}
+      </div>
+    ))}
+  </div>
+);
