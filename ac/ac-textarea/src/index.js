1be046d4b7675ab0056570d40ab62cb5950361cf
Stian Håklev <shaklev@gmail.com>
2017-11-09 13:42:12 +0100

    WIP

--- ac/ac-textarea/src/index.js
+++ ac/ac-textarea/src/index.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react';
-import { type ActivityPackageT } from 'frog-utils';
+import { type ActivityPackageT, ReactiveText } from 'frog-utils';
 
 const meta = {
   name: 'Text area',
@@ -9,14 +9,30 @@ const meta = {
   description: 'Can also show configurable prompt',
   exampleData: [
     {
-      title: 'Case with no data',
-      config: { title: 'No data' },
+      title: 'Case with no data or config',
+      config: {},
       activityData: {}
     },
     {
-      title: 'Case with no data',
-      config: { title: 'No data' },
+      title: 'Case with title and prompt',
+      config: {
+        title: 'Reflection',
+        prompt:
+          'Please discuss how you first discovered that your parents were aliens, and how that made you feel'
+      },
       activityData: {}
+    },
+    {
+      title: 'Case with title and prompt, and incoming data',
+      config: {
+        title: 'Reflection',
+        prompt:
+          'Please discuss how you first discovered that your parents were aliens, and how that made you feel'
+      },
+      data: {
+        text:
+          'It first began a dark night in December, and included confetti and a full moon.'
+      }
     }
   ]
 };
@@ -25,22 +41,49 @@ const config = {
   type: 'object',
   properties: {
     title: {
-      title: 'What is the title?',
+      title: 'Optional title',
+      type: 'string'
+    },
+    prompt: {
+      title: 'Optional prompt',
       type: 'string'
     }
   }
 };
 
 // default empty reactive datastructure, typically either an empty object or array
-const dataStructure = {};
+const dataStructure = { text: '' };
 
 // receives incoming data, and merges it with the reactive data using dataFn.*
-const mergeFunction = (object, dataFn) => {};
+const mergeFunction = (object, dataFn) => {
+  if (object.data && object.data.text) {
+    dataFn.objInsert(object.data.text, 'text');
+  }
+};
 
 // the actual component that the student sees
-const ActivityRunner = ({ logger, activityData, data, dataFn, userInfo }) => (
-  <div>{JSON.stringify(activityData)}</div>
-);
+const ActivityRunner = ({ activityData, dataFn }) => {
+  const header = activityData.config && [
+    activityData.config.title && (
+      <h1 key="title">{activityData.config.title}</h1>
+    ),
+    activityData.config.prompt && (
+      <p key="prompt" style={{ fontSize: '25px' }}>
+        <i>{activityData.config.prompt}</i>
+      </p>
+    )
+  ];
+  return [
+    ...header,
+    <ReactiveText
+      type="textarea"
+      path="text"
+      dataFn={dataFn}
+      key="textarea"
+      style={{ width: '100%', height: '100%', fontSize: '20px' }}
+    />
+  ];
+};
 
 export default ({
   id: 'ac-textarea',
