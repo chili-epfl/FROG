45dc3057212d262d0e9c249827a2347394b3571c
Stian Håklev <shaklev@gmail.com>
2017-11-09 16:25:23 +0100

    Allow op-create-groups to create a chosen number of groups (#651)

--- op/op-create-groups/src/__tests__/index.js
+++ op/op-create-groups/src/__tests__/index.js
@@ -41,3 +41,54 @@ test('Create one large group with everyone', () =>
   expect(operator({ strategy: 'minimum', groupsize: 999 }, obj)).toEqual({
     group: { '1': ['1', '2', '3', '4', '5'] }
   }));
+
+test('Create two groups, odd number', () =>
+  expect(operator({ groupnumber: true, globalnum: 2 }, obj)).toEqual({
+    group: { '1': ['3', '4', '5'], '2': ['1', '2'] }
+  }));
+
+test('Create two groups, exact number', () =>
+  expect(operator({ groupnumber: true, globalnum: 2 }, obj4)).toEqual({
+    group: { '1': ['3', '4'], '2': ['1', '2'] }
+  }));
+
+test('Create three groups, very unequal', () =>
+  expect(operator({ groupnumber: true, globalnum: 3 }, obj4)).toEqual({
+    group: { '1': ['4'], '2': ['3'], '3': ['1', '2'] }
+  }));
+
+const obj11 = {
+  globalStructure: {
+    studentIds: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']
+  }
+};
+
+test('Works with large numbers, 3', () =>
+  expect(operator({ groupnumber: true, globalnum: 3 }, obj11)).toEqual({
+    group: {
+      '1': ['6', '7', '8', '9'],
+      '2': ['2', '3', '4', '5'],
+      '3': ['1', '10', '11']
+    }
+  }));
+
+test('Works with large numbers, 4', () =>
+  expect(operator({ groupnumber: true, globalnum: 4 }, obj11)).toEqual({
+    group: {
+      '1': ['7', '8', '9'],
+      '2': ['4', '5', '6'],
+      '3': ['11', '2', '3'],
+      '4': ['1', '10']
+    }
+  }));
+
+test('Works with large numbers, 5', () =>
+  expect(operator({ groupnumber: true, globalnum: 5 }, obj11)).toEqual({
+    group: {
+      '1': ['8', '9'],
+      '2': ['6', '7'],
+      '3': ['4', '5'],
+      '4': ['2', '3'],
+      '5': ['1', '10', '11']
+    }
+  }));
