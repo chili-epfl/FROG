d45c9ce1810d8cb0e50a8ea216ed3d6884bb8e13
Louis Faucon <lpfaucon@gmail.com>
2017-11-10 16:21:42 +0100

    Undo text example in ac-images

--- ac/ac-image/src/meta.js
+++ ac/ac-image/src/meta.js
@@ -97,16 +97,6 @@ export const meta = {
         ]
       },
       data: {}
-    },
-    {
-      title: 'With texts, through activityData',
-      config: {
-        guidelines: 'Debrief of student contributions'
-      },
-      data: {
-        1: { type: 'text', key: 1, text: 'I think the world is round' },
-        2: { type: 'text', key: 2, text: 'Jolly good fellow' }
-      }
     }
   ]
 };
