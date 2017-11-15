88da1eb45e94c8d55a03b601c24470b62e613958
Stian Håklev <shaklev@gmail.com>
2017-11-15 11:56:26 +0100

    WIP

--- package-scripts.js
+++ package-scripts.js
@@ -1,15 +1,22 @@
+var findParentDir = require('find-parent-dir');
+ 
+const inContext = (exec) => { 
+findParentDir(__dirname, '.git', (err, dir)=> {
+process.chdir(dir)
+return exec
+}
+
 module.exports = {
   scripts: {
     test:
       'flow --quiet && npm run -s start eslint-test && npm run -s start jest',
     dockerTest:
       'rm -rf ./tmp/ && mkdir -p ./tmp/frog && git clone . ./tmp/frog && cd ./tmp/frog && docker build -t frogtest . && docker run frogtest',
-    fix: 'eslint --fix -c .eslintrc-prettier.js --ext .js,.jsx .',
+    fix: inContext('eslint --fix -c .eslintrc-prettier.js --ext .js,.jsx .'),
     eslintTest: 'eslint -c .eslintrc-prettier.js --ext .js,.jsx .',
     jest: 'jest',
     jestWatch: 'jest --watch',
-    flowTest: 'flow',
-    nightmareTest: 'node nightmare/test.js'
+    flowTest: 'flow'
   },
   options: {
     silent: true
