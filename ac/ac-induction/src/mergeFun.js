5391b89b363b5e09f032bb4923d137532b3ce01a
Romain Artru <romain.artru@epfl.ch>
2017-11-22 14:25:31 +0100

    modified the exampleData to fit the new config and the mergeFun to accept an null string as partStr

--- ac/ac-induction/src/mergeFun.js
+++ ac/ac-induction/src/mergeFun.js
@@ -16,29 +16,31 @@ export default (obj: Object, dataFn: Object) => {
     unnecessaryProperties,
     examples
   } = obj.config;
-  const parts = partStr.split(',').map((x, i) => {
-    const n = Number(x.slice(1));
-    switch (x[0]) {
-      case 'e':
-        dataFn.objInsert(genList(examples, n), 'listIndexEx' + (i + 1));
-        return ['Examples', n];
-      case 'd':
-        return ['Definition', 1];
-      case 'f':
-        dataFn.objInsert(
-          genList(examples, n),
-          'listIndexTestWithFeedback' + (i + 1)
-        );
-        return ['Tests with feedback', n];
-      case 't':
-        dataFn.objInsert(genList(examples, n), 'listIndexTest' + (i + 1));
-        return ['Tests', n];
-      default:
-        return null;
-    }
-  });
+  const parts =
+    partStr &&
+    partStr.split(',').map((x, i) => {
+      const n = Number(x.slice(1));
+      switch (x[0]) {
+        case 'e':
+          dataFn.objInsert(genList(examples, n), 'listIndexEx' + (i + 1));
+          return ['Examples', n];
+        case 'd':
+          return ['Definition', 1];
+        case 'f':
+          dataFn.objInsert(
+            genList(examples, n),
+            'listIndexTestWithFeedback' + (i + 1)
+          );
+          return ['Tests with feedback', n];
+        case 't':
+          dataFn.objInsert(genList(examples, n), 'listIndexTest' + (i + 1));
+          return ['Tests', n];
+        default:
+          return null;
+      }
+    });
   dataFn.objInsert(
-    [['Presentation', 1]].concat(parts).concat([['End', 1]]),
+    [['Presentation', 1]].concat(parts || []).concat([['End', 1]]),
     'parts'
   );
 
