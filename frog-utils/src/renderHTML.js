0d5984fd1831139730f215c36d8b28c8994ebf5e
Stian Håklev <shaklev@gmail.com>
2017-11-27 13:14:03 +0100

    WIP

--- frog-utils/src/renderHTML.js
+++ frog-utils/src/renderHTML.js
@@ -2,19 +2,51 @@
 
 import React from 'react';
 import { renderToString } from 'katex';
+import { withState } from 'recompose';
+import { uuid } from 'lodash';
 
-export default ({ html }: { html: string }) => {
-  let toRender;
-  try {
-    toRender = html.replace(/\$\$(.*?)\$\$/gi, hit => {
-      const a = hit.slice(2, hit.length - 2);
-      return renderToString(a);
+const Cache = {};
+
+const getCache = (item: string, setUpdate) => {
+  fetch('//noembed.com/embed?url=' + item.replace(/(<([^>]+)>)/gi, ''))
+    .then(x => x.json())
+    .then(x => {
+      Cache[item] = x.html;
+      setUpdate(uuid());
     });
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(e);
-    toRender = html;
+};
+
+const HTML = ({
+  html,
+  update,
+  setUpdate
+}: {
+  html: string,
+  update: string,
+  setUpdate: string => void
+}) => {
+  let toRender;
+  if (html.startsWith('http')) {
+    if (Cache[html]) {
+      toRender = Cache[html];
+    } else {
+      window.setTimeout(getCache(html, setUpdate), 0);
+      toRender = '...';
+    }
+  } else {
+    try {
+      toRender = html.replace(/\$\$(.*?)\$\$/gi, hit => {
+        const a = hit.slice(2, hit.length - 2);
+        return renderToString(a);
+      });
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e);
+      toRender = html;
+    }
   }
 
   return <div dangerouslySetInnerHTML={{ __html: toRender }} />;
 };
+
+export default withState('update', 'setUpdate', '')(HTML);
