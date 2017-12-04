d2a8f6a8e91554a6b9a5c1a76f4285f0cec3c2ec
Clément Launay <clement.launay@epfl.ch>
2017-12-04 10:44:22 +0100

    Update config example to fit new structure

--- ac/ac-autocode/src/config.js
+++ ac/ac-autocode/src/config.js
@@ -73,41 +73,11 @@ export const exampleConfig1 = {
     guidelines: 'Code an algorithm to sort an array in python',
     specifications:
       'The variable a is an array of integers. You must sort it and assign the result to variable b.',
-    templateCode: 'b=sorted(a)',
-    tests: [
-      {
-        description: 'Already sorted',
-        preCode: 'a=[1, 2, 3]',
-        postCode: 'print b',
-        onlyLastPrint: true,
-        expectedPrint: '[1, 2, 3]',
-        showExpected: true
-      },
-      {
-        description: 'Normal case',
-        preCode: 'a=[10, 2, 15, 8, 7]',
-        postCode: 'print b',
-        onlyLastPrint: true,
-        expectedPrint: '[2, 7, 8, 10, 15]',
-        showExpected: true
-      },
-      {
-        description: 'Edge case 1',
-        preCode: 'a=[42]',
-        postCode: 'print b',
-        onlyLastPrint: true,
-        expectedPrint: '[42]',
-        showExpected: true
-      },
-      {
-        description: 'Edge case 2',
-        preCode: 'a=[]',
-        postCode: 'print b',
-        onlyLastPrint: true,
-        expectedPrint: '[]',
-        showExpected: true
-      }
-    ]
+    template: 'b=a',
+    solution: 'solution = lambda x: sorted(x)',
+    testing:
+      'print b\nprint solution(a)\nprint ("SUCCESS" if b == solution(a) else "FAILURE")',
+    tests: ['a=[1, 2, 3]', 'a=[10, 2, 15, 8, 7]', 'a=[42]', 'a=[]']
   },
   data: {}
 };
@@ -120,49 +90,13 @@ export const exampleConfig2 = {
       'The variable N is the input number. You must print a string representing it in roman notation',
     specifications:
       'Reminder:\nI:1, V:5, X:10, L:50, C:100, D:500, M:1000\nRepeating a numeral up to three times represents addition of the number.\nWriting numerals that decrease from left to right represents addition of the numbers.\nTo write a number that otherwise would take repeating of a numeral four or more times, there is a subtraction rule. Writing a smaller numeral to the left of a larger numeral represents subtraction.',
-    templateCode: 'if N=1:\n\tprint "I"\nelse:\n\tprint "II"',
-    tests: [
-      {
-        description: 'One letter',
-        preCode: 'N=50',
-        postCode: '',
-        onlyLastPrint: true,
-        expectedPrint: 'L',
-        showExpected: true
-      },
-      {
-        description: 'Simple addition',
-        preCode: 'N=12',
-        postCode: '',
-        onlyLastPrint: true,
-        expectedPrint: 'XII',
-        showExpected: true
-      },
-      {
-        description: 'Substraction rule',
-        preCode: 'N=1994',
-        postCode: '',
-        onlyLastPrint: true,
-        expectedPrint: 'MCMXCIV',
-        showExpected: true
-      },
-      {
-        description: 'The largest',
-        preCode: 'N=3999',
-        postCode: '',
-        onlyLastPrint: true,
-        expectedPrint: 'MMMCMXCIX',
-        showExpected: true
-      },
-      {
-        description: 'A bit of everything',
-        preCode: 'N=1474',
-        postCode: '',
-        onlyLastPrint: true,
-        expectedPrint: 'MCDLXXIV',
-        showExpected: true
-      }
-    ]
+    template:
+      'from collections import OrderedDict\ndef write_roman(num):\n\troman = OrderedDict()\n\troman[1000] = "M"\n\troman[900] = "CM"\n\troman[500] = "D"\n\troman[400] = "CD"\n\troman[100] = "C"\n\troman[90] = "XC"\n\troman[50] = "L"\n\troman[40] = "XL"\n\troman[10] = "X"\n\troman[9] = "IX"\n\troman[5] = "V"\n\troman[4] = "IV"\n\troman[1] = "I"\n\tdef roman_num(num):\n\t\tfor r in roman.keys():\n\t\t\tx, y = divmod(num, r)\n\t\t\tyield roman[r] * x\n\t\t\tnum -= (r * x)\n\t\t\tif num > 0:\n\t\t\t\troman_num(num)\n\t\t\telse:\n\t\t\t\tbreak\n\treturn "".join([a for a in roman_num(num)])\nprint write_roman(3)',
+    solution:
+      'from collections import OrderedDict\ndef solution(num):\n\troman = OrderedDict()\n\troman[1000] = "M"\n\troman[900] = "CM"\n\troman[500] = "D"\n\troman[400] = "CD"\n\troman[100] = "C"\n\troman[90] = "XC"\n\troman[50] = "L"\n\troman[40] = "XL"\n\troman[10] = "X"\n\troman[9] = "IX"\n\troman[5] = "V"\n\troman[4] = "IV"\n\troman[1] = "I"\n\tdef roman_num(num):\n\t\tfor r in roman.keys():\n\t\t\tx, y = divmod(num, r)\n\t\t\tyield roman[r] * x\n\t\t\tnum -= (r * x)\n\t\t\tif num > 0:\n\t\t\t\troman_num(num)\n\t\t\telse:\n\t\t\t\tbreak\n\treturn "".join([a for a in roman_num(num)])',
+    testing:
+      'y=write_roman(N)\nz=solution(N)\nprint y\nprint z\nprint ("SUCCESS" if y == z else "FAILURE")',
+    tests: ['N=50', 'N=12', 'N=1994', 'N=3999', 'N=1474']
   },
   data: {}
 };
