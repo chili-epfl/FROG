c5d03ecc2c29315f8c1550c09b30ba84ff96b9c0
romainAA <artruromain@hotmail.fr>
2017-12-06 10:57:48 +0100

    Better configuration for ac-induc (#699)

--- ac/ac-induction/src/mergeFun.js
+++ ac/ac-induction/src/mergeFun.js
@@ -10,35 +10,39 @@ export default (obj: Object, dataFn: Object) => {
   dataFn.objInsert(true, 'testChoice');
 
   const {
-    hasExamples,
-    nbExamples,
-    hasTestWithFeedback,
-    nbTestFeedback,
-    hasDefinition,
-    hasTest,
-    nbTest,
+    partStr,
     suffisantSets,
     contradictoryProperties,
     unnecessaryProperties,
     examples
   } = obj.config;
-
-  if (hasExamples)
-    dataFn.objInsert(genList(examples, nbExamples), 'listIndexEx');
-  if (hasTestWithFeedback)
-    dataFn.objInsert(
-      genList(examples, nbTestFeedback),
-      'listIndexTestWithFeedback'
-    );
-  if (hasTest) dataFn.objInsert(genList(examples, nbTest), 'listIndexTest');
-
-  const parts = ['Presentation'];
-  if (hasExamples) parts.push('Examples');
-  if (hasTestWithFeedback) parts.push('Tests with feedback');
-  if (hasDefinition) parts.push('Definition');
-  if (hasTest) parts.push('Tests');
-  parts.push('End');
-  dataFn.objInsert(parts, 'parts');
+  const parts =
+    partStr &&
+    partStr.split(',').map((x, i) => {
+      const n = Number(x.slice(1));
+      switch (x[0]) {
+        case 'e':
+          dataFn.objInsert(genList(examples, n), 'listIndexEx' + (i + 1));
+          return ['Examples', n];
+        case 'd':
+          return ['Definition', 1];
+        case 'f':
+          dataFn.objInsert(
+            genList(examples, n),
+            'listIndexTestWithFeedback' + (i + 1)
+          );
+          return ['Tests with feedback', n];
+        case 't':
+          dataFn.objInsert(genList(examples, n), 'listIndexTest' + (i + 1));
+          return ['Tests', n];
+        default:
+          return null;
+      }
+    });
+  dataFn.objInsert(
+    [['Presentation', 1]].concat(parts || []).concat([['End', 1]]),
+    'parts'
+  );
 
   const suffisants = [];
   const tmp = new Set();
@@ -65,8 +69,8 @@ export default (obj: Object, dataFn: Object) => {
 const genList = (tab: Array<any>, n: number) => {
   const T = shuffle(tab.filter(x => !x.isIncorrect));
   const F = shuffle(tab.filter(x => x.isIncorrect));
-  let tmp = T.slice(-1 * Math.min(T.length, n / 2)).concat(
-    F.slice(-1 * Math.min(F.length, n / 2))
+  let tmp = T.slice(T.length - Math.min(T.length, n / 2)).concat(
+    F.slice(F.length - Math.min(F.length, n / 2))
   );
   for (let i = 0; tmp.length < n / tab.length + 1; i += 1)
     tmp = tmp.concat(shuffle(tab));
@@ -74,6 +78,5 @@ const genList = (tab: Array<any>, n: number) => {
     realIndex: tab.indexOf(x),
     selectedProperties: []
   }));
-  tmp = tmp.slice(-1 * n);
-  return shuffle(tmp);
+  return shuffle(tmp.slice(-1 * n));
 };
