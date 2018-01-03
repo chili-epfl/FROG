86d929bbc94297c3a109c373f55f6ad091876dec
Clément Launay <clement.launay@epfl.ch>
2018-01-03 18:50:42 +0100

    Improvements

--- ac/ac-autocode/src/Dashboard.jsx
+++ ac/ac-autocode/src/Dashboard.jsx
@@ -8,7 +8,9 @@ import {
   VictoryChart,
   VictoryAxis,
   VictoryTheme,
-  VictoryStack
+  VictoryStack,
+  VictoryLegend,
+  VictoryLabel
 } from 'victory';
 
 const Viewer = (props: Object) => {
@@ -22,6 +24,35 @@ const Viewer = (props: Object) => {
       };
     });
   };
+
+  const sum = x => x['success'] + x['danger'] + x['error'] || 0;
+
+  const debugData = () => {
+    const o = Object.keys(props.data.students).reduce(
+      (acc, val) => {
+        if (acc) {
+          if (props.data.students[val][-1] !== undefined) {
+            const num = sum(props.data.students[val][-1]);
+            acc[num] !== undefined ? acc[num]++ : (acc[num] = 1);
+          } else {
+            acc[0]++;
+          }
+        }
+        return acc;
+      },
+      { '0': 0 }
+    );
+    // ex o = {0:10, 1:5, 2:17} (10 students did not use the debug, 17 used it twice )
+    return Object.keys(o).reduce((acc, val) => {
+      acc.push({ x: val, y: o[val] });
+      return acc;
+    }, []);
+  };
+
+  const green = '#33cc33',
+    red1 = '#ff0000',
+    red2 = '#cc0000';
+
   return (
     <div style={{ height: '2000px', overflow: 'scroll' }}>
       <h1>Dashboard for activity: {props.config.title}</h1>
@@ -32,34 +63,89 @@ const Viewer = (props: Object) => {
       <p>
         {Object.keys(props.data.students).length} students have started coding
       </p>
-      <VictoryChart domainPadding={20} theme={VictoryTheme.material}>
-        <VictoryAxis />
+      <VictoryChart
+        domain={{
+          x: [0, props.config.tests.length],
+          y: [0, Object.keys(props.data.students).length + 1]
+        }}
+        domainPadding={20}
+      >
+        <VictoryLegend
+          x={125}
+          y={0}
+          orientation="horizontal"
+          title="Students' current results"
+          centerTitle
+          gutter={20}
+          style={{ border: { stroke: 'black' } }}
+          data={[
+            { name: 'Sucess', symbol: { fill: green } },
+            { name: 'Danger', symbol: { fill: red1 } },
+            { name: 'Error', symbol: { fill: red2 } }
+          ]}
+        />
+        <VictoryAxis
+          tickFormat={props.config.tests.map(
+            (val, index) => 'Test ' + index + 1
+          )}
+        />
         <VictoryAxis dependentAxis />
         <VictoryStack>
-          <VictoryBar data={chartData('success')} />
-          <VictoryBar data={chartData('danger')} />
-          <VictoryBar data={chartData('error')} />
+          <VictoryBar
+            data={chartData('success')}
+            style={{ data: { fill: green } }}
+          />
+          <VictoryBar
+            data={chartData('danger')}
+            style={{ data: { fill: red1 } }}
+          />
+          <VictoryBar
+            data={chartData('error')}
+            style={{ data: { fill: red2 } }}
+          />
         </VictoryStack>
       </VictoryChart>
-      <pre>{JSON.stringify(props, null, 2)}</pre>
+      <VictoryChart
+        domain={{
+          y: [0, Object.keys(props.data.students).length + 1]
+        }}
+        domainPadding={20}
+      >
+        <VictoryLabel text="Use of debug" />
+        <VictoryAxis />
+        <VictoryAxis dependentAxis />
+        <VictoryBar data={debugData()} />
+      </VictoryChart>
+      // <pre>{JSON.stringify(props, null, 2)}</pre>
     </div>
   );
 };
 
 const mergeLog = (data: any, dataFn: Object, log: LogDBT) => {
-  let previousStatus = '';
+  let previousStatus = '',
+    codeUpdated = true;
   if (data.students[log.userId]) {
-    dataFn.objInsert(log.payload, ['students', log.userId, 'code']);
-    if (data.students[log.userId][log.itemId]) {
-      previousStatus = data.students[log.userId][log.itemId].status;
-      dataFn.numIncr(1, ['students', log.userId, log.itemId, log.value]);
-      dataFn.objInsert(log.value, [
-        'students',
-        log.userId,
-        log.itemId,
-        'status'
-      ]);
+    //student already seen
+    if (data.students[log.userId].code !== log.payload) {
+      // update code if changed
+      dataFn.objInsert(log.payload, ['students', log.userId, 'code']);
     } else {
+      codeUpdated = false;
+    }
+    if (data.students[log.userId][log.itemId]) {
+      // student already submitted for this test (or debug) once
+      previousStatus = data.students[log.userId][log.itemId].status;
+      if (previousStatus !== log.value || codeUpdated) {
+        dataFn.numIncr(1, ['students', log.userId, log.itemId, log.value]);
+        dataFn.objInsert(log.value, [
+          'students',
+          log.userId,
+          log.itemId,
+          'status'
+        ]);
+      }
+    } else {
+      // first time for that test
       dataFn.objInsert(
         {
           status: log.value,
@@ -71,6 +157,7 @@ const mergeLog = (data: any, dataFn: Object, log: LogDBT) => {
       );
     }
   } else {
+    // first time that student is seen
     log.itemId !== undefined &&
       dataFn.objInsert(
         {
