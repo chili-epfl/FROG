556669815e04c5f7c6ace0caa87d1c7c1f7d95ac
Stian Håklev <shaklev@gmail.com>
2018-01-21 08:23:01 +0100

    WIP

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 
-./initial_setup_wo_meteor.sh
+shopt -s dotglob
 
-mkdir frog/node_modules
-ln -s "`pwd`"/node_modules/* frog/node_modules/
+FROG="`pwd`"
+YARN_VERSION='1.4.0'
+if which yarn && [[ `yarn --version` == $YARN_VERSION ]]; then 
+    echo 'Using pre-installed global Yarn'; YARN=yarn 
+else
+    if [ -f "$FROG/node_modules/.bin/yarn" ] && [[ `"$FROG/node_modules/.bin/yarn" --version` == $YARN_VERSION ]]; then 
+        echo 'Using pre-installed local Yarn'; YARN="$FROG/node_modules/.bin/yarn"
+    else
+        echo 'Installing Yarn'; npm install yarn@$YARN_VERSION && YARN="$FROG/node_modules/.bin/yarn"
+    fi
+fi
+echo "Yarn: $YARN"
+
+"$YARN" install
+
+cd frog-utils
+ln -s "$FROG/.babelrc" . 2>/dev/null
+"$YARN" start build &
+
+# install activities and operators packages
+for dir in "$FROG"/ac/ac-*/ "$FROG"/op/op-*/
+do
+    cd "$dir"
+    ln -s "$FROG/.babelrc" . 2>/dev/null
+    "$YARN" start build &
+done
+
+cd "$FROG/frog"
+ln -s "$FROG"/node_modules . 2>/dev/null
+ln -s "$FROG/.babelrc" . 2>/dev/null
+wait
+exit 0
