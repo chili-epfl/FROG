ad164236af2301e57fb73feea466be476048a08e
Stian Håklev <shaklev@gmail.com>
2018-01-23 07:53:15 +0100

    WIP

--- ac/ac-webrtc/src/ActivityRunner/Header.jsx
+++ ac/ac-webrtc/src/ActivityRunner/Header.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import type { ActivityRunnerT } from 'frog-utils';
 
-export default ({ activityData, userInfo, groupingValue }: ActivityRunnerT) => (
+export default ({ activityData }: ActivityRunnerT) => (
   <div>
     <h1>{activityData.config.title}</h1>
     <p>{activityData.config.info}</p>
