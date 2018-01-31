87409792daea3fb1b0d0b3f8bd16a1cfd1a41574
lfaucon <lpfaucon@gmail.com>
2018-01-31 15:14:50 +0100

    Small changes

--- ac/ac-chat/src/Dashboard.jsx
+++ ac/ac-chat/src/Dashboard.jsx
@@ -4,17 +4,20 @@ import React from 'react';
 import WordCloud from 'react-d3-cloud';
 import { type LogDBT } from 'frog-utils';
 
-const fontSizeMapper = (itMax, word) => 10 + word.value * 250 / Number(itMax);
+const fontSizeMapper = (itMax, word) => 10 + word.value * 150 / Number(itMax);
 
 const Viewer = ({ data }: Object) => {
-  const iMax = Object.values(data).reduce((acc, curr) =>
-    Math.max(Number(acc), Number(curr))
+  const iMax = Object.values(data).reduce(
+    (acc, curr) => Math.max(Number(acc), Number(curr)),
+    1
   );
   return (
-    <div>
+    <div style={{ width: '600px', height: '600px', margin: 'auto' }}>
       <WordCloud
         data={[...Object.keys(data).map(w => ({ text: w, value: data[w] }))]}
         fontSizeMapper={word => fontSizeMapper(iMax, word)}
+        width="600"
+        height="600"
       />
     </div>
   );
