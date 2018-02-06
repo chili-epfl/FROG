09e43912847b31375193546512dabd6752fab6c9
Stian Håklev <shaklev@gmail.com>
2018-02-06 08:09:00 +0100

    WIP

--- frog/imports/ui/GraphEditor/store/store.js
+++ frog/imports/ui/GraphEditor/store/store.js
@@ -56,7 +56,6 @@ const getOneId = (coll: Coll, id: string): ?Elem =>
   getOne(coll, x => x.id === id);
 
 export default class Store {
-  state: StateT;
   browserHistory: any;
   url: string;
 
@@ -74,7 +73,7 @@ export default class Store {
       valid: undefined,
       _graphDuration: 120,
       readOnly: false,
-   
+
       get graphDuration(): number {
         return this.ui.isSvg ? this.ui.furthestObject : this._graphDuration;
       },
@@ -86,12 +85,16 @@ export default class Store {
         return this._state || { mode: 'normal' };
       },
 
-      setBrowserHistory: action((history: any, url: string = '/graph') =>{
+      set state(newState: StateT) {
+        this._state = newState;
+      },
+
+      setBrowserHistory: action((history: any, url: string = '/graph') => {
         this.browserHistory = history;
         this.url = url;
       }),
 
-      changeDuration: action((duration: number)=> {
+      changeDuration: action((duration: number) => {
         if (duration && duration >= 30 && duration <= 1200) {
           const oldPanTime = this.ui.panTime;
           // changes the scale on duration change
@@ -99,13 +102,15 @@ export default class Store {
           this.ui.setScaleValue(this.ui.scale / this.graphDuration);
           const needPanDelta = timeToPx(oldPanTime - this.ui.panTime, 1);
           this.ui.panDelta(needPanDelta);
-          Graphs.update(this.graphId, { $set: { duration: this._graphDuration } });
+          Graphs.update(this.graphId, {
+            $set: { duration: this._graphDuration }
+          });
         }
       }),
 
       overlapAllowed: true,
 
-      toggleOverlapAllowed: action(() =>{
+      toggleOverlapAllowed: action(() => {
         this.overlapAllowed = !this.overlapAllowed;
       }),
 
@@ -125,12 +130,12 @@ export default class Store {
         }
         setCurrentGraph(id);
         const graph = Graphs.findOne(id);
-    
+
         this.readOnly = readOnly;
         this.graphId = id;
-    
+
         this.changeDuration(graph ? graph.duration || 120 : 120);
-    
+
         this.activityStore.all = Activities.find(
           { graphId: id },
           { reactive: false }
@@ -138,16 +143,23 @@ export default class Store {
           .fetch()
           .map(
             x =>
-              new Activity(x.plane, x.startTime, x.title, x.length, x._id, x.state)
+              new Activity(
+                x.plane,
+                x.startTime,
+                x.title,
+                x.length,
+                x._id,
+                x.state
+              )
           );
-    
+
         this.operatorStore.all = Operators.find(
           { graphId: id },
           { reactive: false }
         )
           .fetch()
           .map(x => new Operator(x.time, x.y, x.type, x._id, x.title, x.state));
-    
+
         this.connectionStore.all = Connections.find(
           { graphId: id },
           { reactive: false }
@@ -166,13 +178,13 @@ export default class Store {
             }
             return new Connection(source, target, x._id);
           });
-    
+
         this.ui.selected = null;
         this.history = [];
         this.addHistory();
         this.state = { mode: 'normal' };
         this.ui.setSidepanelOpen(false);
-    
+
         const cursors = {
           activities: Activities.find({ graphId: this.graphId }),
           operators: Operators.find({ graphId: this.graphId }),
@@ -183,7 +195,7 @@ export default class Store {
         cursors.operators.observe(this.operatorStore.mongoObservers);
       }),
 
-      addHistory: action(() =>{
+      addHistory: action(() => {
         if (this.readOnly || this.state.mode === 'readOnly') {
           return;
         }
@@ -198,37 +210,40 @@ export default class Store {
           mergeGraph(this.objects);
         }
         this.refreshValidate();
-      }), 
+      }),
 
       refreshValidate: action(() => {
         let validData = this.validate();
         if (validData.errors) {
           let change = 0;
-          validData.errors.filter(x => x.type === 'needsGroupingKey').forEach(x => {
-            if (validData.social && validData.social[x.id]) {
-              Activities.update(x.id, {
-                $set: {
-                  groupingKey: validData.social[x.id][0]
-                }
-              });
-              change += 1;
-            }
-          });
+          validData.errors
+            .filter(x => x.type === 'needsGroupingKey')
+            .forEach(x => {
+              if (validData.social && validData.social[x.id]) {
+                Activities.update(x.id, {
+                  $set: {
+                    groupingKey: validData.social[x.id][0]
+                  }
+                });
+                change += 1;
+              }
+            });
           if (change > 0) {
             validData = this.validate();
           }
         }
-    
+
         this.graphErrors = sortBy(validData.errors, 'severity');
         this.valid = validData;
-    
+
         Graphs.update(this.graphId, {
           $set: {
-            broken: this.graphErrors.filter(x => x.severity === 'error').length > 0
+            broken:
+              this.graphErrors.filter(x => x.severity === 'error').length > 0
           }
         });
       }),
-      
+
       get canUndo(): boolean {
         return Boolean(this.history.length > 0);
       },
@@ -255,7 +270,7 @@ export default class Store {
           }
           return new Connection(source, target, x._id);
         });
-    
+
         mergeGraph(this.objects);
       }),
 
@@ -284,11 +299,7 @@ export default class Store {
           graphDuration: this._graphDuration
         };
       }
-   });
-  }
-
-  set state(newState: StateT) {
-    this._state = newState;
+    });
   }
 
   findId = ({ type, id }: { type: ElementTypes, id: string }): any => {
@@ -299,5 +310,11 @@ export default class Store {
     }
     return getOneId(this.connectionStore.all, id);
   };
- 
+
+  validate = () =>
+    valid(
+      Activities.find({ graphId: this.graphId }).fetch(),
+      Operators.find({ graphId: this.graphId }).fetch(),
+      Connections.find({ graphId: this.graphId }).fetch()
+    );
 }
