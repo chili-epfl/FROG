43744b0a7a762da1b7fafb2d05ce95820f8eeeef
prastut <kr.prastut@gmail.com>
2018-02-06 13:00:34 +0100

    WIP - session.js

--- frog/imports/ui/GraphEditor/store/session.js
+++ frog/imports/ui/GraphEditor/store/session.js
@@ -1,6 +1,6 @@
 // @flow
 
-import { observable, action } from 'mobx';
+import { extendObservable, observable, action } from 'mobx';
 
 export default class Session {
   constructor(
@@ -10,43 +10,40 @@ export default class Session {
       startedAt: number
     }
   ) {
-    this.id = '';
-    this.timeInClass = 0;
-    this.timeInGraph = 0;
-    if (session) {
-      this.id = session._id;
-      this.setTimes(session);
-      this.interval = setInterval(this.updateTimeInClass, 6000);
-    }
+    extendObservable(this, {
+      id: '',
+      timeInClass: 0,
+      timeInGraph: 0,
+
+      startedAt: number,
+      interval: IntervalID,
+
+      setTimes: action((session: Object) => {
+        this.updateTimeInGraph(session.timeInGraph);
+        this.startedAt = session.startedAt;
+        this.updateTimeInClass((Date.now() - this.startedAt) / 6e4);
+      }),
+
+      updateTimeInGraph: action((newTime: number) => {
+        this.timeInGraph = newTime;
+      }),
+
+      updateTimeInClass: action((newTime: ?number) => {
+        if (newTime && newTime > -1) {
+          this.timeInClass = newTime;
+        } else if (this.startedAt > -1) {
+          this.timeInClass = (Date.now() - this.startedAt) / 6e4;
+        } else {
+          this.timeInClass = 0;
+        }
+      })
+    });
   }
 
-  @observable id: string;
-  @observable timeInClass: number;
-  @observable timeInGraph: number;
-  @observable startedAt: number;
-  interval: IntervalID;
-
-  @action
-  setTimes(session: Object): void {
-    this.updateTimeInGraph(session.timeInGraph);
-    this.startedAt = session.startedAt;
-    this.updateTimeInClass((Date.now() - this.startedAt) / 6e4);
-  }
-
-  @action
-  updateTimeInGraph(newTime: number): void {
-    this.timeInGraph = newTime;
-  }
-
-  @action
-  updateTimeInClass(newTime: ?number): void {
-    if (newTime && newTime > -1) {
-      this.timeInClass = newTime;
-    } else if (this.startedAt > -1) {
-      this.timeInClass = (Date.now() - this.startedAt) / 6e4;
-    } else {
-      this.timeInClass = 0;
-    }
+  if(session) {
+    this.id = session._id;
+    this.setTimes(session);
+    this.interval = setInterval(this.updateTimeInClass, 6000);
   }
 
   close = () => {
