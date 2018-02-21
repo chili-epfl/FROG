17091eb191c15a84469bf9b506b48573a65377ae
Romain Artru <romain.artru@epfl.ch>
2018-02-21 17:03:12 +0100

    fix the undo button (could be improved)

--- frog/imports/ui/GraphEditor/store/activityStore.js
+++ frog/imports/ui/GraphEditor/store/activityStore.js
@@ -161,7 +161,7 @@ export default class ActivityStore {
             new Activity(
               x.plane,
               x.startTime,
-              x.title,
+              x.rawTitle,
               x.length,
               x._id,
               x.state
