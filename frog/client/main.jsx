e74c7735ee366d7d2cce94bf6f125953e5f66db0
Stian Håklev <shaklev@gmail.com>
2018-02-26 07:32:51 +0100

    WIP

--- frog/client/main.jsx
+++ frog/client/main.jsx
@@ -4,6 +4,9 @@ import { render } from 'react-dom';
 
 import App from '../imports/ui/App';
 
+global.Buffer = function() {};
+global.Buffer.isBuffer = () => false;
+
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
 });
