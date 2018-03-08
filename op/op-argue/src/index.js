84508e1ca976f00ed45a935f472cfbe112345344
lfaucon <lpfaucon@gmail.com>
2018-03-08 16:18:58 +0100

    WIP on op-argue

--- op/op-argue/src/index.js
+++ op/op-argue/src/index.js
@@ -14,55 +14,36 @@ const config = {
   properties: {}
 };
 
-export const flatten = (data: Object): Object => {
-  const result = {};
-  function recurse(cur, prop) {
-    if (typeof cur === 'string' || typeof cur === 'number') {
-      result[prop] = cur;
-    } else if (Array.isArray(cur)) {
-      const l = cur.length;
-      for (let i = 0; i < l; i += 1) recurse(cur[i], prop + '.' + i);
-      if (l === 0) result[prop] = [];
-    } else {
-      let isEmpty = true;
-      if (cur) {
-        const ks = Object.keys(cur);
-        for (let k = 0; k < ks.length; k += 1) {
-          const p = ks[k];
-          isEmpty = false;
-          recurse(cur[p], prop ? prop + '.' + p : p);
-        }
-      }
-      if (isEmpty && prop) result[prop] = {};
-    }
-  }
-  recurse(data, '');
-  return result;
-};
+const optim = (values) => values.reduce((acc,x) => acc + Math.sqrt(x), 0)
 
 const operator = (configData, object) => {
-  const { globalStructure, activityData: { structure, payload } } = object;
-  if (structure !== 'individual') throw 'The structure needs to be individual';
-  const ids = shuffle(globalStructure.studentIds); // globalStructure.studentIds;
-  if (ids.length === 1) return { group: { '1': new Array(ids[0]) } };
-  const students = dataToArray(ids, payload);
-  const last = students.length % 2 ? students.pop() : null;
+  console.log('OP-ARGUE')
+  console.log(object)
+  const { activityData: { payload } } = object;
+  const { instances, distanceMatrix } = payload.all.data
 
-  const distances = computeDist(students);
-  const tmp = chunk([...students.keys()], 2);
-  let modified = false;
+  console.log('STEP1')
 
-  do {
+  if (instances.length === 1) return { group: { '1': [ instances[0] ] } };
+
+  const last = instances.length % 2 ? instances.pop() : null;
+
+  const tmp = chunk([...instances.keys()], 2);
+
+  console.log(tmp)
+  console.log('STEP2')
+
+  let modified = true
+  while (modified) {
     modified = false;
-    for (let i = 0; i < tmp.length && !modified; i = 1 + i) {
-      const dP1 = distances[tmp[i][0]][tmp[i][1]];
 
-      for (let j = 0; j < tmp.length && !modified; j = 1 + j) {
+
+    for (let i = 0; i < tmp.length && !modified; i = 1 + i) {
+
+
+      for (let j = i+1; j < tmp.length && !modified; j = 1 + j) {
         if (
-          Math.min(
-            distances[tmp[i][0]][tmp[j][0]],
-            distances[tmp[i][1]][tmp[j][1]]
-          ) > dP1
+          optim([distanceMatrix[tmp[i][0]][tmp[j][0]], distanceMatrix[tmp[i][1]][tmp[j][1]]] > dP1
         ) {
           const k = tmp[i][1];
           tmp[i][1] = tmp[j][0];
@@ -70,8 +51,8 @@ const operator = (configData, object) => {
           modified = true;
         } else if (
           Math.min(
-            distances[tmp[i][0]][tmp[j][1]],
-            distances[tmp[i][1]][tmp[j][0]]
+            distanceMatrix[tmp[i][0]][tmp[j][1]],
+            distanceMatrix[tmp[i][1]][tmp[j][0]]
           ) > dP1
         ) {
           const k = tmp[i][1];
@@ -80,60 +61,25 @@ const operator = (configData, object) => {
           modified = true;
         }
       }
+
+
+
+
     }
-  } while (modified);
+  };
 
   if (last) {
-    let maxDif = 0;
-    let maxDist = 0;
-    let maxInd = 0;
-
-    for (let i = 0; i < tmp.length; i += 1) {
-      const tmp1 = dist(students[tmp[i][0]], last);
-      const tmp2 = dist(students[tmp[i][1]], last);
-      const tmp3 = new Set(tmp2);
-      tmp1.forEach(x => tmp3.add(x));
-      if (
-        tmp3.size >= maxDif &&
-        (tmp3.size > maxDif || tmp1.size + tmp2.size > maxDist)
-      ) {
-        maxDif = tmp3.size;
-        maxDist = tmp1.size + tmp2.size;
-        maxInd = i;
-      }
-    }
-    tmp[maxInd].push(students.length);
+    tmp[0].push(instances.length-1);
   }
 
   const result = { group: {} };
 
   for (let i = 0; i < tmp.length; i = 1 + i) {
     tmp[i].sort((a, b) => a - b);
-    result.group[(i + 1).toString()] = tmp[i].map(x => ids[x]);
+    result.group[(i + 1).toString()] = tmp[i].map(x => instances[x]);
   }
   return result;
 };
-
-const dataToArray = (ids, payload) => ids.map(id => flatten(payload[id].data));
-
-export const computeDist = (tab: Array<Object>): Array<Array<number>> => {
-  const result = [];
-
-  for (let i = 0; i < tab.length; i = 1 + i) {
-    result[i] = [];
-    for (let j = 0; j <= i; j = 1 + j) {
-      const tmp = dist(tab[i], tab[j]).size;
-      result[i][j] = tmp;
-      result[j][i] = tmp;
-    }
-  }
-  return result;
-};
-
-export const dist = (A: Object, B: Object): Set<string> =>
-  Object.keys(Object.assign({}, A, B))
-    .filter(k => A[k] !== undefined && B[k] !== undefined && A[k] !== B[k])
-    .reduce((acc, k) => acc.add(k), new Set());
 
 export default ({
   id: 'op-argue',
@@ -143,3 +89,59 @@ export default ({
   meta,
   outputDefinition: ['group']
 }: socialOperatorT);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function s_DataCollectionCreation(){
+  sumup = {}
+  for (let i = 0; i < s_analyticsDomain.length-1; i++) {
+    if(!sumup[s_analyticsDomain[i]]) {
+      sumup[s_analyticsDomain[i]] = {
+        nameAnalyticsDomain: s_analyticsDomain[i],
+        ShortTitles: {
+          [s_shortTitle[i]]: {
+            nameShortTitle: s_shortTitle[i],
+            DPIDS: [s_DPID[i]]
+          }
+        }
+      }
+    } else {
+      if(!sumup[s_analyticsDomain[i]].ShortTitles[s_shortTitle[i]]) {
+        sumup[s_analyticsDomain[i]].ShortTitles[s_shortTitle[i]] = {
+          nameShortTitle: s_shortTitle[i],
+          DPIDS: [s_DPID[i]]
+        }
+      } else {
+        sumup[s_analyticsDomain[i]].ShortTitles[s_shortTitle[i]].DPIDs.push(s_DPID[i])
+      }
+    }
+  }
+}
