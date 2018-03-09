dd87d13249c8374327a3b58f8c3d1ac3041f79ed
lfaucon <lpfaucon@gmail.com>
2018-03-09 09:40:28 +0100

    Working op-argue

--- op/op-argue/src/__tests__/index.js
+++ op/op-argue/src/__tests__/index.js
@@ -51,6 +51,17 @@ const test3 = wrap(
   ]
 )
 
+const test4 = wrap(
+  ['s1','s2','s3','s4','s5'],
+  [
+    [0, 0, 0, 5, 3],
+    [0, 0, 0, 3, 5],
+    [0, 0, 0, 3, 3],
+    [5, 3, 3, 0, 0],
+    [3, 5, 3, 0, 0],
+  ]
+)
+
 test('test 1',
   () => expect(
     operator({},test1)
@@ -66,5 +77,11 @@ test('test 2',
 test('test 3',
   () => expect(
     operator({},test3)
-  ).toEqual({ group: { '1': ['s1','s4'], '2': ['s2','s5'], '3': ['s3','s6'] } })
+  ).toEqual({ group: { '1': ['s1','s4'], '2': ['s3','s6'], '3': ['s2','s5'] } })
+);
+
+test('test 4',
+  () => expect(
+    operator({},test4)
+  ).toEqual({ group: { '1': ['s1','s4','s5'], '2': ['s2','s3'] } })
 );
--- op/op-argue/src/index.js
+++ op/op-argue/src/index.js
@@ -1,6 +1,6 @@
 // @flow
 
-import { shuffle, chunk } from 'lodash';
+import { chunk } from 'lodash';
 import type { socialOperatorT } from 'frog-utils';
 
 const meta = {
@@ -17,12 +17,8 @@ const config = {
 const optim = (values) => values.reduce((acc,x) => acc + Math.sqrt(x), 0)
 
 const operator = (configData, object) => {
-  console.log('OP-ARGUE')
-  console.log(object)
   const { activityData: { payload } } = object;
   const { instances, distanceMatrix } = payload.all.data
-
-  console.log('STEP1')
 
   if (instances.length === 1) return { group: { '1': [ instances[0] ] } };
 
@@ -30,30 +26,32 @@ const operator = (configData, object) => {
 
   const tmp = chunk([...instances.keys()], 2);
 
-  console.log(tmp)
-  console.log('STEP2')
-
   let modified = true
-  while (modified) {
+  let iter = 0
+  while (modified && iter < 10000) {
+    iter += 1;
     modified = false;
-
-
     for (let i = 0; i < tmp.length && !modified; i = 1 + i) {
-
-
       for (let j = i+1; j < tmp.length && !modified; j = 1 + j) {
+        const currentScore = optim([
+          distanceMatrix[tmp[i][0]][tmp[i][1]],
+          distanceMatrix[tmp[j][0]][tmp[j][1]]
+        ])
         if (
-          optim([distanceMatrix[tmp[i][0]][tmp[j][0]], distanceMatrix[tmp[i][1]][tmp[j][1]]] > dP1
+          optim([
+            distanceMatrix[tmp[i][0]][tmp[j][0]],
+            distanceMatrix[tmp[i][1]][tmp[j][1]]
+          ]) > currentScore
         ) {
           const k = tmp[i][1];
           tmp[i][1] = tmp[j][0];
           tmp[j][0] = k;
           modified = true;
         } else if (
-          Math.min(
+          optim([
             distanceMatrix[tmp[i][0]][tmp[j][1]],
             distanceMatrix[tmp[i][1]][tmp[j][0]]
-          ) > dP1
+          ]) > currentScore
         ) {
           const k = tmp[i][1];
           tmp[i][1] = tmp[j][1];
@@ -61,22 +59,18 @@ const operator = (configData, object) => {
           modified = true;
         }
       }
-
-
-
-
     }
   };
 
   if (last) {
-    tmp[0].push(instances.length-1);
+    tmp[0].push(instances.length);
   }
 
   const result = { group: {} };
 
   for (let i = 0; i < tmp.length; i = 1 + i) {
     tmp[i].sort((a, b) => a - b);
-    result.group[(i + 1).toString()] = tmp[i].map(x => instances[x]);
+    result.group[(i + 1).toString()] = tmp[i].map(x => instances[x] || last);
   }
   return result;
 };
@@ -89,59 +83,3 @@ export default ({
   meta,
   outputDefinition: ['group']
 }: socialOperatorT);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function s_DataCollectionCreation(){
-  sumup = {}
-  for (let i = 0; i < s_analyticsDomain.length-1; i++) {
-    if(!sumup[s_analyticsDomain[i]]) {
-      sumup[s_analyticsDomain[i]] = {
-        nameAnalyticsDomain: s_analyticsDomain[i],
-        ShortTitles: {
-          [s_shortTitle[i]]: {
-            nameShortTitle: s_shortTitle[i],
-            DPIDS: [s_DPID[i]]
-          }
-        }
-      }
-    } else {
-      if(!sumup[s_analyticsDomain[i]].ShortTitles[s_shortTitle[i]]) {
-        sumup[s_analyticsDomain[i]].ShortTitles[s_shortTitle[i]] = {
-          nameShortTitle: s_shortTitle[i],
-          DPIDS: [s_DPID[i]]
-        }
-      } else {
-        sumup[s_analyticsDomain[i]].ShortTitles[s_shortTitle[i]].DPIDs.push(s_DPID[i])
-      }
-    }
-  }
-}
