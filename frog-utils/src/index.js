ee098cb80bcb4f741509593c9bdb26c718b23f41
Stian Håklev <shaklev@gmail.com>
2018-04-16 16:07:10 +0200

    WIP

--- frog-utils/src/index.js
+++ frog-utils/src/index.js
@@ -217,3 +217,29 @@ export const getInitialState = (activities: Object, d: number = 1) => {
         second: getInitialState(activities.slice(n, activities.length), -d)
       };
 };
+
+export const cloneDeep = (o: any) => {
+  let newO;
+  let i;
+
+  if (typeof o !== 'object') return o;
+
+  if (!o) return o;
+  if (o instanceof Date) return new Date(o.valueOf());
+  if (Object.prototype.toString.apply(o) === '[object Array]') {
+    newO = [];
+    for (i = 0; i < o.length; i += 1) {
+      newO[i] = cloneDeep(o[i]);
+    }
+    return newO;
+  }
+
+  newO = {};
+  // eslint-disable-next-line no-restricted-syntax
+  for (i in o) {
+    if (Object.prototype.hasOwnProperty.call(o, i)) {
+      newO[i] = cloneDeep(o[i]);
+    }
+  }
+  return newO;
+};
