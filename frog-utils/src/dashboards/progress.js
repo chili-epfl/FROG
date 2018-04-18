095f9883b878f28182711a7fbaeed8e05b106b30
Jiaxi Gu <jiaxi.gu@epfl.ch>
2018-04-18 21:22:40 +0200

    fix lint

--- frog-utils/src/dashboards/progress.js
+++ frog-utils/src/dashboards/progress.js
@@ -2,7 +2,6 @@
 /* eslint-disable react/no-array-index-key */
 
 import * as React from 'react';
-import { Chart } from 'react-google-charts';
 import { type LogDBT, type ActivityDbT, TimedComponent } from 'frog-utils';
 import regression from 'regression';
 import { VictoryChart, VictoryLine, VictoryTheme, VictoryLabel } from 'victory';
@@ -46,23 +45,24 @@ const prepareDataForDisplay = state => {
   const userResultObject = {};
   let currentMaxTime = 0;
   let finishedStudents = 0;
-  for (var user in state) {
+  for (let user in state) {
     var lastIndex = state[user].length - 1;
     const userMaxTime = state[user][lastIndex][1];
     if (userMaxTime > currentMaxTime) {
       currentMaxTime = userMaxTime;
     }
+    var userPredictedTime;
     if (state[user][lastIndex][0] === 1) {
       // already finished - use actual finish time
-      var userPredictedTime = state[user][lastIndex][1];
+      userPredictedTime = state[user][lastIndex][1];
       finishedStudents += 1;
     } else if (state[user].length < 3) {
       // finished only less than 2 task - not sufficient
-      var userPredictedTime = -1;
+      userPredictedTime = -1;
     } else {
       // finished multiple tasks - linear projection
       const userResult = regression.linear(state[user]);
-      var userPredictedTime =
+      userPredictedTime =
         userResult.equation[0] * 1 + userResult.equation[1];
       userResultObject[user] = userResult.equation;
     }
@@ -83,10 +83,10 @@ const prepareDataForDisplay = state => {
   ) {
     const filtered = predictedTime.filter(value => value <= t);
     const progress = [];
-    for (var user in state) {
+    for (let user in state) {
       const filteredProgress = state[user].filter(value => value[1] <= t);
       if (filteredProgress.length != 0) {
-        var lastIndex = filteredProgress.length - 1;
+        let lastIndex = filteredProgress.length - 1;
         progress.push(filteredProgress[lastIndex][0]);
       } else {
         progress.push(0);
