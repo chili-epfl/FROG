ceebc81ccca688da7eeb9686dcb2a68f3dba6d69
lfaucon <lpfaucon@gmail.com>
2018-04-23 19:58:09 +0200

    Deepscan

--- frog/imports/ui/GraphEditor/SidePanel/ActivityPanel/ChooseActivity.js
+++ frog/imports/ui/GraphEditor/SidePanel/ActivityPanel/ChooseActivity.js
@@ -1,7 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import { type ActivityPackageT, type ActivityDbT } from 'frog-utils';
-import { activityTypes, activityTypesObj } from '/imports/activityTypes';
+import { activityTypes } from '/imports/activityTypes';
 import { addActivity } from '/imports/api/activities';
 import { Button } from 'react-bootstrap';
 import jsonSchemaDefaults from 'json-schema-defaults';
