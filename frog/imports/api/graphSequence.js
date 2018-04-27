98a6d997f54881d8400207b07d31bcbb73ecf053
Stian Håklev <shaklev@gmail.com>
2018-04-27 10:46:26 +0200

    WIP

--- frog/imports/api/graphSequence.js
+++ frog/imports/api/graphSequence.js
@@ -1,10 +1,13 @@
 // @flow
 
 import { type ActivityDbT } from 'frog-utils';
+import { Graphs } from './graphs';
+import { Sessions } from './sessions';
 
 export const calculateNextOpen = (
   timeInGraph: number,
-  activities: ActivityDbT[]
+  activities: ActivityDbT[],
+  sessionId?: string
 ): [number, ActivityDbT[]] => {
   const [t0, t1] = [
     ...new Set(
@@ -16,13 +19,25 @@ export const calculateNextOpen = (
   ]
     .sort((a, b) => a - b)
     .slice(0, 2);
-  const newTimeInGraph = t1 ? (t0 + t1) / 2 : -1;
+  if (t1) {
+    const newTimeInGraph = (t0 + t1) / 2;
 
-  const openActivities = activities.filter(
-    a =>
-      a.startTime <= newTimeInGraph && a.startTime + a.length > newTimeInGraph
-  );
-  return [newTimeInGraph, openActivities];
+    const openActivities = activities.filter(
+      a =>
+        a.startTime <= newTimeInGraph && a.startTime + a.length > newTimeInGraph
+    );
+    return [newTimeInGraph, openActivities];
+  } else {
+    if (!sessionId) {
+      return [-1, []];
+    }
+    const graph = Graphs.findOne(Sessions.findOne(sessionId).graphId);
+    const newTime =
+      (Math.max(...activities.map(x => x.startTime + x.length)) +
+        graph.duration) /
+      2;
+    return [newTime, []];
+  }
 };
 
 export const getActivitySequence = (
@@ -33,14 +48,13 @@ export const getActivitySequence = (
   let c = 0;
   while (timeInGraph !== -1) {
     c += 1;
-    const [nt, open] = calculateNextOpen(timeInGraph || -1, activities);
+    const [_, open] = calculateNextOpen(timeInGraph || -1, activities);
     // eslint-disable-next-line no-loop-func
     open.forEach(x => {
       if (!activitySeq[x._id]) {
         activitySeq[x._id] = c;
       }
     });
-    timeInGraph = nt;
   }
   return activitySeq;
 };
