0918dbe17f37e2b57a4a44710faf41844457840b
Marin Luketin <marin.luketin@gmail.com>
2018-05-03 17:00:07 +0200

    Add reload stream, toogle audio video in participant

--- ac/ac-videochat/src/ActivityRunner/participant.js
+++ ac/ac-videochat/src/ActivityRunner/participant.js
@@ -1,163 +1,130 @@
-function Participant(name, id, sendMessageF) {
+function Participant(name, id, sendMessage) {
     this.name = name;
     this.id = id;
-    var rtcPeer;
-    var sendMessage = sendMessageF;
-	var isSendOnly = null;
+	var rtcPeer;
+	this.options;
+	this.mode;
 	var self = this;
 
-	console.log("Created Particiapnt " + name + " " + id);
+	var createPeer = (mode, options) => {
+		if(!this.mode) {
+			this.mode = mode;
+		}
+		if(!this.options) {
+			this.options = options;
+		} 
+		console.log(options);
+		try {
+			if(mode === "sendOnly" && options && options.onaddstream) {
+				options.onaddstream(options.myStream);
+			}
 
-	this.createSendOnlyPeer = options => {
-		this.isSendOnly = true;
-		console.log("CreteSendOnlyPeer()");
-		var self = this;
-		navigator.mediaDevices.getUserMedia(options.userMediaConstraints)
-		.then(myStream => {
-			try {
-				if(options && options.onAddLocalStream){
-					options.onAddLocalStream(myStream);
-				}
-	
-				rtcPeer = new RTCPeerConnection(options.configuration);
-				rtcPeer.addStream(myStream);
-				rtcPeer.onicecandidate = self.onIceCandidate;
-	
-				rtcPeer.createOffer((offer) => {
-					console.log("Created offer for " + self.name);
-					
-					rtcPeer.setLocalDescription(offer);
-					var msg =  { 
-						id : "receiveVideoFrom",
-						sender : id,
+			rtcPeer = new RTCPeerConnection(options.configuration);
+			rtcPeer.onicecandidate = self.onIceCandidate;
+
+		
+			if(mode === "sendOnly") {
+				rtcPeer.addStream(options.myStream);
+			} else if(mode === "recvOnly" && options && options.onaddstream){
+				rtcPeer.onaddstream = options.onaddstream;
+			}	
+			
+
+			rtcPeer.createOffer((offer) => {				
+				rtcPeer.setLocalDescription(offer);
+				var msg =  { 
+					id : "receiveVideoFrom",
+					userId: id,
+					sdpOffer : offer.sdp
+				};
+				if(options && options.reload) {
+					msg =  { 
+						id : "reloadStreamFrom",
 						userId: id,
 						sdpOffer : offer.sdp
 					};
-					sendMessage(msg);
-				}, (error) => {
-	
-					console.error("CreateOffer error ", error);
-				},{
-					mandatory: {
-						OfferToReceiveVideo: false
-						,OfferToReceiveAudio: false
-					}
-				}); 
-			} catch (error) {
-				console.log("Error in create Send only peer after obtaining media", error);
-			}	
+				}
+				sendMessage(msg);
+			}, (error) => {
+				if(options && options.onOfferError) options.onOfferError(error);
+			},
+				options.offerConstraints
+			); 
+		} catch (error) {
+			if(options && options.onError) options.onError(error);
+		}	
+	}
+
+	this.createSendOnlyPeer = options => {
+		//TODO throw error if options does not containt required constraints?
+
+		navigator.mediaDevices.getUserMedia(options.userMediaConstraints)
+		.then(myStream => {
+			options.myStream = myStream;
+			createPeer("sendOnly", options);
 		})
 		.catch(error => {
-            console.log("Media already in use, or blocked");
-            console.log(error);
+			if(options && options.onUserMediaError) options.onUserMediaError(error);
         });
 	}
 
 	this.createRecvOnlyPeer = options => {
-		this.isSendOnly = false;
-		console.log("CreteRecvOnlyPeer()");
-		var self = this;
-
-		rtcPeer = new RTCPeerConnection(options.configuration);
-		rtcPeer.onicecandidate = self.onIceCandidate;
-		rtcPeer.onaddstream = options.onaddstream;
-
-		rtcPeer.createOffer((offer) => {
-			console.log("Created offer (recvonly) for " + this.name);
-			//console.log(offerSdp.sdp);
-
-			rtcPeer.setLocalDescription(offer);
-			console.log("Created offer");
-			var msg =  { 
-				id : "receiveVideoFrom",
-				sender : name,
-				userId: id,
-				sdpOffer : offer.sdp
-			};
-			if(options) {
-				if(options.reload){
-					msg =  { 
-						id : "reloadStreamFrom",
-						sender : name,
-						userId: id,
-						sdpOffer : offer.sdp
-					};
-				}
-			} 
-			sendMessage(msg);
-		}, (error) => {
-
-			console.error("CreateOffer error ", error);
-		}, {
-			mandatory: {
-				OfferToReceiveVideo: true
-				,OfferToReceiveAudio: true
-			}
-		});
+		createPeer("recvOnly", options);
 	}
 
-	function useH264(offer){
-		offer.sdp = offer.sdp.replace(/VP8/g, "H264");
-		return offer;
+	this.reloadStream = () => {
+		this.dispose();
+
+		var options = this.options;
+		options.reload = true;
+		createPeer(this.mode, options);
 	}
 
-	this.processAnswer = function(_answer) {
-		console.log("Answer from " + this.name);
-		//console.log(_answer);
+	this.processAnswer = function(answerSdp) {
 		var answer = new RTCSessionDescription({
 			type: 'answer',
-			sdp: _answer
+			sdp: answerSdp
 		});
-		console.log("Setting answer");
 		rtcPeer.setRemoteDescription(answer);
 	}
 
+	//can throw exception
 	this.onRemoteCandidate = function(candidate){
-		//console.log(candidate);
-		try {
-			rtcPeer.addIceCandidate(new RTCIceCandidate(candidate));
-
-		} catch (error) {
-			console.log("ERROR");
-			console.error(error);
-		}
-	}
-
-	this.offerToReceiveVideo = function(error, offerSdp, wp){
-		if (error) return console.error ("sdp offer error")
-		console.log('Invoking SDP offer callback function');
-		var msg =  { 
-			id : "receiveVideoFrom",
-			sender : name,
-			userId: id,
-			sdpOffer : offerSdp
-		};
-		sendMessage(msg);
+		rtcPeer.addIceCandidate(new RTCIceCandidate(candidate));
 	}
 
 	this.onIceCandidate = function (candidate) {
-		var self = this;
-
-		try {
-			if(event) {
-				if(event.candidate) {
-					var message = {
-						id: 'onIceCandidate',
-						candidate: event.candidate,
-						name: name,
-						userId: id
-					};
-					sendMessage(message);
-				}
-			}
-		} catch (error) {
-			console.log("ERROR on ice candidate");
-			console.error(error);
+		if(event && event.candidate) {
+			var message = {
+				id: 'onIceCandidate',
+				candidate: event.candidate,
+				userId: id
+			};
+			sendMessage(message);
 		}
 	}
 
+	this.toogleAudio = function(sendAudio) {
+		var audioTracks = rtcPeer.getLocalStreams()[0].getAudioTracks()[0];
+		audioTracks.enabled = !audioTracks.enabled;
+	}
+
+	this.toogleVideo = function(sendVideo) {
+		var videoTrack = rtcPeer.getLocalStreams()[0].getVideoTracks()[0];
+		videoTrack.enabled = !videoTrack.enabled;
+	}
+
+	this.isAudioEnabled = function() {
+		var audioTracks = rtcPeer.getLocalStreams()[0].getAudioTracks()[0];
+		return audioTracks.enabled;
+	}
+
+	this.isVideoEnabled = function() {
+		var videoTrack = rtcPeer.getLocalStreams()[0].getVideoTracks()[0];
+		return videoTrack.enabled;
+	}
+	 
 	this.dispose = function() {
-		console.log('Disposing participant ' + this.name);
 		this.disposeRtcPeer();
 	};
 
