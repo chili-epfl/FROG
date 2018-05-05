aeef70cbf61efb2fc90a4fab09b323ec098fe38a
Stian Håklev <shaklev@gmail.com>
2018-05-06 01:47:54 +0200

    Update ActivityRunner.jsx

--- ac/ac-train/src/ActivityRunner.jsx
+++ ac/ac-train/src/ActivityRunner.jsx
@@ -100,10 +100,6 @@ class Main extends React.Component<ActivityRunnerPropsT & { classes: any }> {
   }
 }
 
-const f = (props: { x?: string, name?: { first?: string, last?: string } }) => {
-  console.log(props.name?.first);
-};
-
 // the actual component that the student sees
 const RunnerController = props => {
   const {
