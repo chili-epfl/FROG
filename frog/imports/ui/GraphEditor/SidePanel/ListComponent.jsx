3c66091e93666ea8f1f8c6a66ed9d3a5832c260f
prastut <kr.prastut@gmail.com>
2018-05-07 20:12:22 +0200

    final-polishing

--- frog/imports/ui/GraphEditor/SidePanel/ListComponent.jsx
+++ frog/imports/ui/GraphEditor/SidePanel/ListComponent.jsx
@@ -1,23 +1,33 @@
 // @flow
 
 import React from 'react';
-import { Button } from 'react-bootstrap';
-import { withStyles } from 'material-ui/styles';
+
+import Grid from 'material-ui/Grid';
 import IconButton from 'material-ui/IconButton';
-import List, {
-  ListItem,
-  ListItemAvatar,
-  ListItemIcon,
-  ListItemSecondaryAction,
-  ListItemText
-} from 'material-ui/List';
-import Divider from 'material-ui/Divider';
-import CheckCircle from '@material-ui/icons/CheckCircle';
+import { withStyles } from 'material-ui/styles';
+import { ListItem, ListItemSecondaryAction } from 'material-ui/List';
 import RemoveRedEye from '@material-ui/icons/RemoveRedEye';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import Typography from 'material-ui/Typography';
 
 import { Highlight } from 'frog-utils';
 
-export default ({
+const styles = {
+  iconButtonRoot: {
+    height: '25px',
+    width: '25px'
+  },
+  iconRoot: {
+    width: '0.8em',
+    height: '0.8em'
+  },
+  secondaryText: {
+    color: 'rgba(0, 0, 0, 0.54)'
+  }
+};
+
+const ListItems = ({
+  classes,
   object,
   showExpanded,
   onSelect,
@@ -26,27 +36,80 @@ export default ({
   onPreview,
   searchS
 }: any) => (
-  <List>
-    <ListItem button value={object.id} onClick={onSelect}>
-      <ListItemText
-        secondary={
+  <ListItem button value={object.id} onClick={onSelect}>
+    <Grid container>
+      <Grid item xs={12}>
+        <Typography variant="subheading" gutterBottom>
+          <Highlight text={object.meta.name} searchStr={searchS} />
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography gutterBottom className={classes.secondaryText}>
           <Highlight text={object.meta.shortDesc} searchStr={searchS} />
-        }
-      >
-        <Highlight text={object.meta.name} searchStr={searchS} />
-      </ListItemText>
-
-      <ListItemSecondaryAction>
-        {hasPreview && (
-          <IconButton
-            value={object.id}
-            aria-label="Preview"
-            onClick={onPreview}
+        </Typography>
+      </Grid>
+      {showExpanded && (
+        <Grid item xs={12}>
+          <Typography
+            variant="caption"
+            gutterBottom
+            className={classes.secondaryText}
           >
-            <RemoveRedEye />
-          </IconButton>
-        )}
-      </ListItemSecondaryAction>
-    </ListItem>
-  </List>
+            <i>
+              <Highlight text={object.meta.description} searchStr={searchS} />
+            </i>
+          </Typography>
+        </Grid>
+      )}
+    </Grid>
+
+    {/* <ListItemText
+      secondary={<Highlight text={object.meta.shortDesc} searchStr={searchS} />}
+    >
+      
+    </ListItemText> */}
+
+    <ListItemSecondaryAction>
+      <Grid container direction="column" justify="center">
+        <Grid item xs={6}>
+          {hasPreview && (
+            <IconButton
+              value={object.id}
+              aria-label="Preview"
+              onClick={onPreview}
+              classes={{
+                root: classes.iconButtonRoot
+              }}
+            >
+              <RemoveRedEye
+                classes={{
+                  root: classes.iconRoot
+                }}
+              />
+            </IconButton>
+          )}
+        </Grid>
+        <Grid item xs={6}>
+          {!showExpanded && (
+            <IconButton
+              value={object.id}
+              aria-label="Preview"
+              onClick={expand}
+              classes={{
+                root: classes.iconButtonRoot
+              }}
+            >
+              <ExpandMore
+                classes={{
+                  root: classes.iconRoot
+                }}
+              />
+            </IconButton>
+          )}
+        </Grid>
+      </Grid>
+    </ListItemSecondaryAction>
+  </ListItem>
 );
+
+export default withStyles(styles)(ListItems);
