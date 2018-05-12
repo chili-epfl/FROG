9862c9d343a9e754ab61e006bdf50ea62be833f5
Stian Håklev <shaklev@gmail.com>
2018-05-12 12:47:51 +0200

    WIP

---
+++ frog/babel.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  plugins: [
+    '@babel/plugin-transform-flow-strip-types',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    [
+      '@babel/plugin-proposal-class-properties',
+      {
+        loose: true
+      }
+    ],
+    '@babel/plugin-proposal-optional-chaining',
+    [
+      'captains-log',
+      {
+        methods: ['debug', 'error', 'exception', 'log', 'warn']
+      }
+    ]
+  ],
+  env: {
+    test: {
+      presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+        '@babel/preset-stage-0'
+      ]
+    }
+  }
+};
