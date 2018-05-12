505156d809fedc9912193a4a48efc5eef518293e
Stian Håklev <shaklev@gmail.com>
2018-05-12 12:33:34 +0200

    WIP

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    '@babel/plugin-transform-flow-strip-types',
+    '@babel/plugin-proposal-optional-chaining',
+    [
+      'captains-log',
+      {
+        methods: ['debug', 'error', 'exception', 'log', 'warn']
+      }
+    ]
+  ],
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    ['@babel/preset-stage-0', { decoratorsLegacy: true }]
+  ]
+};
