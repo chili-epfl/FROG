cdc49adcda4e74e69dde80b8cb70ef88105f9452
Stian Håklev <shaklev@gmail.com>
2018-05-14 22:10:40 +0200

    WIP

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: [
     '@babel/plugin-transform-flow-strip-types',
-    '@babel/plugin-proposal-optional-chaining'
+    '@babel/plugin-proposal-optional-chaining',
+    'transform-decorators-legacy'
   ],
   presets: [
     '@babel/preset-env',
--- frog/babel.config.js
+++ frog/babel.config.js
@@ -15,7 +15,7 @@ module.exports = {
       presets: [
         '@babel/preset-env',
         '@babel/preset-react',
-        '@babel/preset-stage-0'
+        ['@babel/preset-stage-0', { decoratorsLegacy: true }]
       ]
     }
   }
