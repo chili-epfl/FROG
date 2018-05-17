2462b35dfc891943a89736b13564be74e4aa3d58
Stian Håklev <shaklev@gmail.com>
2018-05-17 20:24:50 +0200

    Videochat (#1157)

--- ac/ac-videochat/src/ActivityRunner/index.js
+++ ac/ac-videochat/src/ActivityRunner/index.js
@@ -1,8 +1,8 @@
 // @flow
 
+/* eslint-disable no-alert */
 import React, { Component } from 'react';
-import type { ActivityRunnerPropsT } from 'frog-utils';
-import 'webrtc-adapter';
+import { type ActivityRunnerPropsT, values } from 'frog-utils';
 
 import WebRtcConfig from '../webrtc-config/config';
 import BrowserUtils from '../utils/browser';
@@ -12,6 +12,10 @@ import Header from './Header';
 import VideoLayout from './VideoLayout';
 
 import Participant from './participant';
+
+declare var RTCIceCandidate: any;
+declare var RTCPeerConnection: any;
+declare var RTCSessionDescription: any;
 
 /**
  * State consists of local and remote
@@ -31,6 +35,7 @@ type StateT = {
   local: Object,
   remote: Array<any>
 };
+type OptionsT = { myStream?: *, ontrack?: *, configuration: * };
 
 class ActivityRunner extends Component<ActivityRunnerPropsT, StateT> {
   name: string;
@@ -43,6 +48,8 @@ class ActivityRunner extends Component<ActivityRunnerPropsT, StateT> {
     browser: string,
     version: number
   };
+  activityType: string;
+  role: string;
 
   constructor(props: ActivityRunnerPropsT) {
     super(props);
@@ -54,9 +61,26 @@ class ActivityRunner extends Component<ActivityRunnerPropsT, StateT> {
     this.name = this.props.userInfo.name;
     this.id = this.props.userInfo.id;
     this.browser = BrowserUtils.detectBrowser();
+    this.activityType = this.props.activityData.config.activityType;
+    if (!this.props.activityData.config.userMediaConstraints.audio) {
+      WebRtcConfig.mediaConstraints.audio = false;
+    }
+    if (!this.props.activityData.config.userMediaConstraints.video) {
+      WebRtcConfig.mediaConstraints.video = false;
+    }
 
     // TODO, change in the future with activity ID + instanceId
     this.roomId = this.props.sessionId + this.props.groupingValue;
+
+    if (this.activityType === 'many2many') {
+      this.role = 'none';
+    } else if (this.activityType === 'one2many') {
+      if (this.name === 'teacher') {
+        this.role = 'teacher';
+      } else {
+        this.role = 'watcher';
+      }
+    }
 
     this.createWebSocketConnection();
   }
@@ -68,9 +92,26 @@ class ActivityRunner extends Component<ActivityRunnerPropsT, StateT> {
   createWebSocketConnection = () => {
     this.ws = new WebSocket(WebRtcConfig.signalServerURL);
 
+    this.ws.onerror = error => {
+      alert(
+        "If you have AdBlock, it might be blocking connections, please put FROG on AdBlock's whitelist"
+      );
+      console.error(error);
+    };
+
     this.ws.onopen = _ => {
       // when web socket connection is oppened, register on signal server
-      this.requestMediaDevices();
+      if (this.role === 'watcher') {
+        if (this.browser.browser === 'safari') {
+          // safari has a bug where you cannot receive streams if you don't allow media devices
+          // this code should be updated once safari fixes that problem
+          this.requestMediaDevices(this.role);
+        } else {
+          this.register(this.name, this.id, this.roomId, 'watcher');
+        }
+      } else {
+        this.requestMediaDevices(this.role);
+      }
     };
 
     this.ws.onmessage = (message: any) => {
@@ -81,7 +122,11 @@ class ActivityRunner extends Component<ActivityRunnerPropsT, StateT> {
           this.onExistingParticipants(parsedMessage);
           break;
         case 'newParticipantArrived':
-          this.onNewParticipant(parsedMessage.name, parsedMessage.userId);
+          this.onNewParticipant(
+            parsedMessage.name,
+            parsedMessage.userId,
+            parsedMessage.role
+          );
           break;
         case 'participantLeft':
           this.onParticipantLeft(parsedMessage.userId);
@@ -107,20 +152,83 @@ class ActivityRunner extends Component<ActivityRunnerPropsT, StateT> {
     this.ws.send(JSON.stringify(message));
   };
 
-  requestMediaDevices = () => {
+  requestMediaDevices = (role: string) => {
     if (navigator.mediaDevices)
       navigator.mediaDevices
         .getUserMedia(WebRtcConfig.mediaConstraints)
         .then(myStream => {
           this.stream = myStream;
-          this.register(this.name, this.id, this.roomId, 'none');
+          this.register(this.name, this.id, this.roomId, role);
         })
         .catch(error => {
-          console.info(
-            'User blocked media devices, there are no devices or are already in use',
-            error
-          );
-          this.register(this.name, this.id, this.roomId, 'watcher');
+          console.error('Error happened when requesting user media, ', error);
+          switch (error.name) {
+            case 'NotAllowedError':
+              if (role === 'teacher') {
+                alert(
+                  'Your students cannot see/hear you if you do not allow camera/microphone. ' +
+                    'Press F5 to refresh the page and allow camera/microphone'
+                );
+              } else {
+                if (this.browser.browser !== 'safari') {
+                  alert(
+                    'Safari has an issue where you cannot see and hear other users unless ' +
+                      'you allow application to use camera/microphone. '
+                  );
+                }
+                this.register(this.name, this.id, this.roomId, 'watcher');
+              }
+              break;
+
+            case 'NotReadableError':
+              alert(
+                'Your camera and microphone are already being used by another application.'
+              );
+              this.register(this.name, this.id, this.roomId, 'watcher');
+              break;
+
+            case 'NotFoundError':
+              alert('Camera and microphon not found on your computer');
+              this.register(this.name, this.id, this.roomId, 'watcher');
+              break;
+
+            case 'OverconstrainedError':
+              alert(
+                'Your camera/microphone do not meet the constraints requested by this application.'
+              );
+              this.register(this.name, this.id, this.roomId, 'watcher');
+              console.error(error);
+              break;
+
+            case 'TypeError':
+              alert(
+                'Application requested your camera/mic with illegal constraints'
+              );
+              this.register(this.name, this.id, this.roomId, 'watcher');
+              break;
+
+            case 'SecurityError':
+              alert(
+                'User media support is disabled on the Document which requested your camera/mic.'
+              );
+              this.register(this.name, this.id, this.roomId, 'watcher');
+              console.error(error);
+              break;
+
+            case 'AbortError':
+              alert(
+                'Unknown error appeared that prevents usage of your camera/microphone'
+              );
+              this.register(this.name, this.id, this.roomId, 'watcher');
+              console.error(error);
+              break;
+
+            default:
+              console.error(
+                'Error happened when requesting user media, ',
+                error
+              );
+          }
         });
   };
 
@@ -136,7 +244,9 @@ class ActivityRunner extends Component<ActivityRunnerPropsT, StateT> {
   };
 
   onExistingParticipants = msg => {
-    if (this.stream) {
+    // we chach if role is watcher because of bug in safari
+    // that condition should be removed once safari fixes recvonly connections
+    if (this.stream && this.role !== 'watcher') {
       const analysisOptions = {
         local: true,
         name: this.name,
@@ -155,56 +265,59 @@ class ActivityRunner extends Component<ActivityRunnerPropsT, StateT> {
         }
       });
 
-      const options = {
-        configuration: WebRtcConfig.rtcConfiguration,
-        myStream: this.stream,
-        offerConstraints: WebRtcConfig.sendOnlyOfferConstraintChrome
-      };
-
-      if (this.browser.browser === 'firefox') {
-        options.offerConstraints = WebRtcConfig.sendOnlyOfferConstraintFirefox;
-      }
-
-      // create new participant for send only my stream
-      const participant = new Participant(this.name, this.id, this.sendMessage);
-      this.participants[participant.id] = participant;
-      participant.createSendOnlyPeer(options);
+      this.createPeer('sendonly', this.name, this.id, this.role);
     }
 
     // for each of the existing participants, receive their video feed
     msg.data.forEach(this.receiveVideo);
   };
 
-  onNewParticipant = (name, userId) => {
-    this.receiveVideo({ name, id: userId });
+  createPeer = (mode, name, id, role) => {
+    const peerMode = {
+      mode
+    };
+    const participant = new Participant(name, id, role, this.sendMessage);
+    this.participants[participant.id] = participant;
+
+    if (role !== 'watcher') {
+      const onAddRemoteTrack = event => {
+        const stream = event.streams[0];
+        this.addRemoteUserToState(participant, stream);
+      };
+
+      const options: OptionsT = {
+        configuration: WebRtcConfig.rtcConfiguration
+      };
+
+      if (mode === 'sendonly') {
+        options.myStream = this.stream;
+        if (this.browser.browser !== 'chrome') {
+          peerMode.mode = 'sendrecv';
+        }
+      } else if (mode === 'recvonly') {
+        options.ontrack = onAddRemoteTrack;
+      }
+
+      participant.createPeer(peerMode.mode, options);
+    }
+  };
+
+  onNewParticipant = (name, userId, role) => {
+    this.receiveVideo({ name, id: userId, role });
   };
 
   // receive video from remote peer
-  receiveVideo = (newParticipant: { name: string, id: string }) => {
-    const participant = new Participant(
+  receiveVideo = (newParticipant: {
+    name: string,
+    id: string,
+    role: string
+  }) => {
+    this.createPeer(
+      'recvonly',
       newParticipant.name,
       newParticipant.id,
-      this.sendMessage
+      newParticipant.role
     );
-
-    this.participants[participant.id] = participant;
-
-    const onAddRemoteTrack = event => {
-      const stream = event.streams[0];
-      this.addRemoteUserToState(newParticipant, stream);
-    };
-
-    const options = {
-      ontrack: onAddRemoteTrack,
-      configuration: WebRtcConfig.rtcConfiguration,
-      offerConstraints: WebRtcConfig.recvOnlyOfferConstraintChrome
-    };
-
-    if (this.browser.browser === 'firefox') {
-      options.offerConstraints = WebRtcConfig.recvOnlyOfferConstraintFirefox;
-    }
-
-    participant.createRecvOnlyPeer(options);
   };
 
   addRemoteUserToState = (participant: Participant, stream: MediaStream) => {
@@ -252,7 +365,7 @@ class ActivityRunner extends Component<ActivityRunnerPropsT, StateT> {
       id: 'leaveRoom'
     });
 
-    Object.values(this.participants).forEach((p: Participant) => p.dispose());
+    values(this.participants).forEach((p: Participant) => p.dispose());
     this.ws.close();
   };
 
--- ac/ac-videochat/src/ActivityRunner/participant.js
+++ ac/ac-videochat/src/ActivityRunner/participant.js
@@ -1,3 +1,5 @@
+// @flow
+
 type OptionsT = {
   configuration: Object,
   myStream: MediaStream,
@@ -8,21 +10,28 @@ type OptionsT = {
   onError: Function
 };
 
+declare var RTCIceCandidate: any;
+declare var RTCPeerConnection: any;
+declare var RTCSessionDescription: any;
+
 class Participant {
   name: string;
   id: string;
+  role: string;
   sendMessage: Function;
   rtcPeer: RTCPeerConnection;
   options: OptionsT;
   mode: string;
+  isRemoteDescriptionSet: boolean;
 
-  constructor(name, id, sendMessage) {
+  constructor(name: string, id: string, role: string, sendMessage: Function) {
     this.name = name;
     this.id = id;
+    this.role = role;
     this.sendMessage = sendMessage;
   }
 
-  createPeer = (mode: string, options: OptionsT) => {
+  createPeer = (mode: string, options: Object) => {
     if (!this.mode) {
       this.mode = mode;
     }
@@ -32,45 +41,57 @@ class Participant {
     try {
       this.rtcPeer = new RTCPeerConnection(options.configuration);
       this.rtcPeer.onicecandidate = this.onIceCandidate;
-      if (mode === 'sendOnly') {
+      if (mode === 'sendonly' || mode === 'sendrecv') {
         this.rtcPeer.addStream(options.myStream);
-      } else if (mode === 'recvOnly' && options.ontrack) {
+      } else if (mode === 'recvonly' && options.ontrack) {
         this.rtcPeer.ontrack = options.ontrack;
       }
 
-      this.rtcPeer.createOffer(
-        offer => {
-          this.rtcPeer.setLocalDescription(offer);
-          let msg = {
-            id: 'receiveVideoFrom',
-            userId: this.id,
-            sdpOffer: offer.sdp
-          };
-          if (options && options.reload) {
-            msg = {
-              id: 'reloadStreamFrom',
-              userId: this.id,
-              sdpOffer: offer.sdp
-            };
-          }
-          this.sendMessage(msg);
-        },
-        error => {
-          if (options && options.onOfferError) options.onOfferError(error);
-        },
-        options.offerConstraints
-      );
+      const offerOptions = {
+        offerToReceiveAudio: 1,
+        offerToReceiveVideo: 1
+      };
+
+      if (mode === 'sendOnly') {
+        offerOptions.offerToReceiveAudio = 0;
+        offerOptions.offerToReceiveVideo = 0;
+      }
+
+      if (mode === 'sendrecv') {
+        this.rtcPeer
+          .createOffer()
+          .then(this.onOffer)
+          .catch(this.onOfferError);
+      } else {
+        this.rtcPeer
+          .createOffer(offerOptions)
+          .then(this.onOffer)
+          .catch(this.onOfferError);
+      }
     } catch (error) {
       if (options && options.onError) options.onError(error);
     }
   };
 
-  createSendOnlyPeer = options => {
-    this.createPeer('sendOnly', options);
+  onOffer = (offer: Object) => {
+    this.rtcPeer.setLocalDescription(offer);
+    let msg = {
+      id: 'receiveVideoFrom',
+      userId: this.id,
+      sdpOffer: offer.sdp
+    };
+    if (this.options && this.options.reload) {
+      msg = {
+        id: 'reloadStreamFrom',
+        userId: this.id,
+        sdpOffer: offer.sdp
+      };
+    }
+    this.sendMessage(msg);
   };
 
-  createRecvOnlyPeer = options => {
-    this.createPeer('recvOnly', options);
+  onOfferError = (error: Object) => {
+    console.error(error);
   };
 
   reloadStream = () => {
@@ -81,19 +102,21 @@ class Participant {
     this.createPeer(this.mode, options);
   };
 
-  processAnswer = answerSdp => {
+  processAnswer = (answerSdp: string) => {
     const answer = new RTCSessionDescription({
       type: 'answer',
       sdp: answerSdp
     });
-    this.rtcPeer.setRemoteDescription(answer);
+    this.rtcPeer.setRemoteDescription(answer).then(() => {
+      this.isRemoteDescriptionSet = true;
+    });
   };
 
-  onRemoteCandidate = candidate => {
+  onRemoteCandidate = (candidate: Object) => {
     this.rtcPeer.addIceCandidate(new RTCIceCandidate(candidate));
   };
 
-  onIceCandidate = event => {
+  onIceCandidate = (event: Object) => {
     if (event && event.candidate) {
       const message = {
         id: 'onIceCandidate',
@@ -129,8 +152,10 @@ class Participant {
   };
 
   disposeRtcPeer = () => {
-    this.rtcPeer.close();
-    this.rtcPeer = null;
+    if (this.rtcPeer) {
+      this.rtcPeer.close();
+      this.rtcPeer = null;
+    }
   };
 }
 
