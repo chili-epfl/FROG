f6518370eb17e51acb768964c4afe553ec37f9c8
Romain Artru <romain.artru@epfl.ch>
2018-07-09 16:25:47 +0200

    replaced all Picker by WebApp, still need to test '/lti', '/api' broken as we should change the way we give parameters

--- frog/server/api.js
+++ frog/server/api.js
@@ -1,5 +1,3 @@
-import bodyParser from 'body-parser';
-import { Picker } from 'meteor/meteorhacks:picker';
 import { uuid } from 'frog-utils';
 import { Accounts } from 'meteor/accounts-base';
 import { Meteor } from 'meteor/meteor';
@@ -16,63 +14,59 @@ import { mergeOneInstance } from './mergeData';
 import setupH5PRoutes from './h5p';
 import { dashDocId } from '../imports/api/logs';
 
-Picker.middleware(bodyParser.urlencoded({ extended: false }));
-Picker.middleware(bodyParser.json());
-
 setupH5PRoutes();
 
-Picker.filter(req => req.method === 'POST').route(
-  '/lti/:slug',
-  (params, request, response, next) => {
-    const session =
-      params.slug && Sessions.findOne({ slug: params.slug.toUpperCase() });
-    if (!session) {
-      response.writeHead(404);
+WebApp.connectHandlers.use('/lti', (request, response, next) => {
+  if (request.method !== 'POST') next();
+  const url = require('url').parse(request.url);
+  const slug = url.pathname.substring(1);
+  const session = slug && Sessions.findOne({ slug: slug.toUpperCase() });
+  if (!session) {
+    response.writeHead(404);
+    response.end();
+  } else if (session.settings && session.settings.allowLTI === false) {
+    response.writeHead(403);
+    response.end();
+  } else {
+    let user;
+    try {
+      user = request.body.lis_person_name_full;
+    } catch (e) {
+      console.error('Error parsing username in lti request', request.body, e);
+      user = uuid();
+    }
+    let id;
+    try {
+      id = JSON.parse(request.body.lis_result_sourcedid).data.userid;
+    } catch (e) {
+      console.error('Error parsing userid in lti request', request.body, e);
+      id = uuid();
+    }
+    try {
+      // eslint-disable-next-line no-console
+      const { userId } = Accounts.updateOrCreateUserFromExternalService(
+        'frog',
+        {
+          id: user
+        }
+      );
+      Meteor.users.update(userId, { $set: { username: user, userid: id } });
+      const stampedLoginToken = Accounts._generateStampedLoginToken();
+      Accounts._insertLoginToken(userId, stampedLoginToken);
+      InjectData.pushData(request, 'login', {
+        token: stampedLoginToken.token,
+        slug // not sure
+      });
+      next();
+    } catch (e) {
+      console.error('Error responding to lti request', request.body, e);
+      response.writeHead(400);
       response.end();
-    } else if (session.settings && session.settings.allowLTI === false) {
-      response.writeHead(403);
-      response.end();
-    } else {
-      let user;
-      try {
-        user = request.body.lis_person_name_full;
-      } catch (e) {
-        console.error('Error parsing username in lti request', request.body, e);
-        user = uuid();
-      }
-      let id;
-      try {
-        id = JSON.parse(request.body.lis_result_sourcedid).data.userid;
-      } catch (e) {
-        console.error('Error parsing userid in lti request', request.body, e);
-        id = uuid();
-      }
-      try {
-        // eslint-disable-next-line no-console
-        const { userId } = Accounts.updateOrCreateUserFromExternalService(
-          'frog',
-          {
-            id: user
-          }
-        );
-        Meteor.users.update(userId, { $set: { username: user, userid: id } });
-        const stampedLoginToken = Accounts._generateStampedLoginToken();
-        Accounts._insertLoginToken(userId, stampedLoginToken);
-        InjectData.pushData(request, 'login', {
-          token: stampedLoginToken.token,
-          slug: params.slug
-        });
-        next();
-      } catch (e) {
-        console.error('Error responding to lti request', request.body, e);
-        response.writeHead(400);
-        response.end();
-      }
     }
   }
-);
+});
 
-Picker.route('/api/activityTypes', (params, request, response) => {
+WebApp.connectHandlers.use('/api/activityTypes', (request, response) => {
   response.end(
     Stringify(
       activityTypes.map(x => ({
@@ -88,12 +82,18 @@ Picker.route('/api/activityTypes', (params, request, response) => {
   );
 });
 
-const safeDecode = (json, msg, response) => {
-  if (!json) {
-    return undefined;
-  }
+const extractParam = (query, param) =>
+  query.split('&').find(x => x.includes(param))
+    ? query
+        .split('&')
+        .find(x => x.includes(param))
+        .split('=')[1]
+    : undefined;
+
+const safeDecode = (query, field, msg, response) => {
+  const value = extractParam(query, field);
   try {
-    return JSON.parse(json);
+    return value && JSON.parse(value);
   } catch (e) {
     console.error(e);
     return response.end(msg);
@@ -102,145 +102,159 @@ const safeDecode = (json, msg, response) => {
 
 const InstanceDone = {};
 const DashboardDone = {};
-Picker.route(
-  '/api/activityType/:activityTypeId',
-  ({ activityTypeId, query }, request, response, next) => {
-    if (!activityTypesObj[activityTypeId]) {
-      response.end('No matching activity type found');
-    }
-    const activityData = safeDecode(
-      query.activity_data,
-      'Activity data not valid',
-      response
+
+WebApp.connectHandlers.use('/api/activityType', (request, response, next) => {
+  const url = require('url').parse(request.url);
+  const activityTypeId = url.pathname.substring(1);
+  if (!activityTypesObj[activityTypeId]) {
+    response.end('No matching activity type found');
+  }
+  const activityData = safeDecode(
+    url.query,
+    'activity_data',
+    'Activity data not valid',
+    response
+  );
+  const config = safeDecode(
+    url.query,
+    'config',
+    'Config data not valid',
+    response
+  );
+
+  const docId = [
+    extractParam(url.query, 'client_id'),
+    activityTypeId,
+    extractParam(url.query, 'activity_id') || 'default',
+    extractParam(url.query, 'instance_id') || 'default'
+  ].join('/');
+
+  if (!InstanceDone[docId] && !extractParam(url.query, 'readOnly')) {
+    InstanceDone[docId] = true;
+    const aT = activityTypesObj[activityTypeId];
+    Promise.await(
+      new Promise(resolve => {
+        const doc = serverConnection.get('rz', docId);
+        doc.fetch();
+        if (doc.type) {
+          resolve();
+        }
+        doc.once(
+          'load',
+          Meteor.bindEnvironment(() => {
+            if (doc.type) {
+              resolve();
+            } else {
+              mergeOneInstance(
+                null,
+                null,
+                aT.dataStructure,
+                aT.mergeFunction,
+                null,
+                null,
+                null,
+                { data: activityData, config: config || {} },
+                docId
+              );
+              resolve();
+            }
+          })
+        );
+      })
     );
-    const config = safeDecode(query.config, 'Config data not valid', response);
-
-    const docId = [
-      query.client_id,
-      activityTypeId,
-      query.activity_id || 'default',
-      query.instance_id || 'default'
-    ].join('/');
-    if (!InstanceDone[docId] && !query.readOnly) {
-      InstanceDone[docId] = true;
-      const aT = activityTypesObj[activityTypeId];
-      Promise.await(
-        new Promise(resolve => {
-          const doc = serverConnection.get('rz', docId);
-          doc.fetch();
-          if (doc.type) {
-            resolve();
-          }
-          doc.once(
-            'load',
-            Meteor.bindEnvironment(() => {
-              if (doc.type) {
-                resolve();
-              } else {
-                mergeOneInstance(
-                  null,
-                  null,
-                  aT.dataStructure,
-                  aT.mergeFunction,
-                  null,
-                  null,
-                  null,
-                  { data: activityData, config: config || {} },
-                  docId
-                );
-                resolve();
-              }
-            })
-          );
-        })
-      );
-    }
-    const dashboardId = activityTypeId + '-' + (query.activity_id || 'default');
-    if (!DashboardDone[dashboardId] && !query.readOnly) {
-      DashboardDone[dashboardId] = true;
-      const aT = activityTypesObj[activityTypeId];
-      Promise.await(
-        new Promise(resolve => {
-          if (aT.dashboards) {
-            Object.keys(aT.dashboards).forEach(name => {
-              const dash = aT.dashboards[name];
-              const doc = serverConnection.get(
-                'rz',
-                dashDocId(dashboardId, name)
-              );
-              doc.fetch();
-              if (doc.type) {
-                resolve();
-              }
-              doc.once(
-                'load',
-                Meteor.bindEnvironment(() => {
-                  if (doc.type) {
-                    resolve();
-                  } else {
-                    doc.create((dash && dash.initData) || {});
-                    resolve();
-                  }
-                })
-              );
-            });
-          }
-        })
-      );
-    }
-    InjectData.pushData(response, 'api', {
-      callType: 'runActivity',
-      activityType: activityTypeId,
-      userid: query.userid,
-      username: query.username,
-      instance_id: docId,
-      activity_id: query.activity_id,
-      raw_instance_id: query.instance_id || 'default',
-      activity_data: activityData,
-      readOnly: query.readOnly,
-      config
-    });
-    next();
   }
-);
 
-Picker.route(
-  '/api/config/:activityTypeId',
-  ({ activityTypeId, query }, request, response, next) => {
-    if (!activityTypesObj[activityTypeId]) {
-      response.end('No matching activity type found');
-    }
-    const config = safeDecode(query.config, 'Config data not valid', response);
-
-    InjectData.pushData(response, 'api', {
-      callType: 'config',
-      activityType: activityTypeId,
-      config
-    });
-    next();
+  const dashboardId = activityTypeId + '-' + docId[2];
+  if (!DashboardDone[dashboardId] && !extractParam(url.query, 'readOnly')) {
+    DashboardDone[dashboardId] = true;
+    const aT = activityTypesObj[activityTypeId];
+    Promise.await(
+      new Promise(resolve => {
+        if (aT.dashboards) {
+          Object.keys(aT.dashboards).forEach(name => {
+            const dash = aT.dashboards[name];
+            const doc = serverConnection.get(
+              'rz',
+              dashDocId(dashboardId, name)
+            );
+            doc.fetch();
+            if (doc.type) {
+              resolve();
+            }
+            doc.once(
+              'load',
+              Meteor.bindEnvironment(() => {
+                if (doc.type) {
+                  resolve();
+                } else {
+                  doc.create((dash && dash.initData) || {});
+                  resolve();
+                }
+              })
+            );
+          });
+        }
+      })
+    );
   }
-);
+  InjectData.pushData(response, 'api', {
+    callType: 'runActivity',
+    activityType: activityTypeId,
+    userid: extractParam(url.query, 'userid'),
+    username: extractParam(url.query, 'username'),
+    instance_id: docId,
+    activity_id: extractParam(url.query, 'activity_id'),
+    raw_instance_id: extractParam(url.query, 'instance_id') || 'default',
+    activity_data: activityData,
+    readOnly: extractParam(url.query, 'readOnly'),
+    config
+  });
+  next();
+});
 
-Picker.route(
-  '/api/dashboard/:activityTypeId',
-  ({ activityTypeId, query }, request, response, next) => {
-    if (!activityTypesObj[activityTypeId]) {
-      response.end('No matching activity type found');
-    }
-    const config = safeDecode(query.config, 'Config data not valid', response);
-
-    InjectData.pushData(response, 'api', {
-      callType: 'dashboard',
-      activityType: activityTypeId,
-      instances: query.instances,
-      activity_id: query.activity_id || 'default',
-      config
-    });
-    next();
+WebApp.connectHandlers.use('/api/config', (request, response, next) => {
+  const url = require('url').parse(request.url);
+  const activityTypeId = url.pathname.substring(1);
+  if (!activityTypesObj[activityTypeId]) {
+    response.end('No matching activity type found');
   }
-);
+  const config = safeDecode(
+    url.query,
+    'config',
+    'Config data not valid',
+    response
+  );
+  InjectData.pushData(request, 'api', {
+    callType: 'config',
+    activityType: activityTypeId,
+    config
+  });
+  next();
+});
 
-Picker.route('/api/chooseActivity', (req, request, response, next) => {
+WebApp.connectHandlers.use('/api/dashboard/', (request, response, next) => {
+  const url = require('url').parse(request.url);
+  const activityTypeId = url.pathname.substring(1);
+  if (!activityTypesObj[activityTypeId]) {
+    response.end('No matching activity type found');
+  }
+  const config = safeDecode(
+    url.query,
+    'config',
+    'Config data not valid',
+    response
+  );
+  InjectData.pushData(response, 'api', {
+    callType: 'dashboard',
+    activityType: activityTypeId,
+    instances: extractParam(url.query, 'instances'),
+    activity_id: extractParam(url.query, 'activity_id') || 'default',
+    config
+  });
+  next();
+});
+
+WebApp.connectHandlers.use('/api/chooseActivity', (request, response, next) => {
   InjectData.pushData(response, 'api', {
     callType: 'config'
   });
