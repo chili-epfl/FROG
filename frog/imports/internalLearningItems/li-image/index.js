cd4260488c7677f6b0d55014e4326aa169b9c364
anujanegi <anujanegi1@gmail.com>
2018-07-18 00:10:45 +0530

    Updated css for images inside frame in ac-gallery

--- frog/imports/internalLearningItems/li-image/index.js
+++ frog/imports/internalLearningItems/li-image/index.js
@@ -17,7 +17,7 @@ export default ({
   ThumbViewer: ({ data }: { data: any }) => (
     <Paper
       elevation={24}
-      style={{ height: '145px', width: '145px', margin: '20px' }}
+      style={{ height: 'inherit', width: 'inherit' }}
     >
       <ImageReload style={{ margin: '10px' }} src={data.thumburl} />
     </Paper>
