2d2017da40c21abd121d1d8a852a234df725c9d1
Romain Artru <romain.artru@epfl.ch>
2018-07-25 12:53:29 +0200

    wip

--- ac/ac-plot/src/ActivityRunner.js
+++ ac/ac-plot/src/ActivityRunner.js
@@ -1,8 +1,19 @@
 // @flow
 
 import * as React from 'react';
+import Plotly from 'react-plotly.js';
 
 // the actual component that the student sees
-export default ({ logger, activityData, data, dataFn, userInfo }) => (
-  <div>{JSON.stringify(activityData)}</div>
-);
+export default ({ logger, activityData, data, dataFn, userInfo }) => {
+  const myData = [
+    {
+      x: [0, 1, 3, 6, 10],
+      y: [12,23,34,45,47]
+    }
+  ]
+  return (
+  <div>
+    {Plotly.newPlot(<div/>, myData)}
+    {/* JSON.stringify(activityData) */}
+  </div>
+);}
