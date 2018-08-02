20dad0c6537c49ca0d0548a67bbec18d0f39ba52
Romain Artru <romain.artru@epfl.ch>
2018-08-02 16:49:42 +0200

    add works but should be contained for columns

--- frog/imports/internalLearningItems/li-spreadsheet/index.js
+++ frog/imports/internalLearningItems/li-spreadsheet/index.js
@@ -6,12 +6,38 @@ import mathjs from 'mathjs';
 import { assign, each } from 'lodash';
 import Datasheet from 'react-datasheet';
 import { Button } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
 
 if (isBrowser) {
   require('./css.js');
 }
 
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+const AddButton = ({onClick}) =>
+<Button
+  onClick={onClick}
+  variant="fab"
+  color="primary"
+  style={{width: '35px', height: '30px'}}
+>
+  <AddIcon />
+</Button>
+
+const RemoveButton = ({onClick}) =>
+<Button
+  onClick={onClick}
+  variant="fab"
+  color="secondary"
+  style={{width: '35px', height: '30px'}}
+>
+  <RemoveIcon />
+</Button>
+
 class MathSheet extends React.Component<*, *> {
+  // dimensions should be part of data
+
   validateExp(trailKeys, expr) {
     let valid = true;
     const matches = expr.match(/[A-Z][1-9]+/g) || [];
@@ -51,6 +77,15 @@ class MathSheet extends React.Component<*, *> {
     }
   }
 
+  addRow(){
+    this.props.dataFn.listAppend(['', 'A', 'B', 'C', 'D', 'E'].map((col, j) => {
+      if (j === 0) {
+        return { readOnly: true, value: 8 };
+      }
+      return { value: '', key: col + 8, col: j, row: 8 };
+    }))
+  }
+
   cellUpdate(changeCell, expr, col, row, data) {
     const scope = flattenOne(data || this.props.data).reduce(
       (acc, x) => ({
@@ -64,6 +99,8 @@ class MathSheet extends React.Component<*, *> {
       changeCell,
       this.computeExpr(changeCell.key, expr, scope)
     );
+    console.log(updatedCell)
+    console.log(row + ' , '+col)
     this.props.dataFn.listReplace(changeCell, updatedCell, [row, col]);
     const tempData = this.props.data;
     tempData[row][col] = updatedCell;
@@ -89,22 +126,47 @@ class MathSheet extends React.Component<*, *> {
     const data = this.props.readOnly
       ? this.props.data.map(x => x.map(y => ({ ...y, readOnly: true })))
       : this.props.data;
+    console.log(this.props.data)
     return (
-      <div style={{ width: '800px', fontSize: '1.3em' }}>
+      <div style={{ width: '800px', fontSize: '1.3em', display: 'flex', flexDirection: 'column' }}>
+        <div style={{ flexDirection: 'row', display: 'flex' }}>
         <Datasheet
           data={data}
           valueRenderer={cell => cell.value}
           dataRenderer={cell => cell.expr}
           onCellsChanged={this.onCellsChanged}
         />
+        <div style={{ flexDirection: 'column', display: 'flex' }}>
+        <AddButton onClick={() => {
+          data.forEach((x,i) => i === 0 ? this.props.dataFn.listAppend({ readOnly: true, value: alphabet[data[0].length-1] },i) : this.props.dataFn.listAppend({ value: '', key: alphabet[data[0].length-1] + i, col: alphabet[data[0].length-1], row: i },i))
+        }
+        }/>
+        <RemoveButton onClick={() => {
+
+        }
+        }/>
+      </div>
+      </div>
+      <div style={{ flexDirection: 'row', display: 'flex' }}>
+        <AddButton onClick={() => {
+          this.props.dataFn.listAppend(['', ...alphabet.substring(0,data[0].length)].map((col, j) => {
+            if (j === 0) {
+              return { readOnly: true, value: data.length };
+            }
+            return { value: '', key: col + data.length, col: j, row: data.length };
+          }))
+        }
+        }/>
+        <RemoveButton/>
+    </div>
       </div>
     );
   }
 }
 
 export default ({
-  dataStructure: [0, 1, 2, 3, 4, 5, 6, 7].map((row, i) =>
-    ['', 'A', 'B', 'C', 'D', 'E'].map((col, j) => {
+  dataStructure: [0, 1, 2, 3, 4].map((row, i) =>
+    ['', 'A', 'B', 'C', 'D'].map((col, j) => { // ...alphabet.substring(0,4)
       if (i === 0 && j === 0) {
         return { readOnly: true, value: '                ' };
       }
