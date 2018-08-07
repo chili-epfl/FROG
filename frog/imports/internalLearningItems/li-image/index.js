7423b675e85782a5ab6ac2399a428ee596da1b9e
Romain Artru <romain.artru@epfl.ch>
2018-08-07 10:58:30 +0200

    done, css could be improved

--- frog/imports/internalLearningItems/li-image/index.js
+++ frog/imports/internalLearningItems/li-image/index.js
@@ -7,15 +7,25 @@ import Paper from '@material-ui/core/Paper';
 import ToolBar from './ToolBar';
 import { Creator } from '../li-file';
 
-const ImageEditor = (props) => {
-    console.log(props.data)
-    console.log(props)
-    return (<Paper elevation={24} style={{ margin: '20px' }}>
-      <ToolBar data={props.data} dataFn={props.dataFn}/>
-      <ImageReload style={{ margin: '10px' }} src={props.data.url} />
-    </Paper>)
-  }
-
+const ImageEditor = props => (
+  <Paper elevation={24} style={{ margin: '20px', overflow: 'scroll', display: 'flex', flexDirection: 'column' }}>
+    <ToolBar data={props.data} dataFn={props.dataFn} />
+    <ImageReload
+      style={{
+        margin: '10px',
+        width: 'fit-content',
+        height: 'fit-content',
+        transform:
+          'rotate(' +
+          (props.data.rotation || 0) +
+          'deg) scale(' +
+          (props.data.scale || 1) +
+          ')'
+      }}
+      src={props.data.url}
+    />
+  </Paper>
+);
 
 export default ({
   name: 'image',
