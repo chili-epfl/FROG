724bb4f7ec2a6a75b4de5e12f6f8e141b4555a8c
Marin Luketin <marin.luketin@gmail.com>
2018-08-10 03:05:33 +0200

    Disable eslint in clmtrackr library

--- ac/ac-videochat/src/lib/clmtrackr.js
+++ ac/ac-videochat/src/lib/clmtrackr.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
