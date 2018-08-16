8cff42fa1082989b52be0ea32d50f2f50d442718
Romain Artru <romain.artru@epfl.ch>
2018-08-16 10:44:47 +0200

    done

--- frog/imports/ui/App/index.js
+++ frog/imports/ui/App/index.js
@@ -1,6 +1,7 @@
 // @flow
 
 import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
 import { InjectData } from 'meteor/staringatlights:inject-data';
 import { Accounts } from 'meteor/accounts-base';
 import * as React from 'react';
@@ -214,19 +215,46 @@ const FROGRouter = withRouter(
   }
 );
 
+const ConnectionDiv = () => (
+  <div
+    style={{
+      backgroundColor: 'white',
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      opacity: '0.8',
+      zIndex: '1500'
+    }}
+  >
+    <div
+      style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%,-50%)'
+      }}
+    >
+      <h2>Disconnected, waiting for reconnection…</h2>
+      <CircularProgress />
+    </div>
+  </div>
+);
+
 export default class Root extends React.Component<
   {},
   {
     mode: string,
     api?: boolean,
-    data?: Object
+    data?: Object,
+    connected: Boolean
   }
 > {
   constructor() {
-
     super();
-    this.state = { mode: 'waiting' };
-
+    this.state = { mode: 'waiting', connected: Meteor.status().connected };
+    Tracker.autorun(() =>
+      this.setState({ connected: Meteor.status().connected })
+    );
   }
 
   componentDidMount = () => {
@@ -236,16 +264,19 @@ export default class Root extends React.Component<
   };
 
   render() {
-    console.log(Meteor.connection.status().connected)
-    if(!Meteor.connection.status().connected)
-      return <div>Offline</div>
     if (this.state.mode === 'waiting') {
       return null;
     } else if (this.state.api && this.state.data) {
-      return <APICall data={this.state.data} />;
+      return (
+        <>
+          {!this.state.connected && <ConnectionDiv />}
+          <APICall data={this.state.data} />
+        </>
+      );
     } else {
       return (
         <ErrorBoundary>
+          {!this.state.connected && <ConnectionDiv />}
           <Router>
             <Switch>
               <Route path="/:slug" component={FROGRouter} />
