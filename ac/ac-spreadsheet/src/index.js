c2bc5d7eaa170026dbb2f1f1660cecfd388e7354
Stian Håklev <shaklev@gmail.com>
2018-08-24 11:36:14 +0200

    WIP

---
+++ ac/ac-spreadsheet/src/index.js
@@ -0,0 +1,49 @@
+// @flow
+
+import { type ActivityPackageT } from 'frog-utils';
+
+const meta = {
+  name: 'Spreadsheet',
+  shortDesc: 'Spreadsheet/table',
+  description: 'Supports some calculations',
+  exampleData: []
+};
+
+const config = {
+  type: 'object',
+  properties: {
+    title: {
+      title: 'What is the title?',
+      type: 'string'
+    }
+  }
+};
+
+// default empty reactive datastructure, typically either an empty object or array
+const dataStructure = [0, 1, 2, 3, 4].map((row, i) =>
+  ['', 'A', 'B', 'C', 'D'].map((col, j) => {
+    if (i === 0 && j === 0) {
+      return { readOnly: true, value: '                ' };
+    }
+    if (row === 0) {
+      return { readOnly: true, value: col };
+    }
+    if (j === 0) {
+      return { readOnly: true, value: row };
+    }
+    return { value: '', key: col + row, col: j, row: i };
+  })
+);
+
+// receives incoming data, and merges it with the reactive data using dataFn.*
+const mergeFunction = (object, dataFn) => {};
+
+export default ({
+  id: 'ac-spreadsheet',
+  type: 'react-component',
+  configVersion: 1,
+  meta,
+  config,
+  dataStructure,
+  mergeFunction
+}: ActivityPackageT);
