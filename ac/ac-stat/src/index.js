f2d39766817161346d148ffb71bf9ffcf7ba2cde
Romain Artru <romain.artru@epfl.ch>
2018-08-31 14:07:35 +0200

    added a all dataset graph

--- ac/ac-stat/src/index.js
+++ ac/ac-stat/src/index.js
@@ -29,18 +29,6 @@ const config = {
 // default empty reactive datastructure, typically either an empty object or array
 const dataStructure = {};
 
-// receives incoming data, and merges it with the reactive data using dataFn.*
-// const mergeFunction = ({ data: incoming }, dataFn, data) => {
-//   if (!Array.isArray(incoming)) {
-//     return;
-//   }
-//   incoming.forEach(({ trace, ...rest }) => {
-//     if (!data[trace]) dataFn.objInsert([], trace);
-//     dataFn.listAppend({ ...rest }, trace);
-//   });
-//   dataFn.objInsert(data, 'originalData');
-// };
-
 const mergeFunction = ({ data: incoming }, dataFn, data) => {
   if (!Array.isArray(incoming)) {
     return;
@@ -55,6 +43,16 @@ const mergeFunction = ({ data: incoming }, dataFn, data) => {
     });
     dataFn.listAppend(tmpEntry, [trace, 'values']);
   });
+
+  const {originalData,...datasets} = data
+  if(Object.keys(datasets).length > 1)
+    dataFn.objInsert(({
+      columns: [Object.values(datasets)[0].columns[0], 'dataset'],
+      values: Object.keys(datasets).reduce((acc,cur) => [...acc, ...datasets[cur].values.map(entry => [entry[0], cur])]
+    ,[])
+  })
+    , 'all datasets')
+
   dataFn.objInsert(data, 'originalData');
 };
 
