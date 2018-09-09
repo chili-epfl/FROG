4185712519bfeafa14890f6176cd66c717e69aca
Louis Faucon <lfaucon@users.noreply.github.com>
2018-09-09 07:08:30 +0200

    Fix acgallery (#1320)

--- ac/ac-gallery/src/components/CategoryBox.js
+++ ac/ac-gallery/src/components/CategoryBox.js
@@ -1,49 +1,53 @@
 // @flow
 
 import React from 'react';
-import styled from 'styled-components';
+import { withStyles } from '@material-ui/core/styles';
 import { type LogT } from 'frog-utils';
 
-const CategoryContainer = styled.button`
-  position: relative;
-  border: solid 2px #a0a0a0;
-  background: none;
-  max-width: 250px;
-  height: 250px;
-  width: 100%;
-  margin: 5px;
-  padding: 0px;
-  flex: 0 1 auto;
-  display: flex;
-  flex-flow: row wrap;
-  align-items: center;
-  justify-content: center;
-`;
-
-const CategoryName = styled.span`
-  height: 10%;
-  font-size: 3em;
-  bottom: 0px;
-`;
+const styles = () => ({
+  container: {
+    position: 'relative',
+    border: 'solid 2px #a0a0a0',
+    background: 'none',
+    maxWidth: '250px',
+    height: '250px',
+    width: '100%',
+    margin: '5px',
+    padding: '0px',
+    flex: '0 1 auto',
+    display: 'flex',
+    flexFlow: 'row wrap',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    height: '10%',
+    fontSize: '3em',
+    bottom: '0px'
+  }
+});
 
 const CategoryBox = ({
   category,
   setCategory,
-  logger
+  logger,
+  classes
 }: {
   category: string,
   setCategory: Function,
-  logger: LogT => void
+  logger: LogT => void,
+  classes: Object
 }) => (
-  <CategoryContainer
+  <button
+    className={classes.container}
     onClick={() => {
       logger({ type: 'category.enter', value: category });
       setCategory(category);
     }}
   >
-    <CategoryName>{category}</CategoryName>
-  </CategoryContainer>
+    <span className={classes.text}>{category}</span>
+  </button>
 );
 
 CategoryBox.displayName = 'CategoryBox';
-export default CategoryBox;
+export default withStyles(styles)(CategoryBox);
