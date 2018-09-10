3d7163c94e3983716a42966e3b16526cb57e4800
lfaucon <lpfaucon@gmail.com>
2018-09-10 08:55:28 +0200

    Fix

--- linkFiles.js
+++ linkFiles.js
@@ -1,21 +1,21 @@
 const rimraf = require('rimraf');
 const fs = require('fs');
 
-rimraf.sync('../frog/frog/public/clientFiles');
-fs.mkdirSync('../frog/frog/public/clientFiles');
-fs.readdirSync('../frog/op').forEach(x => {
-  if (fs.existsSync(`../frog/op/${x}/clientFiles`)) {
+rimraf.sync('./frog/public/clientFiles');
+fs.mkdirSync('./frog/public/clientFiles');
+fs.readdirSync('./op').forEach(x => {
+  if (fs.existsSync(`./op/${x}/clientFiles`)) {
     fs.symlinkSync(
-      `../../../op/${x}/clientFiles`,
-      `../frog/frog/public/clientFiles/${x}`
+      `./../../../op/${x}/clientFiles`,
+      `./frog/public/clientFiles/${x}`
     );
   }
 });
-fs.readdirSync('../frog/ac').forEach(x => {
-  if (fs.existsSync(`../frog/ac/${x}/clientFiles`)) {
+fs.readdirSync('./ac').forEach(x => {
+  if (fs.existsSync(`./ac/${x}/clientFiles`)) {
     fs.symlinkSync(
-      `../../../ac/${x}/clientFiles`,
-      `../frog/frog/public/clientFiles/${x}`
+      `./../../../ac/${x}/clientFiles`,
+      `./frog/public/clientFiles/${x}`
     );
   }
 });
