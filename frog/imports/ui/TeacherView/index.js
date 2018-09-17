c97706117eb8b4d062953a0d496b3708233b1703
Stian Håklev <shaklev@gmail.com>
2018-09-17 15:44:12 +0200

    WIP

--- frog/imports/ui/TeacherView/index.js
+++ frog/imports/ui/TeacherView/index.js
@@ -19,9 +19,21 @@ const TeacherView = props => (
   </>
 );
 
-const TeacherViewRunner = withTracker(() => {
+const TeacherViewRunner = withTracker(({ match }) => {
   const user = Meteor.user();
-  const session = user.profile && Sessions.findOne(user.profile.controlSession);
+  let session;
+  if (match?.params?.graphId) {
+    const graph = Graphs.findOne(match.params.graphId.trim());
+    if (graph.sessionId) {
+      session = Sessions.findOne(graph.sessionId);
+      Meteor.users.findOne(user._id, {
+        $set: { 'profile.controlSession': session._id }
+      });
+    }
+  }
+  if (!session) {
+    session = user.profile && Sessions.findOne(user.profile.controlSession);
+  }
   const activities =
     session && Activities.find({ graphId: session.graphId }).fetch();
   const students =
