30af8b016a2a7ac499a83c92aedb32c87bd657ae
Stian Håklev <shaklev@gmail.com>
2018-09-18 15:41:46 +0200

    WIP

--- frog/imports/ui/TeacherView/GraphView.jsx
+++ frog/imports/ui/TeacherView/GraphView.jsx
@@ -2,9 +2,7 @@
 
 import * as React from 'react';
 import { Provider } from 'mobx-react';
-import { withRouter } from 'react-router';
 import { isEqual } from 'lodash';
-
 import { withStyles } from '@material-ui/core/styles';
 
 import ShowInfo from './ShowInfo';
@@ -13,7 +11,6 @@ import { store } from '../GraphEditor/store';
 
 type GraphViewPropsT = {
   session: Object,
-  history: Object,
   classes: Object
 };
 
@@ -68,6 +65,6 @@ class GraphViewController extends React.Component<GraphViewPropsT, {}> {
   }
 }
 
-const GraphView = withRouter(GraphViewController);
+const GraphView = GraphViewController;
 
 export default withStyles(styles)(GraphView);
