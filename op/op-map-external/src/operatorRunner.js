04655c0a2b52076c47d02d1c135f2d4387400bbb
Stian Håklev <shaklev@gmail.com>
2018-09-27 17:15:08 +0200

    WIP

--- op/op-map-external/src/operatorRunner.js
+++ op/op-map-external/src/operatorRunner.js
@@ -1,7 +1,27 @@
 // @flow
 
-import { type productOperatorRunnerT, wrapUnitAll } from 'frog-utils';
+import util from 'util';
+import { type productOperatorRunnerT, wrapUnitAll, values } from 'frog-utils';
+import { isObject, invert } from 'lodash';
 
-const operator = (configData, object) => wrapUnitAll({});
+const operator = (configData, object) => {
+  const data = object.activityData.payload.all.data;
+  const res = {};
+  if (!isObject(data)) {
+    return wrapUnitAll({});
+  }
+  values(data).forEach(item => {
+    const username = item.li?.liDocument?.username;
+    const students = invert(object.globalStructure.students);
+    const userid = username && students[username];
+    if (userid) {
+      if (!res[userid]) {
+        res[userid] = { data: {}, config: {} };
+      }
+      res[userid].data[item.id] = item;
+    }
+  });
+  return { structure: 'individual', payload: res };
+};
 
 export default (operator: productOperatorRunnerT);
