5e23d1457aad35e981bbb515be2aef79e041b3cf
lfaucon <lpfaucon@gmail.com>
2018-10-01 11:35:38 +0200

    Eslint

--- ac/ac-quiz/src/ActivityRunner/Question.js
+++ ac/ac-quiz/src/ActivityRunner/Question.js
@@ -12,7 +12,7 @@ import Checkbox from '@material-ui/core/Checkbox';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 import { condShuffle } from './Quiz';
-import { computeProgress, computeCoordinates, isAnswered } from '../utils';
+import { computeProgress, computeCoordinates } from '../utils';
 
 const styles = theme => ({
   root: {
