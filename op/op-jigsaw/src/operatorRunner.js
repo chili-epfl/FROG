f96c926fb6b050c365b4b92576b822e95dfa74b6
Stian Håklev <shaklev@gmail.com>
2018-10-02 06:06:54 +0200

    WIP

--- op/op-jigsaw/src/operatorRunner.js
+++ op/op-jigsaw/src/operatorRunner.js
@@ -5,6 +5,9 @@ import { focusRole, type socialOperatorRunnerT } from 'frog-utils';
 const operator = (configData, object) => {
   const { globalStructure, socialStructure } = object;
 
+  if (globalStructure.studentIds.length === 0) {
+    return { group: {}, role: {} };
+  }
   const roles = configData.roles.split(',').map(x => x.trim());
   const groupSize = roles.length;
   const socStruc = {};
