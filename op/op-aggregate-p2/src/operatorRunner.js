762b7a928bd39a8c47a2671c3cf28b94936d090d
lfaucon <lpfaucon@gmail.com>
2018-10-08 18:22:19 +0200

    Make it work with several incoming operators

--- op/op-aggregate-p2/src/operatorRunner.js
+++ op/op-aggregate-p2/src/operatorRunner.js
@@ -1,24 +1,28 @@
 // @flow
-import { focusStudent, type productOperatorRunnerT } from 'frog-utils';
+import { focusStudent, values, type productOperatorRunnerT } from 'frog-utils';
 
 const operator = (configData, object) => {
   const { socialStructure } = object;
   const groups = Object.keys(socialStructure[configData.grouping]);
 
   const studentMapping = focusStudent(socialStructure);
-  let res = groups.reduce((acc, k) => ({ ...acc, [k]: [] }), {});
-  Object.keys(object.activityData.payload).forEach(x => {
-    const items = configData.wholeElement
-      ? object.activityData.payload[x].data
-      : Object.values(object.activityData.payload[x].data);
-    const group = studentMapping[x]
-      ? studentMapping[x][configData.grouping]
-      : x;
-    res[group] = [
-      ...(res[group] || []),
-      ...(configData.wholeElement ? [items] : items)
-    ];
+  let res = groups.reduce((acc, g) => ({ ...acc, [g]: [] }), {});
+
+  values(object.activityData).forEach(inputProduct => {
+    Object.keys(inputProduct.payload).forEach(x => {
+      const items = configData.wholeElement
+        ? inputProduct.payload[x].data
+        : Object.values(inputProduct.payload[x].data);
+      const group = studentMapping[x]
+        ? studentMapping[x][configData.grouping]
+        : x;
+      res[group] = [
+        ...(res[group] || []),
+        ...(configData.wholeElement ? [items] : items)
+      ];
+    });
   });
+
   res = Object.keys(res).reduce(
     (acc, x) => ({ ...acc, [x]: { data: res[x], config: {} } }),
     {}
