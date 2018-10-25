fcda6f2a0f101b419bb04e890272e297a5aa87ed
Thanuditha Ruchiranga Wickramasinghe <truchiranga@gmail.com>
2018-10-25 14:58:41 +0200

    Add link, image and video to toolbar

--- frog-utils/src/ReactiveRichText.js
+++ frog-utils/src/ReactiveRichText.js
@@ -26,6 +26,8 @@ const toolbarOptions = [
   [{ size: ['small', false, 'large', 'huge'] }],
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
 
+  ['link', 'image', 'video'],
+
   [{ color: [] }, { background: [] }],
   [{ font: [] }],
   [{ align: [] }],
