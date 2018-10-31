4924fcbdcad1c566b1e1fe76e82a73c5b40ae64b
Thanuditha Ruchiranga Wickramasinghe <truchiranga@gmail.com>
2018-10-31 13:51:14 +0100

    Add serach functionality

--- frog/imports/internalLearningItems/li-richText/index.js
+++ frog/imports/internalLearningItems/li-richText/index.js
@@ -1,22 +1,38 @@
 import * as React from 'react';
+import { get } from 'lodash';
 import { type LearningItemT, ReactiveRichText } from 'frog-utils';
 
-export const FlexViewer = ({ data, dataFn }) => (
-  <div>
-    <ReactiveRichText data={data} path="text" readOnly dataFn={dataFn} />
-  </div>
-);
+const path = 'text';
+
+export const FlexViewer = ({ data, dataFn, search }) => {
+  if (search) {
+    const editorContent = get(dataFn.doc.data, dataFn.getMergedPath(path));
+    const textContent = editorContent.ops.map(op => {
+      if (typeof op.insert === 'string') {
+        return op.insert;
+      }
+    });
+    if (!textContent.join('').toLowerCase().includes(search)) {
+      return null;
+    }
+  }
+  return (
+    <div>
+      <ReactiveRichText data={data} path={path} readOnly dataFn={dataFn} />
+    </div>
+  )
+};
 
 export const Editor = ({ dataFn }) => (
   <div>
-    <ReactiveRichText path="text" dataFn={dataFn} />
+    <ReactiveRichText path={path} dataFn={dataFn} />
   </div>
 );
 
 export default ({
   name: 'Rich text',
   id: 'li-richText',
-  dataStructure: { text: '' },
+  dataStructure: { [path]: '' },
   ThumbViewer: FlexViewer,
   Viewer: FlexViewer,
   Editor
