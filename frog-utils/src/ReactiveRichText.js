6a6d3856c2fcaf0aae9c17a96880a5afc9fd9ea6
Thanuditha Ruchiranga Wickramasinghe <truchiranga@gmail.com>
2018-11-02 02:46:44 +0100

    Fix lint issue

--- frog-utils/src/ReactiveRichText.js
+++ frog-utils/src/ReactiveRichText.js
@@ -1,10 +1,10 @@
 // @flow
+import '@houshuang/react-quill/dist/quill.snow.css';
+
 import React, { Component } from 'react';
 import { get, isEqual, last } from 'lodash';
 import ReactQuill from '@houshuang/react-quill';
 import { shortenRichText } from './index';
-
-import '@houshuang/react-quill/dist/quill.snow.css';
 
 type ReactivePropsT = {
   path: string,
