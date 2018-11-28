02a0a4e0ff29d25f9bf4305852470e7fa86144b2
Stian Håklev <shaklev@gmail.com>
2018-11-28 19:57:47 +0800

    WIP

--- frog-utils/src/index.js
+++ frog-utils/src/index.js
@@ -4,6 +4,29 @@
 import * as React from 'react';
 import { compose, withHandlers, withState } from 'recompose';
 import { shuffle, isString, filter, split, isEqual } from 'lodash';
+import ReactLoadable from 'react-loadable';
+
+export const Loadable = ({
+  loader,
+  componentDescription
+}: {
+  loader: void => Promise<*>,
+  componentDescription: string
+}) =>
+  ReactLoadable({
+    loader,
+    loading(props) {
+      if (props.error) {
+        console.error(props.error);
+        return <div>React Loader error! {componentDescription}</div>;
+      } else if (props.timedOut) {
+        return <div>React Loader Timed Out! {componentDescription}</div>;
+      } else {
+        return null;
+      }
+    },
+    timeout: 10000
+  });
 
 export const isBrowser = (() => {
   try {
--- frog/imports/internalActivities/ac-dash/index.js
+++ frog/imports/internalActivities/ac-dash/index.js
@@ -1,13 +1,13 @@
 // @flow
 
-import type { ActivityPackageT } from 'frog-utils';
-import Loadable from 'react-loadable';
+import { type ActivityPackageT, Loadable } from 'frog-utils';
 import path from 'path';
 
 const ConfigComponent = Loadable({
   loader: () => import('./config.js'),
   loading: () => null,
-  serverSideRequirePath: path.resolve(__dirname, './config.js')
+  serverSideRequirePath: path.resolve(__dirname, './config.js'),
+  componentDescription: 'configComponent'
 });
 
 export const meta = {
