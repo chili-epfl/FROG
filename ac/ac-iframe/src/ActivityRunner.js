7192505b47774ac3b911737348fce10c99b12c96
Stian Håklev <shaklev@gmail.com>
2018-12-14 09:22:59 +0100

    Adding template and security options to ac-iframe (#1567)

--- ac/ac-iframe/src/ActivityRunner.js
+++ ac/ac-iframe/src/ActivityRunner.js
@@ -1,12 +1,22 @@
 import * as React from 'react';
 import type { ActivityRunnerPropsT } from 'frog-utils';
 
-const ActivityRunner = ({ activityData }: ActivityRunnerPropsT) => (
-  <iframe
-    title="IFrame"
-    src={activityData.config.url}
-    style={{ width: '100%', height: '100%', overflow: 'auto' }}
-  />
-);
+const ActivityRunner = ({ activityData, data }: ActivityRunnerPropsT) => {
+  let url = activityData.config.url;
+  if (data.uuid) {
+    url = url && url.replace('{}', data.uuid);
+  }
+  return (
+    <iframe
+      title="IFrame"
+      src={url}
+      allow={
+        activityData.config.trusted &&
+        'geolocation *; microphone *; camera *; midi *; encrypted-media *;'
+      }
+      style={{ width: '100%', height: '100%', overflow: 'auto' }}
+    />
+  );
+};
 
 export default ActivityRunner;
