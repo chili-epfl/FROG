389e602b1aba25daf1701d94909595d8b25cf51e
Thanuditha Ruchiranga Wickramasinghe <truchiranga@gmail.com>
2018-12-22 20:21:30 +0100

    Prevent LI content from going out Paper bounds

--- frog-utils/src/ReactiveRichText.js
+++ frog-utils/src/ReactiveRichText.js
@@ -37,7 +37,8 @@ const styles = theme => ({
   root: {
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2
+    paddingBottom: theme.spacing.unit * 2,
+    overflow: 'auto'
   },
   button: {
     color: '#AA0000',
