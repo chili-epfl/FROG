0b23543f0202e41b246e602bcf992db772d6e21d
Stian Håklev <shaklev@gmail.com>
2019-01-08 13:12:26 +0100

    ac-single-li RTE instructions

--- ac/ac-single-li/src/ActivityRunner.js
+++ ac/ac-single-li/src/ActivityRunner.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { type ActivityRunnerPropsT } from 'frog-utils';
+import { HTML, type ActivityRunnerPropsT } from 'frog-utils';
 import Fab from '@material-ui/core/Fab';
 import Button from '@material-ui/core/Button';
 import EditIcon from '@material-ui/icons/Edit';
@@ -41,7 +41,7 @@ class ActivityRunner extends React.Component<
         {conf.title && <h1>{conf.title}</h1>}
         {conf.instructions && (
           <p>
-            <b>{conf.instructions}</b>
+            <HTML html={conf.instructions} />
           </p>
         )}
       </>
@@ -62,29 +62,32 @@ class ActivityRunner extends React.Component<
             render={({ editable, children }) => (
               <div>
                 {children}
-                {!editing && !conf.noSubmit && conf.allowEditing && (
-                  <Fab
-                    onClick={() =>
-                      editable
-                        ? this.setState({ editing: true })
-                        : dataFn.objDel(null, 'li')
-                    }
-                    color="secondary"
-                    aria-label={editable ? 'edit' : 'delete'}
-                  >
-                    {editable ? <EditIcon /> : <CloseIcon />}
-                  </Fab>
-                )}
-                {editing && !conf.noSubmit && (
-                  <Button
-                    onClick={() => this.setState({ editing: false })}
-                    color="primary"
-                    variant="contained"
-                    aria-label="save"
-                  >
-                    Save
-                  </Button>
-                )}
+                {!editing &&
+                  !conf.noSubmit &&
+                  conf.allowEditing && (
+                    <Fab
+                      onClick={() =>
+                        editable
+                          ? this.setState({ editing: true })
+                          : dataFn.objDel(null, 'li')
+                      }
+                      color="secondary"
+                      aria-label={editable ? 'edit' : 'delete'}
+                    >
+                      {editable ? <EditIcon /> : <CloseIcon />}
+                    </Fab>
+                  )}
+                {editing &&
+                  !conf.noSubmit && (
+                    <Button
+                      onClick={() => this.setState({ editing: false })}
+                      color="primary"
+                      variant="contained"
+                      aria-label="save"
+                    >
+                      Save
+                    </Button>
+                  )}
               </div>
             )}
           />
