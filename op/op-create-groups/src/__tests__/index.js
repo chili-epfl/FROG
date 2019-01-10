0e682ff5f694e10d546b232d61130f57d802fb87
lfaucon <lpfaucon@gmail.com>
2019-01-10 13:22:56 +0100

    Replaces algorithm for splitting arrays of students

--- op/op-create-groups/src/__tests__/index.js
+++ op/op-create-groups/src/__tests__/index.js
@@ -42,17 +42,17 @@ test('Create one large group with everyone', () =>
 
 test('Create two groups, odd number', () =>
   expect(operator({ groupnumber: true, globalnum: 2 }, obj)).toEqual({
-    group: { '1': ['3', '4', '5'], '2': ['1', '2'] }
+    group: { '1': ['1', '3', '5'], '2': ['2', '4'] }
   }));
 
 test('Create two groups, exact number', () =>
   expect(operator({ groupnumber: true, globalnum: 2 }, obj4)).toEqual({
-    group: { '1': ['3', '4'], '2': ['1', '2'] }
+    group: { '1': ['1', '3'], '2': ['2', '4'] }
   }));
 
 test('Create three groups, very unequal', () =>
   expect(operator({ groupnumber: true, globalnum: 3 }, obj4)).toEqual({
-    group: { '1': ['4'], '2': ['3'], '3': ['1', '2'] }
+    group: { '1': ['1', '4'], '2': ['2'], '3': ['3'] }
   }));
 
 const obj11 = {
@@ -64,30 +64,30 @@ const obj11 = {
 test('Works with large numbers, 3', () =>
   expect(operator({ groupnumber: true, globalnum: 3 }, obj11)).toEqual({
     group: {
-      '1': ['6', '7', '8', '9'],
-      '2': ['2', '3', '4', '5'],
-      '3': ['1', '10', '11']
+      '1': ['1', '2', '5', '8'],
+      '2': ['10', '3', '6', '9'],
+      '3': ['11', '4', '7']
     }
   }));
 
 test('Works with large numbers, 4', () =>
   expect(operator({ groupnumber: true, globalnum: 4 }, obj11)).toEqual({
     group: {
-      '1': ['7', '8', '9'],
-      '2': ['4', '5', '6'],
-      '3': ['11', '2', '3'],
-      '4': ['1', '10']
+      '1': ['1', '3', '7'],
+      '2': ['10', '4', '8'],
+      '3': ['11', '5', '9'],
+      '4': ['2', '6']
     }
   }));
 
 test('Works with large numbers, 5', () =>
   expect(operator({ groupnumber: true, globalnum: 5 }, obj11)).toEqual({
     group: {
-      '1': ['8', '9'],
-      '2': ['6', '7'],
-      '3': ['4', '5'],
-      '4': ['2', '3'],
-      '5': ['1', '10', '11']
+      '1': ['1', '4', '9'],
+      '2': ['10', '5'],
+      '3': ['11', '6'],
+      '4': ['2', '7'],
+      '5': ['3', '8']
     }
   }));
 
@@ -104,3 +104,32 @@ test('test empty', () =>
       }
     )
   ).toEqual({ group: {} }));
+
+test('Fixed number of groups and several extraneous students to assign to groups', () =>
+  expect(
+    operator(
+      { groupnumber: true, globalnum: 8, grouping: 'group' },
+      {
+        _id: 'cjqp26bs7000g01ywt3m8s6n9',
+        activityData: {
+          structure: 'all',
+          payload: { all: { data: null, config: {} } }
+        },
+        socialStructure: {},
+        globalStructure: {
+          studentIds: 'QWERTZUIOPASDFGHJKLYXCVBNM123'.split('')
+        }
+      }
+    )
+  ).toEqual({
+    group: {
+      '1': ['1', 'F', 'N', 'V'],
+      '2': ['2', 'G', 'O', 'W'],
+      '3': ['3', 'H', 'P', 'X'],
+      '4': ['A', 'I', 'Q', 'Y'],
+      '5': ['B', 'J', 'R', 'Z'],
+      '6': ['C', 'K', 'S'],
+      '7': ['D', 'L', 'T'],
+      '8': ['E', 'M', 'U']
+    }
+  }));
