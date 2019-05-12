d60112038acab8dabeb7760ee37c2c0bc1ddd18f
Stian Håklev <shaklev@gmail.com>
2019-05-12 21:43:38 +0200

    WIP

---
+++ frog/imports/api/wikiDocHelpers.js
@@ -0,0 +1,82 @@
+import { uuid } from 'frog-utils';
+
+export const addNewWikiPage = (
+  wikiDoc,
+  pageTitle,
+  setCreated,
+  liType = 'li-richText',
+  plane,
+  instances = {},
+  socialStructure,
+  noNewInstances
+) => {
+  const pageId = uuid();
+  const obj = {
+    id: pageId,
+    valid: true,
+    created: setCreated || false,
+    title: pageTitle,
+    liType,
+    plane,
+    instances,
+    noNewInstances
+  };
+  if (socialStructure) {
+    obj.socialStructure = socialStructure;
+  }
+
+  const op = {
+    p: ['pages', pageId],
+    oi: obj
+  };
+
+  wikiDoc.submitOp(op);
+  return pageId;
+};
+
+export const invalidateWikiPage = (wikiDoc, pageId, cb) => {
+  const op = {
+    p: ['pages', pageId, 'valid'],
+    od: true,
+    oi: false
+  };
+
+  wikiDoc.submitOp(op);
+  if (cb) {
+    cb();
+  }
+};
+
+export const changeWikiPageTitle = (
+  wikiDoc,
+  pageId,
+  oldPageTitle,
+  newPageTitle
+) => {
+  const op = {
+    p: ['pages', pageId, 'title'],
+    od: oldPageTitle,
+    oi: newPageTitle
+  };
+
+  wikiDoc.submitOp(op);
+};
+
+export const markPageAsCreated = (wikiDoc, pageId) => {
+  const op = {
+    p: ['pages', pageId, 'created'],
+    od: false,
+    oi: true
+  };
+
+  wikiDoc.submitOp(op);
+};
+
+export const addInstance = (wikiDoc, pageId, instanceId, liId, username) => {
+  const op = {
+    p: ['pages', pageId, 'instances', instanceId],
+    oi: { liId, username }
+  };
+
+  wikiDoc.submitOp(op);
+};
---
+++ frog/server/wiki.js
@@ -0,0 +1,98 @@
+// @flow
+
+import { addNewWikiPage } from '/imports/api/wikiDocHelpers';
+import { generateReactiveFn } from '/imports/api/generateReactiveFn';
+import { activityTypesObj } from '/imports/activityTypes';
+import { Sessions } from '/imports/api/sessions';
+import { Activities } from '/imports/api/activities';
+import { Objects } from '/imports/api/objects';
+import { Meteor } from 'meteor/meteor';
+import { serverConnection as connection } from './share-db-manager';
+
+const exportActivity = (activityId, wiki, page) => {
+  const act = Activities.findOne(activityId);
+
+  const obj = Objects.findOne(act._id);
+  importWikiFromFROG(act, obj, wiki, page || act.title);
+};
+
+const exportSessionWiki = (sessionId, wiki, userId) => {
+  const session = Sessions.findOne(sessionId);
+
+  const activities = Activities.find({
+    graphId: session.graphId
+  }).fetch();
+
+  activities.forEach(act => {
+    const obj = Objects.findOne(act._id);
+    importWikiFromFROG(act, obj, wiki, act.title, userId);
+  });
+};
+
+export const importWikiFromFROG = async (item, object, wiki, page, userId) => {
+  const instances = await new Promise(resolve =>
+    connection.createFetchQuery(
+      'rz',
+      { _id: { $regex: '^' + item._id } },
+      null,
+      (err, results) => {
+        if (err) {
+          console.error(err);
+        }
+        resolve(results.map(x => x.id.replace(item._id + '/', '')));
+      }
+    )
+  );
+  const genericDoc = connection.get('li');
+  const dataFn = generateReactiveFn(genericDoc);
+
+  const instanceData = instances
+    .filter(x => x !== userId)
+    .reduce((acc, x) => {
+      const payload = {
+        acType: item.activityType,
+        activityData: { config: item.data },
+        rz: item._id + '/' + x,
+        title: item.title,
+        activityTypeTitle: activityTypesObj[item.activityType].meta.name
+      };
+
+      const newId = dataFn.createLearningItem(
+        'li-activity',
+        payload,
+        {
+          title: page
+        },
+        true
+      );
+      acc[x] = {
+        liId: newId,
+        username: Meteor.users.findOne(x)?.username
+      };
+      return acc;
+    }, {});
+
+  const wikiDoc = connection.get('wiki', wiki);
+  wikiDoc.subscribe(err => {
+    if (err) throw err;
+    if (!wikiDoc.type) {
+      wikiDoc.create({ wikiId: wiki, pages: {} });
+    }
+
+    addNewWikiPage(
+      wikiDoc,
+      page,
+      true,
+      'li-activity',
+      item.plane,
+      instanceData,
+      item.groupingKey ? object.socialStructure[item.groupingKey] : undefined,
+      true
+    );
+  });
+};
+
+Meteor.methods({
+  'export.session.wiki': exportSessionWiki,
+  'export.activity.wiki': exportActivity
+});
