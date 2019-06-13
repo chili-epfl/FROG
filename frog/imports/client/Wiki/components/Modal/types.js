6f460f87479535adbf0d9b8aa2a213614770f576
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-06-13 16:53:40 +0200

    Comments

--- frog/imports/client/Wiki/components/Modal/types.js
+++ frog/imports/client/Wiki/components/Modal/types.js
@@ -7,6 +7,8 @@ export type ShowModalFunctionT = (View: React.Node) => void;
 export type HideModalFunctionT = () => void;
 
 export type ModalParentPropsT = {
+  /** Displays the provided React component as a modal */
   showModal: ShowModalFunctionT,
+  /** Hides the current modal */
   hideModal: HideModalFunctionT
 };
