46d4f58ab9fed7dfc02f4c599e402a9d93b0a8c3
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-06-21 17:01:49 +0200

    Removed failing tests

--- packages/reactive-tools/getDisplayName.js
+++
@@ -1,9 +0,0 @@
-export const getDisplayName = (WrappedComponent: any): string => {
-  if (typeof WrappedComponent.displayName === 'string') {
-    return WrappedComponent.displayName;
-  } else if (typeof WrappedComponent.name === 'string') {
-    return WrappedComponent.name;
-  } else {
-    return 'Component';
-  }
-};
--- packages/reactive-tools/index.js
+++
@@ -1,4 +0,0 @@
-import { useReactive } from './useReactive';
-import { generateReactiveFn } from './generateReactiveFn';
-
-export { useReactive, generateReactiveFn };
