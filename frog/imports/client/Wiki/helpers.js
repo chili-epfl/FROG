3a18299ab5e5accc54eccb345595f0e91a595d1c
ajainuary <anurag.jain@epfl.ch>
2019-06-27 14:40:48 +0200

    Linting

--- frog/imports/client/Wiki/helpers.js
+++ frog/imports/client/Wiki/helpers.js
@@ -106,18 +106,26 @@ const listWikis = async () => {
 };
 
 // Function to get the pages of all pages in a wiki, returns [Title, ID]
-const listPages = async (wikiDoc) => {
+const listPages = async wikiDoc => {
   const list = await new Promise(resolve =>
     wikiDoc.fetch(err => {
-      if(err) {
-        console.log("Unable to fetch");
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.log('Unable to fetch');
         return;
       }
-      resolve(Object.keys(wikiDoc.data.pages).filter(key => wikiDoc.data.pages[key].created && wikiDoc.data.pages[key].valid).map(key => [wikiDoc.data.pages[key].title, key])); 
+      resolve(
+        Object.keys(wikiDoc.data.pages)
+          .filter(
+            key =>
+              wikiDoc.data.pages[key].created && wikiDoc.data.pages[key].valid
+          )
+          .map(key => [wikiDoc.data.pages[key].title, key])
+      );
     })
   );
   return list;
-}
+};
 
 export {
   parseDocResults,
