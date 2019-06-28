01362637c1da91f522e219f9a39634d2ec501cb9
Stian Håklev <shaklev@gmail.com>
2019-06-28 09:57:20 +0200

    WIP

--- linkFiles.js
+++ linkFiles.js
@@ -3,18 +3,30 @@ const rimraf = require('rimraf');
 
 rimraf.sync('./frog/public/clientFiles');
 fs.mkdirSync('./frog/public/clientFiles');
-fs.readdirSync('./op').forEach(x => {
-  if (fs.existsSync(`./op/${x}/clientFiles`)) {
+if (fs.existsSync('./op')) {
+  fs.readdirSync('./op').forEach(x => {
+    if (fs.existsSync(`./op/${x}/clientFiles`)) {
+      fs.symlinkSync(
+        `./../../../op/${x}/clientFiles`,
+        `./frog/public/clientFiles/${x}`
+      );
+    }
+  });
+}
+if (fs.existsSync('./ac')) {
+  fs.readdirSync('./ac').forEach(x => {
+    if (fs.existsSync(`./ac/${x}/clientFiles`)) {
+      fs.symlinkSync(
+        `./../../../ac/${x}/clientFiles`,
+        `./frog/public/clientFiles/${x}`
+      );
+    }
+  });
+}
+fs.readdirSync('./frog/imports/internalActivities').forEach(x => {
+  if (fs.existsSync(`./frog/imports/internalActivities/${x}/clientFiles`)) {
     fs.symlinkSync(
-      `./../../../op/${x}/clientFiles`,
-      `./frog/public/clientFiles/${x}`
-    );
-  }
-});
-fs.readdirSync('./ac').forEach(x => {
-  if (fs.existsSync(`./ac/${x}/clientFiles`)) {
-    fs.symlinkSync(
-      `./../../../ac/${x}/clientFiles`,
+      `./../../../frog/imports/internalActivities/${x}/clientFiles`,
       `./frog/public/clientFiles/${x}`
     );
   }
