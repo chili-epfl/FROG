83e4a4e5e320ab5c873889129548042c18062ea4
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-07-01 16:18:22 +0200

    Remove cyclic dependencies in `frog-utils` (#1757)

---
+++ frog-utils/src/booleanize.js
@@ -0,0 +1,3 @@
+// @flow
+
+export const booleanize = (bool: string): boolean => bool === 'true';
---
+++ frog-utils/src/wordWrap.js
@@ -0,0 +1,20 @@
+// @flow
+
+export const wordWrap = (text: string, maxLength: number): string[] => {
+  const result = [];
+  let line = [];
+  let length = 0;
+  text.split(' ').forEach(word => {
+    if (length + word.length >= maxLength) {
+      result.push(line.join(' '));
+      line = [];
+      length = 0;
+    }
+    length += word.length + 1;
+    line.push(word);
+  });
+  if (line.length > 0) {
+    result.push(line.join(' '));
+  }
+  return result;
+};
