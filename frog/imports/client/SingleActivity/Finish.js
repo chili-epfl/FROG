5f11528078cfaa03fcd4cf12ec5353772f8b790c
Anurag Jain <anurag.jain@epfl.ch>
2019-07-02 14:11:54 +0200

    Implementing Single Activities via Sessions (#1758)

---
+++ frog/imports/client/SingleActivity/Finish.js
@@ -0,0 +1,49 @@
+// @flow
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
+import Card from '@material-ui/core/Card';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+import Typography from '@material-ui/core/Typography';
+import { type PropsT } from './types';
+import { style } from './style';
+
+const BASE_URL = window.location.origin;
+
+/**
+ * The final screen displayed after the creation of a single activity.
+ * @param {Object} url - An object containing the paths of public and dashboard view
+ * @param {Function} onReturn - Callback function for handling the 'Back' button
+ */
+function Finish(
+  props: {
+    url: {
+      public: string,
+      dashboard: string
+    },
+    onReturn: Function
+  } & PropsT
+) {
+  const { url, classes } = props;
+  return (
+    <Card raised className={classes.card}>
+      <IconButton
+        onClick={() => props.onReturn()}
+        className={classes.back_button}
+      >
+        <ArrowBack />
+      </IconButton>
+      <Typography variant="h3" component="h2" className={classes.padded_text}>
+        You're all set, please share this link with the participants:{' '}
+        <Link to={'/' + url.public}>{BASE_URL + '/' + url.public}</Link>
+      </Typography>
+      <Typography variant="h4" component="h3" className={classes.padded_text}>
+        Here's a secret link to your behind-the-scene view of the dashboard:{' '}
+        <Link to={'/' + url.dashboard}>{BASE_URL + '/' + url.dashboard}</Link>
+      </Typography>
+    </Card>
+  );
+}
+export default withStyles(style)(Finish);
