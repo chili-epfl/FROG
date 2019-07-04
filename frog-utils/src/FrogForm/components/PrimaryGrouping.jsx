554130fd82572e95c08faa161098f1aefd872b30
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-07-04 14:54:35 +0200

    Fix top bar

--- frog-utils/src/FrogForm/components/PrimaryGrouping.jsx
+++ frog-utils/src/FrogForm/components/PrimaryGrouping.jsx
@@ -1,20 +1,22 @@
 // @flow
 
 import * as React from 'react';
-import { makeStyles, Typography, IconButton } from '@material-ui/core';
+import { makeStyles, Typography, ButtonBase } from '@material-ui/core';
 import { ExpandMore } from '@material-ui/icons';
 
 import type { ObjectFieldTemplatePropsT } from '../types';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(0, 3),
     borderBottom: `1px ${theme.palette.grey[300]} solid`
   },
   header: {
     display: 'flex',
     alignItems: 'center',
-    height: theme.spacing(10)
+    width: '100%',
+    height: theme.spacing(10),
+    padding: theme.spacing(0, 3),
+    textAlign: 'left'
   },
   badge: {
     marginLeft: theme.spacing(1)
@@ -29,6 +31,7 @@ const useStyles = makeStyles(theme => ({
     transition: 'all .2s'
   },
   fields: {
+    padding: theme.spacing(0, 3),
     marginBottom: theme.spacing(3)
   }
 }));
@@ -39,7 +42,10 @@ const useStyles = makeStyles(theme => ({
  */
 export const PrimaryGrouping = (props: ObjectFieldTemplatePropsT) => {
   const classes = useStyles();
-  const [expand, setExpand] = React.useState(false);
+
+  // State to control whether the group is expanded or not
+  // We set it to true to show expanded by default
+  const [expand, setExpand] = React.useState(true);
 
   const title = props.title || props.uiSchema['ui:title'];
   const description = props.description;
@@ -48,23 +54,24 @@ export const PrimaryGrouping = (props: ObjectFieldTemplatePropsT) => {
 
   return (
     <div className={classes.root}>
-      <div className={classes.header}>
+      <ButtonBase
+        className={classes.header}
+        elevation={0}
+        onClick={() => setExpand(!expand)}
+      >
         {title && <Typography variant="h6">{title}</Typography>}
         {description && (
           <Typography className={classes.description} variant="body2">
             {description}
           </Typography>
         )}
-        <IconButton
+        <ExpandMore
           className={classes.expandIcon}
           style={{
             transform: expand ? 'rotate(180deg)' : undefined
           }}
-          onClick={() => setExpand(!expand)}
-        >
-          <ExpandMore />
-        </IconButton>
-      </div>
+        />
+      </ButtonBase>
       {expand && (
         <div className={classes.fields}>
           {properties.map(props => (
