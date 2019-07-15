5f005a62b1d335c5ddfdf14031d9cca0d6faed9d
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-07-15 10:32:32 +0200

    Fix linting

--- frog-utils/src/FrogForm/FieldTemplate.js
+++ frog-utils/src/FrogForm/FieldTemplate.js
@@ -2,34 +2,13 @@
 
 import * as React from 'react';
 
-import { Typography, makeStyles } from '@material-ui/core';
-
 import { Label } from './components/Label';
 import type { FieldTemplatePropsT } from './types';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    padding: theme.spacing(1, 0)
-  },
-  titleAndDescription: {
-    display: 'flex',
-    marginBottom: theme.spacing(0.5)
-  },
-  title: {
-    fontWeight: 700
-  },
-  description: {
-    color: theme.palette.grey[700],
-    paddingLeft: theme.spacing(2)
-  }
-}));
 
 /**
  * Redefines the UI of fields, by changing the way labels are displayed
  */
 export const FieldTemplate = (props: FieldTemplatePropsT) => {
-  const classes = useStyles();
-
   if (props.displayLabel) {
     return (
       <Label label={props.label} description={props.rawDescription}>
