6f4fb954386584be55ce8bb7dbbc0eb6818c11ab
RachitMalik12 <rachitmalik1947@gmail.com>
2019-07-17 11:00:02 +0200

    Added validation to server code when creating an account

--- frog/server/accountManagement.js
+++ frog/server/accountManagement.js
@@ -4,6 +4,28 @@ import { Meteor } from 'meteor/meteor';
 
 type Profile = { displayName: string };
 
+const errorBasedOnChars = (
+  value: string,
+  numberOfChars: number,
+  type: string
+) => {
+  return value.length < numberOfChars
+    ? `${type} has to have a minimum of ${numberOfChars} characters`
+    : '';
+};
+
+const emailErrors = (email: string) => {
+  const emailRegex = RegExp(
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+  );
+  if (email.length === 0)
+    return 'This field is required, please enter an email address';
+  return emailRegex.test(email) ? '' : 'invalid email address';
+};
+const passwordErrors = (password: string) => {
+  return errorBasedOnChars(password, 5, 'password');
+};
+
 /**
  * Creates a new user account if userid is null or upgrades adds the new information to the given userid and makes the given account 'verified'.
  *
@@ -17,34 +39,52 @@ export const createAccount = (
   password: string,
   profile: Profile
 ) => {
-  const user = Meteor.user();
+  if (password !== '' && email !== '' && profile.displayName !== '') {
+    // Validate input params
+    if (
+      passwordErrors(password) !== '' ||
+      emailErrors(email) !== '' ||
+      errorBasedOnChars(profile.displayName, 1, 'Display Name') !== ''
+    ) {
+      throw new Meteor.Error(
+        passwordErrors(password) +
+          '  ' +
+          emailErrors(email) +
+          '  ' +
+          errorBasedOnChars(profile.displayName, 1, 'Display Name')
+      );
+    } else if (!Accounts.findUserByEmail(email)) {
+      const user = Meteor.user();
 
-  if (user?.isAnonymous) {
-    // checks for duplicate email and displays error on the console.
-    if (!Accounts.findUserByEmail(email)) {
-      Meteor.users.update(user._id, {
-        $set: {
-          emails: [{ address: email, verified: false }],
-          isAnonymous: false,
+      if (user?.isAnonymous) {
+        // checks for duplicate email and displays error on the console.
+
+        Meteor.users.update(user._id, {
+          $set: {
+            emails: [{ address: email, verified: false }],
+            isAnonymous: false,
+            profile
+          }
+        });
+        Accounts.setPassword(user._id, password, { logout: false });
+      } else {
+        Accounts.createUser({
+          email,
+          password,
           profile
-        }
-      });
-      Accounts.setPassword(user._id, password, { logout: false });
+        });
+        const newUserId = Accounts.findUserByEmail(email);
+        Meteor.users.update(newUserId, {
+          $set: {
+            isAnonymous: false
+          }
+        });
+      }
     } else {
       throw new Meteor.Error('Email already exists');
     }
   } else {
-    Accounts.createUser({
-      email,
-      password,
-      profile
-    });
-    const newUserId = Accounts.findUserByEmail(email);
-    Meteor.users.update(newUserId, {
-      $set: {
-        isAnonymous: false
-      }
-    });
+    throw new Meteor.Error('Invalid arguments');
   }
 };
 
