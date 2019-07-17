b67248067c58544d601fa2172d4827c72127a7b7
Anurag Jain <anurag.jain@epfl.ch>
2019-07-17 17:06:54 +0200

    Wiki Permissions (#1784)

---
+++ frog/imports/client/Wiki/ModalLocked.js
@@ -0,0 +1,15 @@
+// @flow
+
+import React from 'react';
+import { Modal } from '/imports/frog-utils';
+
+/**
+ * This modal displays an error message when accessing an archived wiki
+ */
+export default () => {
+  return (
+    <Modal title="Wiki Locked" actions={[]}>
+      This wiki has been locked and you do not have access to its contents.
+    </Modal>
+  );
+};
