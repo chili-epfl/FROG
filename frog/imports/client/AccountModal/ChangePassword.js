43aa043ec2912f2c4d0d56914d1484800cb5618a
RachitMalik12 <rachitmalik1947@gmail.com>
2019-07-31 14:07:54 +0200

    Initial changes to build change password modal

--- frog/imports/client/AccountModal/ChangePassword.js
+++ frog/imports/client/AccountModal/ChangePassword.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import {Modal, useModal} from '/imports/ui/Modal';
-import {useToast} from '/imports/ui/Toast'; 
+import { Modal, useModal } from '/imports/ui/Modal';
+import { useToast } from '/imports/ui/Toast';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -19,29 +19,28 @@ const useStyles = makeStyles(theme => ({
     marginTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.secondary.main
   },
   form: {
     width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+    marginTop: theme.spacing(1)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    margin: theme.spacing(3, 0, 2)
+  }
 }));
 
-
 const ChangePasswordModal = () => {
-  const classes = makeStyles(); 
-  const [showToast, hideToast] = useToast(); 
-  const [showModal, hideModal] = useModal(); 
+  const classes = makeStyles();
+  const [showToast, hideToast] = useToast();
+  const [showModal, hideModal] = useModal();
 
   return (
-  	<Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="xs">
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
@@ -71,7 +70,7 @@ const ChangePasswordModal = () => {
             type="password"
             id="newpassword"
           />
-           <TextField
+          <TextField
             variant="outlined"
             margin="normal"
             required
@@ -93,8 +92,5 @@ const ChangePasswordModal = () => {
         </form>
       </div>
     </Container>
-
-
-
-
-}
+  );
+};
