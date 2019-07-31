7d5a52a1f040fb4b36554a35bc1c7ef1440e9c45
RachitMalik12 <rachitmalik1947@gmail.com>
2019-07-31 13:49:25 +0200

    Change Password modal

---
+++ frog/imports/client/AccountModal/ChangePassword.js
@@ -0,0 +1,100 @@
+import * as React from 'react';
+import {Modal, useModal} from '/imports/ui/Modal';
+import {useToast} from '/imports/ui/Toast'; 
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+
+const ChangePasswordModal = () => {
+  const classes = makeStyles(); 
+  const [showToast, hideToast] = useToast(); 
+  const [showModal, hideModal] = useModal(); 
+
+  return (
+  	<Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Reset password
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="oldpassword"
+            label="Old password "
+            name="oldpassword"
+            autoComplete="password"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="newpassword"
+            label="New Password"
+            type="password"
+            id="newpassword"
+          />
+           <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="confirmnewpassword"
+            label="Confirm new password"
+            type="password"
+            id="confirmnewpassword"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Reset password
+          </Button>
+        </form>
+      </div>
+    </Container>
+
+
+
+
+}
