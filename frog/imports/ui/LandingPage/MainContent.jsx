119dc0f061d6038f629e1b2e4fe10af631fd375b
RachitMalik12 <rachitmalik1947@gmail.com>
2019-08-05 13:09:08 +0200

    Linting

--- frog/imports/ui/LandingPage/MainContent.jsx
+++ frog/imports/ui/LandingPage/MainContent.jsx
@@ -7,59 +7,48 @@ import FolderIcon from '@material-ui/icons/Folder';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button'; 
+import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 
-
 const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing(1),
-      display:'flex',
-      justifyContent: 'space-between', 
-      alignItems: 'center'
-    }
-  }));
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(1),
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
+}));
 
-const MainContent = ({itemList,title, action}) =>  {
-    const classes = useStyles(); 
-    return(
-   <div className = {classes.root}>
-       <Grid container spacing = {3}>
-           <Grid item xs={12}>
-               <Paper className = {classes.paper}>
-               <Typography variant = "h5"> {title} </Typography>
-              <Button color = "primary" > {action} </Button>
-               </Paper>
-           </Grid>
-       
-             <List>
-             {itemList.map(item =>{
-                 return(
+const MainContent = ({ itemList, title, action }) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Typography variant="h5"> {title} </Typography>
+            <Button color="primary"> {action} </Button>
+          </Paper>
+        </Grid>
 
-               <ListItem>
-                 <ListItemIcon>
-                   <FolderIcon />
-                 </ListItemIcon>
-                 <ListItemText
-                  primary = {item}
-                 />
-               </ListItem>)
-        })}
-        
-
-       </List>
-           
-
-       </Grid>
+        <List>
+          {itemList.map(item => {
+            return (
+              <ListItem>
+                <ListItemIcon>
+                  <FolderIcon />
+                </ListItemIcon>
+                <ListItemText primary={item} />
+              </ListItem>
+            );
+          })}
+        </List>
+      </Grid>
     </div>
-    );
-    
+  );
+};
 
-
-}
-
-export default MainContent; 
-
+export default MainContent;
--- frog/imports/ui/LandingPage/MainContent.stories.js
+++ frog/imports/ui/LandingPage/MainContent.stories.js
@@ -2,5 +2,10 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import MainContent from './MainContent';
 
-
-storiesOf('MainContentLandingPage', module).add('MainContent', () => <MainContent itemList = {['Tweets 1','Tweets 2','Tweets 3']} title =  'Sessions view'  action = 'Add new session'/> ); 
+storiesOf('MainContentLandingPage', module).add('MainContent', () => (
+  <MainContent
+    itemList={['Tweets 1', 'Tweets 2', 'Tweets 3']}
+    title="Sessions view"
+    action="Add new session"
+  />
+));
