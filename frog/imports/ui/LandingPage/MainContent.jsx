dc83a73a493a4c6b53ac7d3f5ec8e8b102b24319
RachitMalik12 <rachitmalik1947@gmail.com>
2019-08-05 13:08:31 +0200

    Initial work on sessions view on the landing page

---
+++ frog/imports/ui/LandingPage/MainContent.jsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import Typography from '@material-ui/core/Typography';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import FolderIcon from '@material-ui/icons/Folder';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button'; 
+import List from '@material-ui/core/List';
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(1),
+      display:'flex',
+      justifyContent: 'space-between', 
+      alignItems: 'center'
+    }
+  }));
+
+const MainContent = ({itemList,title, action}) =>  {
+    const classes = useStyles(); 
+    return(
+   <div className = {classes.root}>
+       <Grid container spacing = {3}>
+           <Grid item xs={12}>
+               <Paper className = {classes.paper}>
+               <Typography variant = "h5"> {title} </Typography>
+              <Button color = "primary" > {action} </Button>
+               </Paper>
+           </Grid>
+       
+             <List>
+             {itemList.map(item =>{
+                 return(
+
+               <ListItem>
+                 <ListItemIcon>
+                   <FolderIcon />
+                 </ListItemIcon>
+                 <ListItemText
+                  primary = {item}
+                 />
+               </ListItem>)
+        })}
+        
+
+       </List>
+           
+
+       </Grid>
+    </div>
+    );
+    
+
+
+}
+
+export default MainContent; 
+
---
+++ frog/imports/ui/LandingPage/MainContent.stories.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import MainContent from './MainContent';
+
+
+storiesOf('MainContentLandingPage', module).add('MainContent', () => <MainContent itemList = {['Tweets 1','Tweets 2','Tweets 3']} title =  'Sessions view'  action = 'Add new session'/> ); 
