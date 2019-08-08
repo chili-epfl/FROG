ee644019c865011857bfa7fc736dfd0150cd41b4
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-08-08 11:23:06 +0200

    Update TopBar

--- frog/imports/ui/Breadcrumb/index.stories.js
+++ frog/imports/ui/Breadcrumb/index.stories.js
@@ -5,9 +5,9 @@ import { storiesOf } from '@storybook/react';
 import { PlayArrow } from '@material-ui/icons';
 
 import { Breadcrumb } from '.';
-import { StorybookContainer } from '../../StorybookContainer';
+import { StorybookContainer } from '../StorybookContainer';
 
-storiesOf('TopBar/Breadcrumb', module).add('simple', () => (
+storiesOf('Breadcrumb', module).add('simple', () => (
   <StorybookContainer width={300}>
     <Breadcrumb icon={<PlayArrow />} paths={['My Session', 'Ready']} />
   </StorybookContainer>
--- frog/imports/ui/TopBar/index.stories.js
+++ frog/imports/ui/TopBar/index.stories.js
@@ -5,38 +5,18 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { PlayArrow, Details } from '@material-ui/icons';
 import { TopBar } from '.';
+import { Breadcrumb } from '../Breadcrumb';
+import { MinimalButton } from '../Button';
 
 const SimpleWrapper = () => {
   return (
     <TopBar
-      icon={<Details />}
-      paths={['Recent']}
-      primaryActions={[
-        {
-          id: 'start',
-          title: 'Start session',
-          icon: <PlayArrow />,
-          callback: action('action_callback')
-        },
-        {
-          id: 'projector',
-          title: 'Open projector view',
-          callback: action('action_callback')
-        }
-      ]}
-      secondaryActions={[
-        {
-          id: 'start',
-          title: 'Start session',
-          icon: <PlayArrow />,
-          callback: action('action_callback')
-        },
-        {
-          id: 'projector',
-          title: 'Open projector view',
-          callback: action('action_callback')
-        }
-      ]}
+      navigation={<Breadcrumb icon={<Details />} paths={['Recent']} />}
+      actions={
+        <>
+          <MinimalButton text="Clear recents" />
+        </>
+      }
     />
   );
 };
