3b3224732815b6022c61838dc89fabb5eaade76d
RachitMalik12 <rachitmalik1947@gmail.com>
2019-08-12 13:54:44 +0200

    Linting

--- frog/imports/ui/TopBar/index.stories.js
+++ frog/imports/ui/TopBar/index.stories.js
@@ -2,57 +2,20 @@
 
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-<<<<<<< HEAD
-import { action } from '@storybook/addon-actions';
-import { PlayArrow, Details } from '@material-ui/icons';
-import { TopBar } from '.';
-=======
 import { Details } from '@material-ui/icons';
 import { TopBar } from '.';
 import { Breadcrumb } from '../Breadcrumb';
 import { Button } from '../Button';
->>>>>>> cf54266f13f273a46fc131cb3072d9c9ba7bf8ce
 
 const SimpleWrapper = () => {
   return (
     <TopBar
-<<<<<<< HEAD
-      icon={<Details />}
-      paths={['Recent']}
-      primaryActions={[
-        {
-          id: 'start',
-          title: 'Start session',
-          icon: <PlayArrow />,
-          callback: action('action_callback')
-        },
-        {
-          id: 'projector',
-          title: 'Open projector view',
-          callback: action('action_callback')
-        }
-      ]}
-      secondaryActions={[
-        {
-          id: 'start',
-          title: 'Start session',
-          icon: <PlayArrow />,
-          callback: action('action_callback')
-        },
-        {
-          id: 'projector',
-          title: 'Open projector view',
-          callback: action('action_callback')
-        }
-      ]}
-=======
       navigation={<Breadcrumb icon={<Details />} paths={['Recent']} />}
       actions={
         <>
           <Button>Clear recents</Button>
         </>
       }
->>>>>>> cf54266f13f273a46fc131cb3072d9c9ba7bf8ce
     />
   );
 };
