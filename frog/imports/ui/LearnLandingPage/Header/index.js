551e351270a3c668cbd96a325bc4bfa26858c2b1
Stian Håklev <shaklev@gmail.com>
2019-08-14 17:27:02 +0200

    WIP

--- frog/imports/ui/LearnLandingPage/Header/index.js
+++ frog/imports/ui/LearnLandingPage/Header/index.js
@@ -1,24 +1,23 @@
-import * as React from "react";
-import { makeStyles, Typography, Button } from "@material-ui/core";
-import { blueGrey, indigo } from "@material-ui/core/colors";
-import HomeIcon from "@material-ui/icons/Home";
+import * as React from 'react';
+import { makeStyles, Typography } from '@material-ui/core';
+import { blueGrey } from '@material-ui/core/colors';
 
 const useStyle = makeStyles(theme => ({
   root: {
     height: theme.spacing(8),
-    background: "#FFF",
-    display: "flex",
-    alignItems: "center",
+    background: '#FFF',
+    display: 'flex',
+    alignItems: 'center',
     width: `calc(100% - ${2 * theme.spacing(4)})`,
     padding: theme.spacing(0, 4),
-    margin: "0",
-    boxShadow: "0px 5px 10px rgba(0,0,0,.025)"
+    margin: '0',
+    boxShadow: '0px 5px 10px rgba(0,0,0,.025)'
   },
   logo: {
-    fontWeight: "600",
-    fontSize: "16px",
-    textTransform: "uppercase",
-    letterSpacing: "2px",
+    fontWeight: '600',
+    fontSize: '16px',
+    textTransform: 'uppercase',
+    letterSpacing: '2px',
     color: blueGrey[600]
   }
 }));
--- frog/imports/ui/LearnLandingPage/Header/index.stories.js
+++ frog/imports/ui/LearnLandingPage/Header/index.stories.js
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { storiesOf } from "@storybook/react";
-import { Header } from ".";
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Header } from '.';
 
-storiesOf("Learn Landing Page/Header", module).add("Simple", () => <Header />);
+storiesOf('Learn Landing Page/Header', module).add('Simple', () => <Header />);
