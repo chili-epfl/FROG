8d22771dc2401e1083faa81f1229b6098d067a62
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-08-23 16:03:32 +0200

    Fix linting

--- frog/imports/client/TeacherView/components/WelcomeView.js
+++ frog/imports/client/TeacherView/components/WelcomeView.js
@@ -3,24 +3,24 @@
 import * as React from 'react';
 import { makeStyles, Typography } from '@material-ui/core';
 
-const useStyle = makeStyles((theme) => ({
-    root: {
-        textAlign: 'center',
-        padding: theme.spacing(2),
-    }
+const useStyle = makeStyles(theme => ({
+  root: {
+    textAlign: 'center',
+    padding: theme.spacing(2)
+  }
 }));
 
 type WelcomeViewPropsT = {
-    slug: string,
-}
+  slug: string
+};
 
 export const WelcomeView = (props: WelcomeViewPropsT) => {
-    const classes = useStyle();
+  const classes = useStyle();
 
-    return (
-        <div className={classes.root}>
-          <Typography variant="h5">Students can access this session at:</Typography>
-          <Typography variant="h2">{ props.slug }</Typography>
-        </div>
-    )
-}
+  return (
+    <div className={classes.root}>
+      <Typography variant="h5">Students can access this session at:</Typography>
+      <Typography variant="h2">{props.slug}</Typography>
+    </div>
+  );
+};
