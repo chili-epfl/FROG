366ebf6dc0849ae76f5666a1a2450c126f428661
Abhinav Thukral <thukral.abhinav@gmail.com>
2019-08-26 19:05:43 +0530

    Simple Date Formatting for Draft/Session List Items (#1851)
    * Fixing Operator Movement
    
    * Removing Comment
    
    * Initial Commit
    
    * Variable Fix
    
    * Linting
    
    * Adding Time Logic
    
    * Linting
    
    * Resetting the wrong commit

--- frog/imports/client/UserDashboard/data-utils/helpers.js
+++ frog/imports/client/UserDashboard/data-utils/helpers.js
@@ -23,7 +23,8 @@ type meteorDraftsList = Array<meteorDraftObjectT>;
 type meteorSessionsListT = Array<meteorSessionObjectT>;
 
 export const parseDate = (date): Date => {
-  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+  return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} ${date.getDate()}/${date.getMonth() +
+    1}/${date.getFullYear()}`;
 };
 
 const parseEpocDate = (epoc): string => {
