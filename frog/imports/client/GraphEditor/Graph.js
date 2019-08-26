3f57d080204d2d54dbc3d4b2eec5a91544f357a3
Abhinav Thukral <thukral.abhinav@gmail.com>
2019-08-26 13:58:09 +0530

    Fixing Operator Movement

--- frog/imports/client/GraphEditor/Graph.js
+++ frog/imports/client/GraphEditor/Graph.js
@@ -23,7 +23,8 @@ const scrollMouse = e => {
 
 const mousemove = e => {
   // We do -100 here because there is 100px above the graph editor
-  store.ui.socialMove(e.clientX, e.clientY - 100);
+  //store.ui.socialMove(e.clientX, e.clientY - 100);
+  store.ui.socialMove(e.clientX, e.clientY - (screen.height - 300 - 100));
 };
 
 const Graph = connect(
