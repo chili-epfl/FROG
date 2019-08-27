286be97eca63b324955c8af6130537ecc7ef9e1a
Stian Håklev <shaklev@gmail.com>
2019-08-27 09:59:07 +0200

    Fixing template, removing templates to avoid confusion

--- templates/operator/src/operatorRunner.js
+++
@@ -1,7 +0,0 @@
-// @flow
-
-import { type productOperatorRunnerT, wrapUnitAll } from 'frog-utils';
-
-const operator = (configData, object) => wrapUnitAll({});
-
-export default (operator: productOperatorRunnerT);
