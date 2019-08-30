9f5510cdfc345062abc08133821c82365a8d4c7d
Quentin Golsteyn <qgolsteyn@gmail.com>
2019-08-30 13:59:01 +0200

    Fix title in array field

--- frog/imports/frog-utils/FrogForm/FieldTemplate.js
+++ frog/imports/frog-utils/FrogForm/FieldTemplate.js
@@ -1,14 +1,25 @@
 // @flow
 
 import * as React from 'react';
+import { makeStyles } from '@material-ui/core';
 
 import { Label } from './components/Label';
 import type { FieldTemplatePropsT } from './types';
+
+const useStyle = makeStyles(theme => ({
+  '@global': {
+    '.field-array legend': {
+      fontSize: '1.2em !important',
+      fontWeight: 700
+    }
+  }
+}));
 
 /**
  * Redefines the UI of fields, by changing the way labels are displayed
  */
 export const FieldTemplate = (props: FieldTemplatePropsT) => {
+  useStyle();
   if (props.displayLabel) {
     return (
       <Label label={props.label} description={props.rawDescription}>
