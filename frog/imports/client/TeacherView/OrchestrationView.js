67ee570e808c5669cdd6f0904b05095e97d2aeca
Abhinav Thukral <thukral.abhinav@gmail.com>
2019-09-04 13:25:12 +0530

    Sessions Log in fix

--- frog/imports/client/TeacherView/OrchestrationView.js
+++ frog/imports/client/TeacherView/OrchestrationView.js
@@ -6,6 +6,10 @@ import { compose } from 'recompose';
 import { withDragDropContext } from '/imports/frog-utils';
 import { Activities } from '/imports/api/activities';
 
+import { getUserType } from '/imports/api/users';
+import AccountModal from '/imports/client/AccountModal/AccountModal';
+import Dialog from '@material-ui/core/Dialog';
+
 import { OrchestrationContextProvider } from './context';
 import OrchestrationLayout from './components/OrchestrationLayout';
 import { WelcomeView, ConcludedView } from './components/WelcomeView';
@@ -38,12 +42,27 @@ const OrchestrationViewRaw = (props: OrchestrationViewPropsT) => {
   const toggle = id =>
     currentActivity === id ? setCurrentActivity(null) : setCurrentActivity(id);
 
+  const [open, setOpen] = React.useState(true);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <OrchestrationContextProvider
       session={props.session}
       activities={props.activities}
       students={props.students}
     >
+      {getUserType() == 'Anonymous' ? (
+        <Dialog open={open} onClose={handleClose}>
+          <AccountModal
+            formToDisplay="login"
+            closeModal={handleClose}
+            variant="guest"
+          />
+        </Dialog>
+      ) : null}
       <OrchestrationLayout
         orchestrationControl={<SessionControlContainer />}
         sessionSteps={
