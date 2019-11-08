442b92e9667de3ee0913a3affd82e8eff0280afe
sintaz1992 <sina.shah71@gmail.com>
2019-11-08 05:47:20 -0500

    state modified

--- frog/imports/internalActivities/ac-cellulo/dashboard.js
+++ frog/imports/internalActivities/ac-cellulo/dashboard.js
@@ -1,35 +1,26 @@
 import * as React from 'react';
-
 const Viewer = ({ sendMsg, state, activity }) => {
-  console.log('hello');
-  //console.log(state.SEND[0]);
   return (
     <div style={{ maxWidth: 500 }}>
       <h1>Classroom Summary of Robots movement</h1>
-
       <h1>Aggregated heatmap</h1>
-
       <button onClick={() => sendMsg('alarm')}>Alarm</button>
     </div>
   );
 };
-
+const widthGridTable = 10;
+const heightGridTable = 10;
 const prepareDataForDisplay = state => state;
-const NumberTeams = 2;
 const mergeLog = (state, log) => {
-  console.log(log);
-  state.position.x[log.ID.TeamNumber] = log.Payload.positionx;
-  state.position.y[log.ID.TeamNumber] = log.Payload.positiony;
-  console.log(state);
+  state.gridTable[1][4] = 3;
+  //log.Payload.positionx
+  console.log(state.gridTable);
 };
 export default {
   initData: {
-    studentsList: [],
-    position: {
-      x: new Array(NumberTeams).fill(0),
-      y: new Array(NumberTeams).fill(0),
-      theta: new Array(NumberTeams).fill(0)
-    }
+    gridTable: new Array(heightGridTable)
+      .fill(0)
+      .map(() => new Array(widthGridTable).fill(0))
   },
   mergeLog,
   prepareDataForDisplay,
