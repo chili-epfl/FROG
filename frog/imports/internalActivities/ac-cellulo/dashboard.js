4630e5195b7f54d0ce6f1ba5a024c6aff9924bc6
sintaz1992 <sina.shah71@gmail.com>
2019-11-10 04:51:26 -0500

    template for heatmap added

--- frog/imports/internalActivities/ac-cellulo/dashboard.js
+++ frog/imports/internalActivities/ac-cellulo/dashboard.js
@@ -1,18 +1,19 @@
 import * as React from 'react';
+import { HeatMap } from './heatMap';
 const Viewer = ({ sendMsg, state, activity }) => {
   return (
-    <div style={{ maxWidth: 500 }}>
-      <h1>Classroom Summary of Robots movement</h1>
+    <div>
+      <HeatMap Data={state.gridTable} />
       <h1>Aggregated heatmap</h1>
       <button onClick={() => sendMsg('alarm')}>Alarm</button>
     </div>
   );
 };
+const Numofteams = 2;
 const widthGridTable = 10;
 const heightGridTable = 10;
 const prepareDataForDisplay = state => state;
 const mergeLog = (state, log) => {
-  state.gridTable[1][4] = 3;
   //log.Payload.positionx
   console.log(state.gridTable);
 };
---
+++ frog/imports/internalActivities/ac-cellulo/heatMap.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const HeatMap = ({ Data }: { Data: number[] }) => {
+  return <h1>dsdsds{Data[1]}</h1>;
+};
