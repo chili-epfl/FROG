6766cf2b777087db535df8fb56b3a98af10506ab
sintaz1992 <sina.shah71@gmail.com>
2019-11-11 08:56:49 -0500

    map celllo added2

--- frog/imports/internalActivities/ac-cellulo/dashboard.js
+++ frog/imports/internalActivities/ac-cellulo/dashboard.js
@@ -3,59 +3,6 @@ import * as React from 'react';
 import { HeatMap } from './heatMap';
 
 const Viewer = ({ sendMsg, state, activity }) => {
-  const data = [
-    {
-      name: 'Round 1',
-      values: [
-        {
-          id: 1,
-          label: 'Test 1',
-          value: 0,
-          color: 'red'
-        },
-        {
-          id: 2,
-          label: 'Test 2',
-          value: 0,
-          color: ['yellow', 'green']
-        }
-      ]
-    },
-    {
-      name: 'Round 2',
-      values: [
-        {
-          id: 1,
-          label: 'Test 1',
-          value: 10,
-          color: 'red'
-        },
-        {
-          id: 2,
-          label: 'Test 2',
-          value: 5,
-          color: ['yellow', 'green']
-        }
-      ]
-    },
-    {
-      name: 'Round 3',
-      values: [
-        {
-          id: 1,
-          label: 'Test 1',
-          value: 12,
-          color: 'red'
-        },
-        {
-          id: 2,
-          label: 'Test 2',
-          value: 21,
-          color: ['yellow', 'green']
-        }
-      ]
-    }
-  ];
   return (
     <div>
       <HeatMap Data={state.gridTable} />
