ee064937af87f5dbc81b5c0cb5d36dd6282b2cd8
sintaz1992 <sina.shah71@gmail.com>
2019-11-11 08:00:11 -0500

    WIP - Logo.svg

--- frog/imports/internalActivities/ac-cellulo/dashboard.js
+++ frog/imports/internalActivities/ac-cellulo/dashboard.js
@@ -1,10 +1,65 @@
 import * as React from 'react';
+
 import { HeatMap } from './heatMap';
+
 const Viewer = ({ sendMsg, state, activity }) => {
+  const data = [
+    {
+      name: 'Round 1',
+      values: [
+        {
+          id: 1,
+          label: 'Test 1',
+          value: 0,
+          color: 'red'
+        },
+        {
+          id: 2,
+          label: 'Test 2',
+          value: 0,
+          color: ['yellow', 'green']
+        }
+      ]
+    },
+    {
+      name: 'Round 2',
+      values: [
+        {
+          id: 1,
+          label: 'Test 1',
+          value: 10,
+          color: 'red'
+        },
+        {
+          id: 2,
+          label: 'Test 2',
+          value: 5,
+          color: ['yellow', 'green']
+        }
+      ]
+    },
+    {
+      name: 'Round 3',
+      values: [
+        {
+          id: 1,
+          label: 'Test 1',
+          value: 12,
+          color: 'red'
+        },
+        {
+          id: 2,
+          label: 'Test 2',
+          value: 21,
+          color: ['yellow', 'green']
+        }
+      ]
+    }
+  ];
   return (
     <div>
       <HeatMap Data={state.gridTable} />
-      <h1>Aggregated heatmap</h1>
+      <img src="/clientFiles/ac-cellulo/logo.svg" alt="Logo" />;<h1>Aggregated heatmap</h1>
       <button onClick={() => sendMsg('alarm')}>Alarm</button>
     </div>
   );
