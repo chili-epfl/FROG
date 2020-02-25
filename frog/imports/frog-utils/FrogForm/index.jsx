42f9c76ec9bb945867002656bc9c6527b8debf28
lfaucon <lpfaucon@gmail.com>
2020-02-25 14:22:58 +0100

    Fix weird issue with jsonschema-form WithTheme import

--- frog/imports/frog-utils/FrogForm/index.jsx
+++ frog/imports/frog-utils/FrogForm/index.jsx
@@ -1,6 +1,8 @@
 // @flow
 
-import { withTheme } from 'react-jsonschema-form';
+import * as React from 'react';
+
+import withTheme from 'react-jsonschema-form/lib/withTheme';
 
 import { ObjectFieldTemplate } from './ObjectFieldTemplate';
 import { FieldTemplate } from './FieldTemplate';
@@ -13,8 +15,8 @@ const customTheme = {
 };
 
 /**
- * Themed instance of a `react-jsonschema-form` Form. Adds first and second
- * level layouts for object type parameters
+ * Themed instance of a `react-jsonschema-form` Form. Adds
+ * first and second level layouts for object type parameters
  */
 const FrogForm = withTheme(customTheme);
 
